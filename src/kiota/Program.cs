4f092f4da1f05ceff38f835a3d2ff9c6baf6fef4
Darrel Miller <darrel.miller@microsoft.com>
2020-12-22 12:05:08 -0500

    Added generate method

--- src/kiota/Program.cs
+++ src/kiota/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
 using kiota.core;
 using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Readers;
 
 namespace kiota
 {
@@ -19,6 +22,34 @@ namespace kiota
                     .AddEnvironmentVariables(prefix: "KIOTA_")
                     .AddCommandLine(args)
                     .Build();
+        }
+
+        private static void GenerateSDK(string inputPath, string outputPath)
+        {
+            Stream input;
+            if (inputPath.StartsWith("http"))
+            {
+                var httpClient = new HttpClient();
+                input = httpClient.GetStreamAsync(inputPath).GetAwaiter().GetResult();
+            }
+            else
+            {
+                input = new FileStream(inputPath, FileMode.Open);
+            }
+
+            // Parse OpenAPI Input
+            var reader = new OpenApiStreamReader();
+            var doc = reader.Read(input, out var diag);
+            // TODO: Check for errors
+
+            // Generate Code Model
+            var root = KiotaBuilder.Generate(doc);
+
+            // Render source output
+            var outfile = new FileStream(outputPath, FileMode.Create);
+            var renderer = new CSharpRenderer();
+            renderer.Render(root, outfile);
+            outfile.Close();
         }
     }
 }
