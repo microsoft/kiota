ec4d123755684c5a8e4712824c4cd3a9f10b2c01
Darrel Miller <darrel.miller@microsoft.com>
2020-12-22 11:25:22 -0500

    Created empty project files

---
+++ src/kiota.core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace kiota.core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/kiota/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kiota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
