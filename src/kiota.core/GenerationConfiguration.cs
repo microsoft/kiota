f84c776e042f2bf80ff6cede7f5d7fd4e1b2c79c
Vincent Biret <vibiret@microsoft.com>
2020-12-30 17:06:26 -0500

    - adds the default shema namspace for replacement

--- src/kiota.core/GenerationConfiguration.cs
+++ src/kiota.core/GenerationConfiguration.cs
@@ -4,6 +4,7 @@
         public string OutputPath { get; set; } = "./output";
         public string ClientClassName { get; set; } = "GraphClient";
         public string ClientNamespaceName { get; set; } = "GraphClient";
+        public string SchemaRootNamespaceName { get; set; } = "microsoft.graph";
         public GenerationLanguage Language { get; set; } = GenerationLanguage.CSharp;
     }
 }
