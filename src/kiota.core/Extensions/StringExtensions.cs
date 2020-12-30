e2099d2175d87ec989308114c1c6db18403ea5c5
Vincent Biret <vibiret@microsoft.com>
2020-12-30 13:15:30 -0500

    - defensive programming refactorign

--- src/kiota.core/Extensions/StringExtensions.cs
+++ src/kiota.core/Extensions/StringExtensions.cs
@@ -3,28 +3,21 @@ using System.Linq;
 
 namespace kiota.core {
     public static class StringExtensions {
-        public static string ToFirstCharacterLowerCase(this string input) {
-            return string.IsNullOrEmpty(input) ? input : $"{char.ToLowerInvariant(input.FirstOrDefault())}{input.Substring(1)}";
-        }
+        public static string ToFirstCharacterLowerCase(this string input)
+            => string.IsNullOrEmpty(input) ? input : $"{char.ToLowerInvariant(input.FirstOrDefault())}{input.Substring(1)}";
         public static string ToFirstCharacterUpperCase(this string input)
-        {
-            if (input.Length == 0) return input;
-            return Char.ToUpperInvariant(input.FirstOrDefault()) + input.Substring(1);
-        }
-
+            => string.IsNullOrEmpty(input) ? input : Char.ToUpperInvariant(input.FirstOrDefault()) + input.Substring(1);
         public static string ToCamelCase(this string name)
         {
-            var chunks = name.Split("-");
+            if(string.IsNullOrEmpty(name)) return name;
+            var chunks = name.Split("-", StringSplitOptions.RemoveEmptyEntries);
             var identifier = String.Join(null, chunks.Take(1)
                                                   .Union(chunks.Skip(1)
                                                                 .Select(s => ToFirstCharacterUpperCase(s))));
             return identifier;
         }
         public static string ToPascalCase(this string name)
-        {
-            var chunks = name.Split("-");
-            var identifier = String.Join(null, chunks.Select(s => ToFirstCharacterUpperCase(s)));
-            return identifier;
-        }
+            => string.IsNullOrEmpty(name) ? name : String.Join(null, name.Split("-", StringSplitOptions.RemoveEmptyEntries)
+                                                                            .Select(s => ToFirstCharacterUpperCase(s)));
     }
 }
