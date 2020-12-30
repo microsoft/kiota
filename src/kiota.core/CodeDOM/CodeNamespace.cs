5f46ff82f2a931cbf63b1447680613396fe94045
Vincent Biret <vibiret@microsoft.com>
2020-12-30 17:06:00 -0500

    - adds support to add and search for namespaces

--- src/kiota.core/CodeDOM/CodeNamespace.cs
+++ src/kiota.core/CodeDOM/CodeNamespace.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace kiota.core
 {
@@ -23,6 +25,23 @@ namespace kiota.core
         public void AddClass(CodeClass codeClass)
         {
             this.InnerChildElements.Add(codeClass);
+        }
+        public void AddNamespace(params CodeNamespace[] codeNamespaces) {
+            if(!codeNamespaces.Any() || codeNamespaces.Any(x => x == null))
+                throw new ArgumentOutOfRangeException(nameof(codeNamespaces));
+            this.InnerChildElements.AddRange(codeNamespaces);
+        }
+        public bool IsRequestsNamespace { get; set; }
+        public CodeNamespace RequestsNamespace { get => IsRequestsNamespace ? this : this.InnerChildElements.OfType<CodeNamespace>().FirstOrDefault(x => x.IsRequestsNamespace);}
+        public CodeNamespace GetNamespace(string namespaceName) {
+            if(string.IsNullOrEmpty(namespaceName)) 
+                throw new ArgumentNullException(nameof(namespaceName));
+            else if(Name.Equals(namespaceName, StringComparison.InvariantCultureIgnoreCase)) 
+                return this;
+            else if(this.InnerChildElements.OfType<CodeNamespace>().Any()) 
+                return this.InnerChildElements.OfType<CodeNamespace>().FirstOrDefault(x => x.GetNamespace(namespaceName) != null);
+            else 
+                return null;
         }
     }
 }
