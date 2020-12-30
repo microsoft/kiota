125b907269e676d68971d40e53ca4f5cb5eac1b6
Vincent Biret <vibiret@microsoft.com>
2020-12-30 17:08:50 -0500

    - fixes a bug where tree arragenement would stop at the first namespace - fixes a bug where typescript generation would add imports for native types

--- src/kiota.core/Refiners/TypeScriptRefiner.cs
+++ src/kiota.core/Refiners/TypeScriptRefiner.cs
@@ -3,6 +3,7 @@ using System.Linq;
 namespace kiota.core {
     public class TypeScriptRefiner : ILanguageRefiner
     {
+        private readonly string[] defaultTypes = new string[] {"string", "integer", "boolean", "array", "object"};
         public void Refine(CodeNamespace generatedCode)
         {
             AddRelativeImports(generatedCode);
@@ -13,11 +14,13 @@ namespace kiota.core {
                                     .GetChildElements()
                                     .OfType<CodeProperty>()
                                     .Select(x =>x.Type.Name)
+                                    .Union(current.GetChildElements().OfType<CodeMethod>().Select(x => x.ReturnType.Name))
                                     .Distinct()
+                                    .Except(defaultTypes)
                                     .Select(x => new CodeUsing{Name = x})
                                     .ToArray());
             }
-            foreach(var childClass in current.GetChildElements().OfType<CodeClass>())
+            foreach(var childClass in current.GetChildElements())
                 AddRelativeImports(childClass);
         }
     }
