2ff4e45a7717a320dc3cb3695bd07302987088c2
Vincent Biret <vibiret@microsoft.com>
2020-12-30 10:12:46 -0500

    - replaces manual mock by a mocking framework

--- tests/kiota.core.tests/KiotaBuilderTests.cs
+++ tests/kiota.core.tests/KiotaBuilderTests.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.IO;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 
 namespace kiota.core.tests
@@ -11,28 +10,11 @@ namespace kiota.core.tests
         public void Single_root_node_creates_single_request_builder_class()
         {
             var node = new OpenApiUrlSpaceNode("");
-            var builder = new KiotaBuilder(new MockLogger(), new GenerationConfiguration() { ClientClassName = "Graph" });
+            var mockLogger = new Mock<ILogger<KiotaBuilder>>();
+            var builder = new KiotaBuilder(mockLogger.Object, new GenerationConfiguration() { ClientClassName = "Graph" });
             var codeModel = builder.CreateSourceModel(node);
 
             Assert.Single(codeModel.InnerChildElements);
-        }
-    }
-
-    public class MockLogger : ILogger<KiotaBuilder>
-    {
-        public IDisposable BeginScope<TState>(TState state)
-        {
-            return new MemoryStream(); // Something harmless that implements IDisposable
-        }
-
-        public bool IsEnabled(LogLevel logLevel)
-        {
-            return false;
-        }
-
-        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
-        {
-            //NOOP
         }
     }
 }
