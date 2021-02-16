45ba514da83f482685effb0159c84ba54651067b
Vincent Biret <vibiret@microsoft.com>
2021-02-16 12:23:41 -0500

    - alings typescript import management with Java/CSharp

--- src/kiota.core/Refiners/TypeScriptRefiner.cs
+++ src/kiota.core/Refiners/TypeScriptRefiner.cs
@@ -2,28 +2,13 @@ using System.Linq;
 using System.Collections.Generic;
 
 namespace kiota.core {
-    public class TypeScriptRefiner : ILanguageRefiner
+    public class TypeScriptRefiner : CommonLanguageRefiner, ILanguageRefiner
     {
         private readonly HashSet<string> defaultTypes = new HashSet<string> {"string", "integer", "boolean", "array", "object", "(input: object) => object"};
-        public void Refine(CodeNamespace generatedCode)
+        public override void Refine(CodeNamespace generatedCode)
         {
             PatchResponseHandlerType(generatedCode);
-            AddRelativeImports(generatedCode);
-        }
-        private void AddRelativeImports(CodeElement current) {
-            if(current is CodeClass currentClass) {
-                var additionalUsings = current
-                                    .GetChildElements()
-                                    .OfType<CodeProperty>()
-                                    .Select(x =>x.Type)
-                                    .Union(current.GetChildElements().OfType<CodeMethod>().Select(x => x.ReturnType))
-                                    .Where(x => !defaultTypes.Contains(x.Name))
-                                    .Select(x => new CodeUsing(currentClass){Name = x.Name, Declaration = x});
-                if(additionalUsings.Any())
-                    currentClass.AddUsing(additionalUsings.ToArray());
-            }
-            foreach(var childClass in current.GetChildElements())
-                AddRelativeImports(childClass);
+            AddPropertiesAndMethodTypesImports(generatedCode, true, true, true);
         }
         private void PatchResponseHandlerType(CodeElement current) {
             var properties = current.GetChildElements()
