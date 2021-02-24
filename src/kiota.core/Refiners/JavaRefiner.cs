05c428d347ae4736a6de0b0629f1944187ac67f7
Vincent Biret <vibiret@microsoft.com>
2021-02-24 16:06:22 -0500

    - fixes a bug where java optional parameters would not be tagged as optional - fixes a bug where java overrides would be missing for request executors - patches and aligns the handler types for java - adds support for class inheritance in java - adds request executor implementation for java

--- src/kiota.core/Refiners/JavaRefiner.cs
+++ src/kiota.core/Refiners/JavaRefiner.cs
@@ -14,9 +14,40 @@ namespace kiota.core {
             ReplaceIndexersByMethodsWithParameter(generatedCode);
             AddRequireNonNullImports(generatedCode);
             AddPropertiesAndMethodTypesImports(generatedCode, true, false, true);
+            AddDefaultImports(generatedCode);
+            CorrectCoreType(generatedCode);
+            PatchHeaderParametersType(generatedCode);
+        }
+        private static readonly Tuple<string, string>[] defaultNamespacesForRequestBuilders = new Tuple<string, string>[] { 
+            new ("HttpCore", "com.microsoft.kiota"),
+            new ("HttpMethod", "com.microsoft.kiota"),
+            new ("RequestInfo", "com.microsoft.kiota"),
+            new ("QueryParametersBase", "com.microsoft.kiota"),
+            new ("Map", "java.util"),
+            new ("URI", "java.net"),
+            new ("URISyntaxException", "java.net"),
+            new ("InputStream", "java.io"),
+        };
+        private void AddDefaultImports(CodeElement current) {
+            if(current is CodeClass currentClass && currentClass.ClassKind == CodeClassKind.RequestBuilder) {
+                currentClass.AddUsing(defaultNamespacesForRequestBuilders
+                                        .Select(x => {
+                                                        var nUsing = new CodeUsing(currentClass) { 
+                                                            Name = x.Item2,
+                                                        };
+                                                        nUsing.Declaration = new CodeType(nUsing) { Name = x.Item1 };
+                                                        return nUsing;
+                                                    }).ToArray());
+            }
+            CrawlTree(current, AddDefaultImports);
+        }
+        private void CorrectCoreType(CodeElement currentElement) {
+            if (currentElement is CodeProperty currentProperty && currentProperty.Type.Name.Equals("IHttpCore", StringComparison.InvariantCultureIgnoreCase))
+                currentProperty.Type.Name = "HttpCore";
+            CrawlTree(currentElement, CorrectCoreType);
         }
         private void AddRequireNonNullImports(CodeElement currentElement) {
-            if(currentElement is CodeMethod currentMethod && currentMethod.Parameters.Any(x => !x.Type.IsNullable)) {
+            if(currentElement is CodeMethod currentMethod && currentMethod.Parameters.Any(x => !x.Optional)) {
                 var parentClass = currentMethod.Parent as CodeClass;
                 var newUsing = new CodeUsing(parentClass) {
                     Name = "java.util",
@@ -35,28 +66,39 @@ namespace kiota.core {
             if(currentElement is CodeClass currentClass && codeMethods.Any()) {
                 codeMethods
                     .SelectMany(x => x.Parameters)
-                    .Where(x => x.ParameterKind == CodeParameterKind.QueryParameter)
+                    .Where(x => x.ParameterKind == CodeParameterKind.QueryParameter || x.ParameterKind == CodeParameterKind.Headers)
                     .ToList()
                     .ForEach(x => x.Optional = false);
                 currentClass.AddMethod(codeMethods
-                                    .Select(x => GetMethodClone(x))
+                                    .Select(x => GetMethodClone(x, CodeParameterKind.QueryParameter))
+                                    .Where(x => x != null)
+                                    .ToArray());
+                currentClass.AddMethod(codeMethods
+                                    .Select(x => GetMethodClone(x, CodeParameterKind.QueryParameter, CodeParameterKind.Headers))
                                     .Where(x => x != null)
                                     .ToArray());
             }
             
             CrawlTree(currentElement, MakeQueryStringParametersNonOptionalAndInsertOverrideMethod);
         }
-        private CodeMethod GetMethodClone(CodeMethod currentMethod) {
-            if(currentMethod.Parameters.Any(x => x.ParameterKind == CodeParameterKind.QueryParameter)) {
+        private void PatchHeaderParametersType(CodeElement currentElement) {
+            if(currentElement is CodeMethod currentMethod && currentMethod.Parameters.Any(x => x.ParameterKind == CodeParameterKind.Headers))
+                currentMethod.Parameters.Where(x => x.ParameterKind == CodeParameterKind.Headers)
+                                        .ToList()
+                                        .ForEach(x => x.Type.Name = "Map<String, String>");
+            CrawlTree(currentElement, PatchHeaderParametersType);
+        }
+        private CodeMethod GetMethodClone(CodeMethod currentMethod, params CodeParameterKind[] parameterTypesToExclude) {
+            if(currentMethod.Parameters.Any(x => parameterTypesToExclude.Contains(x.ParameterKind))) {
                 var cloneMethod = currentMethod.Clone() as CodeMethod;
-                cloneMethod.Parameters.RemoveAll(x => x.ParameterKind == CodeParameterKind.QueryParameter);
+                cloneMethod.Parameters.RemoveAll(x => parameterTypesToExclude.Contains(x.ParameterKind));
                 return cloneMethod;
             }
             else return null;
         }
         private void PatchResponseHandlerType(CodeElement current) {
             if(current is CodeProperty currentProperty && currentProperty.PropertyKind == CodePropertyKind.ResponseHandler) {
-                currentProperty.Type.Name = "java.util.function.Function<Object,java.util.concurrent.CompletableFuture<Object>>";
+                currentProperty.Type.Name = "java.util.function.Function<InputStream,java.util.concurrent.CompletableFuture<Object>>";
                 currentProperty.DefaultValue = "x -> defaultResponseHandler(x)";
             }
             CrawlTree(current, PatchResponseHandlerType);
