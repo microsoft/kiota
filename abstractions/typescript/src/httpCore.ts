c13f4b3dff91ba601addd7710a67d10a4f8b6689
Vincent Biret <vibiret@microsoft.com>
2021-02-24 12:04:29 -0500

    - fixes a bug where http core prototype would be using the wrong type as parameter

--- abstractions/typescript/src/httpCore.ts
+++ abstractions/typescript/src/httpCore.ts
@@ -1,3 +1,4 @@
+import { RequestInfo } from "./requestInfo";
 export interface HttpCore {
     sendAsync(requestInfo: RequestInfo): Promise<ReadableStream>;
     sendNativeAsync<NativeResponseType>(requestInfo: RequestInfo): Promise<NativeResponseType>;
