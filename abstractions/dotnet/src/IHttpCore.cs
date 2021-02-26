70ce773cbfd43b86a4fd8e3fcc05178174275c73
Vincent Biret <vibiret@microsoft.com>
2021-02-26 12:56:52 -0500

    - adds a response handler interface to simplify the core interface

--- abstractions/dotnet/src/IHttpCore.cs
+++ abstractions/dotnet/src/IHttpCore.cs
@@ -1,9 +1,7 @@
 using System.Threading.Tasks;
-using System.IO;
 
 namespace Kiota.Abstractions {
     public interface IHttpCore {
-        Task<Stream> SendAsync(RequestInfo requestInfo);
-        Task<NativeResponseType> SendNativAsync<NativeResponseType>(RequestInfo requestInfo);
+        Task<ModelType> SendAsync<ModelType>(RequestInfo requestInfo, IResponseHandler responseHandler = default);
     }
 }
