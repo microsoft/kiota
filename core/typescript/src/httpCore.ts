9564a45e7c1754145ccaae1db3d74ce9c2548139
Vincent Biret <vibiret@microsoft.com>
2021-03-12 15:53:55 -0500

    - fixes typescript configuration for core - makes the core implementation platform agnostic (node/browser)

--- core/typescript/src/httpCore.ts
+++ core/typescript/src/httpCore.ts
@@ -1,4 +1,5 @@
 import { AuthenticationProvider, HttpCore as IHttpCore, RequestInfo, ResponseHandler } from '@microsoft/kiota-abstractions';
+import fetch from 'isomorphic-fetch';
 export class HttpCore implements IHttpCore {
     private static readonly authorizationHeaderKey = "Authorization";
     /**
@@ -21,21 +22,33 @@ export class HttpCore implements IHttpCore {
             if(!token) {
                 throw new Error('Could not get an authorization token');
             }
+            if(!requestInfo.headers) {
+                requestInfo.headers = new Map<string, string>();
+            }
             requestInfo.headers?.set(HttpCore.authorizationHeaderKey, `Bearer ${token}`);
         }
         const request = this.getRequestFromRequestInfo(requestInfo);
-        const response = await fetch(request);
+        const response = await fetch(this.getRequestUrl(requestInfo), request);
         if(responseHandler) {
             return await responseHandler.handleResponseAsync(response);
         } else {
             return {} as ModelType; //TODO call default respone handler which will handle deserialization
         }
     }
-    private getRequestFromRequestInfo = (requestInfo: RequestInfo): Request => {
-        let url = requestInfo.URI?.toString() ?? '';
+    private getRequestFromRequestInfo = (requestInfo: RequestInfo): object => {
+        const request = {
+            method: requestInfo.httpMethod?.toString(),
+            headers: new Map<string, string>(),
+            body: requestInfo.content,
+        };
+        requestInfo.headers?.forEach((v, k) => request.headers.set(k, v));
+        return request;
+    }
+    private getRequestUrl = (requestInfo: RequestInfo) : string => {
+        let url = requestInfo.URI ?? '';
         if(requestInfo.queryParameters?.size ?? -1 > 0) {
             url+= '?';
-            requestInfo.queryParameters?.forEach((k, v) => {
+            requestInfo.queryParameters?.forEach((v, k) => {
                 url += k;
                 if(v) {
                     url += `=${v}`;
@@ -44,14 +57,7 @@ export class HttpCore implements IHttpCore {
             });
             url = url.substring(0, url.length -1); // removing the last &
         }
-        const request = {
-            method: requestInfo.httpMethod?.toString(),
-            url,
-            headers: new Headers(),
-            body: requestInfo.content,
-        } as Request;
-        requestInfo.headers?.forEach((k, v) => request.headers.set(k, v));
-        return request;
+        return url;
     }
 
 }
--- core/typescript/src/index.ts
+++ core/typescript/src/index.ts
@@ -1,1 +1,2 @@
 export * from './httpCore';
+export * from './azureIdentityAuthenticationProvider';
