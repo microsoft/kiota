4c6d74b256e2fd3e0eb8ea5d566826ae699cd981
Vincent Biret <vibiret@microsoft.com>
2021-03-19 16:00:24 -0400

    - defensive programming on json serializer

--- core/dotnet/src/serialization/JsonSerializationWriter.cs
+++ core/dotnet/src/serialization/JsonSerializationWriter.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Linq;
 using System.IO;
 using System.Text.Json;
 using Kiota.Abstractions.Serialization;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace KiotaCore.Serialization {
     public class JsonSerializationWriter : ISerializationWriter, IDisposable, IAsyncDisposable {
@@ -14,6 +16,7 @@ namespace KiotaCore.Serialization {
         }
         public async Task<Stream> GetSerializedContent() {
             await writer.FlushAsync();
+            stream.Position = 0;
             return stream;
         }
         public void WriteStringValue(string key, string value) {
@@ -44,62 +47,65 @@ namespace KiotaCore.Serialization {
             if(!string.IsNullOrEmpty(key)) writer.WritePropertyName(key);
             writer.WriteStringValue(value);
         }
-        public void WriteCollectionOfPrimitiveValues<T>(string key, params T[] values) {
+        public void WriteCollectionOfPrimitiveValues<T>(string key, IEnumerable<T> values) {
             if(!string.IsNullOrEmpty(key)) writer.WritePropertyName(key);
             writer.WriteStartArray();
-            foreach(var collectionValue in values) {
-                switch(collectionValue) {
-                    case bool v:
-                        writer.WriteBooleanValue(v);
-                    break;
-                    case string v:
-                        writer.WriteStringValue(v);
-                    break;
-                    case Guid v:
-                        writer.WriteStringValue(v);
-                    break;
-                    case DateTimeOffset v:
-                        writer.WriteStringValue(v);
-                    break;
-                    case int v:
-                        writer.WriteNumberValue(v);
-                    break;
-                    case float v:
-                        writer.WriteNumberValue(v);
-                    break;
-                    case double v:
-                        writer.WriteNumberValue(v);
-                    break;
-                    default:
-                        throw new InvalidOperationException($"unknown type for serialization {collectionValue.GetType().FullName}");
+            if(values?.Any() ?? false)
+                foreach(var collectionValue in values) {
+                    switch(collectionValue) {
+                        case bool v:
+                            writer.WriteBooleanValue(v);
+                        break;
+                        case string v:
+                            writer.WriteStringValue(v);
+                        break;
+                        case Guid v:
+                            writer.WriteStringValue(v);
+                        break;
+                        case DateTimeOffset v:
+                            writer.WriteStringValue(v);
+                        break;
+                        case int v:
+                            writer.WriteNumberValue(v);
+                        break;
+                        case float v:
+                            writer.WriteNumberValue(v);
+                        break;
+                        case double v:
+                            writer.WriteNumberValue(v);
+                        break;
+                        default:
+                            throw new InvalidOperationException($"unknown type for serialization {collectionValue.GetType().FullName}");
+                    }
                 }
-            }
             writer.WriteEndArray();
         }
-        public void WriteCollectionOfObjectValues<T>(string key, params T[] values) where T : class, IParsable<T>, new() {
+        public void WriteCollectionOfObjectValues<T>(string key, IEnumerable<T> values) where T : class, IParsable<T>, new() {
             if(!string.IsNullOrEmpty(key)) writer.WritePropertyName(key);
             writer.WriteStartArray();
-            foreach(var item in values) {
-                item.Serialize(this);
-            }
+            if(values?.Any() ?? false)
+                foreach(var item in values.Where(x => x != null)) {
+                    writer.WriteStartObject();
+                    item.Serialize(this);
+                    writer.WriteEndObject();
+                }
             writer.WriteEndArray();
         }
         public void WriteObjectValue<T>(string key, T value) where T : class, IParsable<T>, new() {
-            if(!string.IsNullOrEmpty(key)) writer.WritePropertyName(key);
-            writer.WritePropertyName(key);
-            writer.WriteStartObject();
-            value.Serialize(this);
-            writer.WriteEndObject();
+            if(value != null) {
+                if(!string.IsNullOrEmpty(key)) writer.WritePropertyName(key);
+                writer.WriteStartObject();
+                value.Serialize(this);
+                writer.WriteEndObject();
+            }
         }
         public void Dispose()
         {
             writer.Dispose();
-            stream.Dispose();
         }
-        public async ValueTask DisposeAsync()
+        public ValueTask DisposeAsync()
         {
-            await writer.DisposeAsync();
-            await stream.DisposeAsync();
+            return writer.DisposeAsync();
         }
     }
 }
