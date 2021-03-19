36bb54e4bcca6ee6ca46143b11f79e0d6880dc0b
Vincent Biret <vibiret@microsoft.com>
2021-03-19 15:59:11 -0400

    - replaces params by collections to simplify code gen

--- abstractions/dotnet/src/serialization/ISerializationWriter.cs
+++ abstractions/dotnet/src/serialization/ISerializationWriter.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
 namespace Kiota.Abstractions.Serialization {
-    public interface ISerializationWriter {
+    public interface ISerializationWriter : IDisposable, IAsyncDisposable {
         void WriteStringValue(string key, string value);
         void WriteBoolValue(string key, bool value);
         void WriteIntValue(string key, int value);
@@ -11,8 +12,8 @@ namespace Kiota.Abstractions.Serialization {
         void WriteDoubleValue(string key, double value);
         void WriteGuidValue(string key, Guid value);
         void WriteDateTimeOffsetValue(string key, DateTimeOffset value);
-        void WriteCollectionOfPrimitiveValues<T>(string key, params T[] values);
-        void WriteCollectionOfObjectValues<T>(string key, params T[] values) where T : class, IParsable<T>, new();
+        void WriteCollectionOfPrimitiveValues<T>(string key, IEnumerable<T> values);
+        void WriteCollectionOfObjectValues<T>(string key, IEnumerable<T> values) where T : class, IParsable<T>, new();
         void WriteObjectValue<T>(string key, T value) where T : class, IParsable<T>, new();
         Task<Stream> GetSerializedContent();
     }
