dc96c6665de5af27d16475c7b78cd4af41d7359a
Vincent Biret <vibiret@microsoft.com>
2021-03-19 15:59:46 -0400

    - specializes vanity method for json payloads

--- abstractions/dotnet/src/RequestInfo.cs
+++ abstractions/dotnet/src/RequestInfo.cs
@@ -13,8 +13,11 @@ namespace Kiota.Abstractions
         public IDictionary<string, object> QueryParameters { get; set; } = new Dictionary<string, object>(StringComparer.InvariantCultureIgnoreCase);
         public IDictionary<string, string> Headers { get; set; } = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
         public Stream Content { get; set; }
-        public async Task SetContentFromParsable<T>(T item, ISerializationWriter writer) where T : class, IParsable<T>, new() {
+        private const string jsonContentType = "application/json";
+        public async Task SetJsonContentFromParsable<T>(T item, Func<string, ISerializationWriter> writerFactory) where T : class, IParsable<T>, new() {
+            using var writer = writerFactory.Invoke(jsonContentType);
             writer.WriteObjectValue(null, item);
+            Headers.Add("Content-Type", jsonContentType);
             Content = await writer.GetSerializedContent();
         }
     }
