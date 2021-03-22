9636aa75907c9c799b16d956e56e1b88e00d135d
Vincent Biret <vibiret@microsoft.com>
2021-03-22 14:31:39 -0400

    - defines serialization and deserialization methods for typescript

--- abstractions/typescript/src/serialization/parseNode.ts
+++ abstractions/typescript/src/serialization/parseNode.ts
@@ -1,3 +1,13 @@
-export interface ParseNode {
+import { Parsable } from "./parsable";
 
+export interface ParseNode {
+    getStringValue(): string;
+    getChildNode(identifier: string): ParseNode;
+    getBooleanValue(): boolean;
+    getNumberValue(): number;
+    getGuidValue(): string;
+    getDateTimeOffsetValue(): Date;
+    getCollectionOfPrimitiveValues<T>(): T[] | undefined;
+    getCollectionOfObjectValues<T extends Parsable<T>>(): T[] | undefined;
+    getObjectValue<T extends Parsable<T>>(): T;
 }
--- abstractions/typescript/src/serialization/serializationWriter.ts
+++ abstractions/typescript/src/serialization/serializationWriter.ts
@@ -1,3 +1,14 @@
-export interface SerializationWriter {
+import { Parsable } from "./parsable";
+import { ReadableStream } from 'web-streams-polyfill/es2018';
 
+export interface SerializationWriter {
+    writeStringValue(key?: string | undefined, value?: string | undefined): void;
+    writeBooleanValue(key?: string | undefined, value?: boolean | undefined): void;
+    writeNumberValue(key?: string | undefined, value?: number | undefined): void;
+    writeGuidValue(key?: string | undefined, value?: string | undefined): void;
+    writeDateTimeOffsetValue(key?: string | undefined, value?: Date | undefined): void;
+    writeCollectionOfPrimitiveValues<T>(key?: string | undefined, values?: T[] | undefined): void;
+    writeCollectionOfObjectValues<T extends Parsable<T>>(key?: string | undefined, values?: T[]): void;
+    writeObjectValue<T extends Parsable<T>>(key?: string | undefined, value?: T | undefined): void;
+    getSerializedContent(): Promise<ReadableStream>;
 }
