fcdf37abecca3c3cb6dfeebe011c184085b84224
Vincent Biret <vibiret@microsoft.com>
2021-04-14 09:11:25 -0400

    Apply suggestions from code review
    Co-authored-by: Mustafa Zengin <mzengin88@gmail.com>

--- core/dotnet/src/extensions/StringExtensions.cs
+++ core/dotnet/src/extensions/StringExtensions.cs
@@ -3,6 +3,6 @@ using System.Linq;
 namespace KiotaCore.Extensions {
     public static class StringExtensions {
         public static string ToFirstCharacterLowerCase(this string input)
-            => string.IsNullOrEmpty(input) ? input : $"{char.ToLowerInvariant(input.FirstOrDefault())}{input.Substring(1)}";
+            => string.IsNullOrEmpty(input) ? input : $"{char.ToLowerInvariant(input[0])}{input[1..]}";
     }
 }
