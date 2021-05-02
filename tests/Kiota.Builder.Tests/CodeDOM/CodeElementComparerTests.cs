ed4c491fec5d6d48b6726d7c33c84baced26880a
Vincent Biret <vibiret@microsoft.com>
2021-05-02 10:19:03 +0200

    - brings codeelementordercomparer coverage to 96%

---
+++ tests/Kiota.Builder.Tests/CodeDOM/CodeElementComparerTests.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Kiota.Builder.tests {
+    public class CodeElementComparerTests {
+        [Fact]
+        public void Works() {
+            var root = CodeNamespace.InitRootNamespace();
+            var comparer = new CodeElementOrderComparer();
+            var method = new CodeMethod(root);
+            method.Parameters.Add(new CodeParameter(method) {
+                Name = "param"
+            });
+            var dataSet = new List<Tuple<CodeElement, CodeElement, int>> {
+                new(null, null, 0),
+                new(null, new CodeClass(root), -1),
+                new(new CodeClass(root), null, 1),
+                new(new CodeUsing(root), new CodeProperty(root), -100),
+                new(new CodeIndexer(root), new CodeProperty(root), 100),
+                new(method, new CodeProperty(root), 101),
+                
+            };
+            foreach(var dataEntry in dataSet) {
+                Assert.Equal(dataEntry.Item3, comparer.Compare(dataEntry.Item1, dataEntry.Item2));
+            }
+        }
+    }
+}
