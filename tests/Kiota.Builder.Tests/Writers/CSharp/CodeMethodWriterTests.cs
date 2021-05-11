4b4d33f24b1f799285bf989239c076c1450dcebf
Vincent Biret <vibiret@microsoft.com>
2021-05-11 15:00:58 +0200

    - adds unit test for CSHarp code method writer

---
+++ tests/Kiota.Builder.Tests/Writers/CSharp/CodeMethodWriterTests.cs
@@ -0,0 +1,290 @@
+using System;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace Kiota.Builder.Writers.CSharp.Tests {
+    public class CodeMethodWriterTests : IDisposable {
+        private const string defaultPath = "./";
+        private const string defaultName = "name";
+        private readonly StringWriter tw;
+        private readonly LanguageWriter writer;
+        private readonly CodeMethod method;
+        private readonly CodeClass parentClass;
+        private const string methodName = "methodName";
+        private const string returnTypeName = "Somecustomtype";
+        private const string methodDescription = "some description";
+        private const string paramDescription = "some parameter description";
+        private const string paramName = "paramName";
+        public CodeMethodWriterTests()
+        {
+            writer = LanguageWriter.GetLanguageWriter(GenerationLanguage.CSharp, defaultPath, defaultName);
+            tw = new StringWriter();
+            writer.SetTextWriter(tw);
+            var root = CodeNamespace.InitRootNamespace();
+            parentClass = new CodeClass(root) {
+                Name = "parentClass"
+            };
+            root.AddClass(parentClass);
+            method = new CodeMethod(parentClass) {
+                Name = methodName,
+            };
+            method.ReturnType = new CodeType(method) {
+                Name = returnTypeName
+            };
+            parentClass.AddMethod(method);
+        }
+        public void Dispose()
+        {
+            tw?.Dispose();
+        }
+        private void AddSerializationProperties() {
+            var addData = parentClass.AddProperty(new CodeProperty(parentClass) {
+                Name = "additionalData",
+                PropertyKind = CodePropertyKind.AdditionalData,
+            }).First();
+            addData.Type = new CodeType(addData) {
+                Name = "string"
+            };
+            var dummyProp = parentClass.AddProperty(new CodeProperty(parentClass) {
+                Name = "dummyProp",
+            }).First();
+            dummyProp.Type = new CodeType(dummyProp) {
+                Name = "string"
+            };
+            var dummyCollectionProp = parentClass.AddProperty(new CodeProperty(parentClass) {
+                Name = "dummyColl",
+            }).First();
+            dummyCollectionProp.Type = new CodeType(dummyCollectionProp) {
+                Name = "string",
+                CollectionKind = CodeTypeBase.CodeTypeCollectionKind.Array,
+            };
+            var dummyComplexCollection = parentClass.AddProperty(new CodeProperty(parentClass) {
+                Name = "dummyComplexColl"
+            }).First();
+            dummyComplexCollection.Type = new CodeType(dummyComplexCollection) {
+                Name = "Complex",
+                CollectionKind = CodeTypeBase.CodeTypeCollectionKind.Array,
+                TypeDefinition = new CodeClass(parentClass.Parent) {
+                    Name = "SomeComplexType"
+                }
+            };
+            var dummyEnumProp = parentClass.AddProperty(new CodeProperty(parentClass){
+                Name = "dummyEnumCollection",
+            }).First();
+            dummyEnumProp.Type = new CodeType(dummyEnumProp) {
+                Name = "SomeEnum",
+                TypeDefinition = new CodeEnum(parentClass.Parent) {
+                    Name = "EnumType"
+                }
+            };
+        }
+        private void AddInheritanceClass() {
+            (parentClass.StartBlock as CodeClass.Declaration).Inherits = new CodeType(parentClass) {
+                Name = "someParentClass"
+            };
+        }
+        private void AddRequestBodyParameters() {
+            var stringType = new CodeType(method) {
+                Name = "string",
+            };
+            method.AddParameter(new CodeParameter(method) {
+                Name = "h",
+                ParameterKind = CodeParameterKind.Headers,
+                Type = stringType,
+            });
+            method.AddParameter(new CodeParameter(method){
+                Name = "q",
+                ParameterKind = CodeParameterKind.QueryParameter,
+                Type = stringType,
+            });
+            method.AddParameter(new CodeParameter(method){
+                Name = "b",
+                ParameterKind = CodeParameterKind.RequestBody,
+                Type = stringType,
+            });
+            method.AddParameter(new CodeParameter(method){
+                Name = "r",
+                ParameterKind = CodeParameterKind.ResponseHandler,
+                Type = stringType,
+            });
+        }
+        [Fact]
+        public void WritesRequestBodiesThrowOnNullHttpMethod() {
+            method.MethodKind = CodeMethodKind.RequestExecutor;
+            Assert.Throws<InvalidOperationException>(() => writer.Write(method));
+            method.MethodKind = CodeMethodKind.RequestGenerator;
+            Assert.Throws<InvalidOperationException>(() => writer.Write(method));
+        }
+        [Fact]
+        public void WritesRequestExecutorBody() {
+            method.MethodKind = CodeMethodKind.RequestExecutor;
+            method.HttpMethod = HttpMethod.Get;
+            AddRequestBodyParameters();
+            writer.Write(method);
+            var result = tw.ToString();
+            Assert.Contains("var requestInfo", result);
+            Assert.Contains("SendAsync", result);
+            Assert.Contains("async", result);
+            Assert.Contains("await", result);
+        }
+        [Fact]
+        public void WritesRequestGeneratorBody() {
+            method.MethodKind = CodeMethodKind.RequestGenerator;
+            method.HttpMethod = HttpMethod.Get;
+            AddRequestBodyParameters();
+            writer.Write(method);
+            var result = tw.ToString();
+            Assert.Contains("var requestInfo = new RequestInfo", result);
+            Assert.Contains("HttpMethod = HttpMethod.GET", result);
+            Assert.Contains("h?.Invoke", result);
+            Assert.Contains("AddQueryParameters", result);
+            Assert.Contains("SetJsonContentFromParsable", result);
+            Assert.Contains("return requestInfo;", result);
+        }
+        [Fact]
+        public void ThrowsOnDeserializerMethods() {
+            var parameter = new CodeParameter(method){
+                Description = paramDescription,
+                Name = paramName
+            };
+            parameter.Type = new CodeType(parameter) {
+                Name = "string"
+            };
+            method.MethodKind = CodeMethodKind.DeserializerBackwardCompatibility;
+            method.IsAsync = false;
+            AddSerializationProperties();
+            Assert.Throws<InvalidOperationException>(() => writer.Write(method));
+        }
+        [Fact]
+        public void WritesInheritedSerializerBody() {
+            method.MethodKind = CodeMethodKind.Serializer;
+            method.IsAsync = false;
+            AddSerializationProperties();
+            AddInheritanceClass();
+            writer.Write(method);
+            var result = tw.ToString();
+            Assert.Contains("base.Serialize(writer);", result);
+        }
+        [Fact]
+        public void WritesSerializerBody() {
+            var parameter = new CodeParameter(method){
+                Description = paramDescription,
+                Name = paramName
+            };
+            parameter.Type = new CodeType(parameter) {
+                Name = "string"
+            };
+            method.MethodKind = CodeMethodKind.Serializer;
+            method.IsAsync = false;
+            AddSerializationProperties();
+            writer.Write(method);
+            var result = tw.ToString();
+            Assert.Contains("WriteStringValue", result);
+            Assert.Contains("WriteCollectionOfPrimitiveValues", result);
+            Assert.Contains("WriteCollectionOfObjectValues", result);
+            Assert.Contains("WriteEnumValue", result);
+            Assert.Contains("WriteAdditionalData(additionalData);", result);
+        }
+        [Fact]
+        public void WritesMethodAsyncDescription() {
+            
+            method.Description = methodDescription;
+            var parameter = new CodeParameter(method){
+                Description = paramDescription,
+                Name = paramName
+            };
+            parameter.Type = new CodeType(parameter) {
+                Name = "string"
+            };
+            method.AddParameter(parameter);
+            writer.Write(method);
+            var result = tw.ToString();
+            Assert.Contains("/// <summary>", result);
+            Assert.Contains(methodDescription, result);
+            Assert.Contains("<param name=", result);
+            Assert.Contains("</param>", result);
+            Assert.Contains(paramName, result);
+            Assert.Contains(paramDescription, result); 
+            Assert.Contains("</summary>", result);
+        }
+        [Fact]
+        public void WritesMethodSyncDescription() {
+            
+            method.Description = methodDescription;
+            method.IsAsync = false;
+            var parameter = new CodeParameter(method){
+                Description = paramDescription,
+                Name = paramName
+            };
+            parameter.Type = new CodeType(parameter) {
+                Name = "string"
+            };
+            method.AddParameter(parameter);
+            writer.Write(method);
+            var result = tw.ToString();
+            Assert.DoesNotContain("@returns a Promise of", result);
+        }
+        [Fact]
+        public void Defensive() {
+            var codeMethodWriter = new CodeMethodWriter(new CSharpConventionService());
+            Assert.Throws<ArgumentNullException>(() => codeMethodWriter.WriteCodeElement(null, writer));
+            Assert.Throws<ArgumentNullException>(() => codeMethodWriter.WriteCodeElement(method, null));
+        }
+        [Fact]
+        public void ThrowsIfParentIsNotClass() {
+            method.Parent = CodeNamespace.InitRootNamespace();
+            Assert.Throws<InvalidOperationException>(() => writer.Write(method));
+        }
+        [Fact]
+        public void ThrowsIfReturnTypeIsMissing() {
+            method.ReturnType = null;
+            Assert.Throws<InvalidOperationException>(() => writer.Write(method));
+        }
+        [Fact]
+        public void WritesReturnType() {
+            writer.Write(method);
+            var result = tw.ToString();
+            Assert.Contains(methodName, result);
+            Assert.Contains(returnTypeName, result);
+            Assert.Contains("Task<", result);// async default
+        }
+        [Fact]
+        public void DoesNotAddUndefinedOnNonNullableReturnType(){
+            method.ReturnType.IsNullable = false;
+            writer.Write(method);
+            var result = tw.ToString();
+            Assert.DoesNotContain("?", result);
+        }
+        [Fact]
+        public void DoesNotAddAsyncInformationOnSyncMethods() {
+            method.IsAsync = false;
+            writer.Write(method);
+            var result = tw.ToString();
+            Assert.DoesNotContain("Promise<", result);
+            Assert.DoesNotContain("async", result);
+        }
+        //TODO: we might want to move those into the convention service tests
+        [Fact]
+        public void WritesPublicMethodByDefault() {
+            writer.Write(method);
+            var result = tw.ToString();
+            Assert.Contains("public ", result);// public default
+        }
+        [Fact]
+        public void WritesPrivateMethod() {
+            method.Access = AccessModifier.Private;
+            writer.Write(method);
+            var result = tw.ToString();
+            Assert.Contains("private ", result);
+        }
+        [Fact]
+        public void WritesProtectedMethod() {
+            method.Access = AccessModifier.Protected;
+            writer.Write(method);
+            var result = tw.ToString();
+            Assert.Contains("protected ", result);
+        }
+    }
+    
+}
