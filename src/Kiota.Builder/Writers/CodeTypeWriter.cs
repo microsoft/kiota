1ad239de791d6963aefa0e0ef1e46bcd1e71e602
Vincent Biret <vibiret@microsoft.com>
2021-05-16 21:40:35 +0200

    - deduplicates code type writers

---
+++ src/Kiota.Builder/Writers/CodeTypeWriter.cs
@@ -0,0 +1,10 @@
+namespace  Kiota.Builder.Writers {
+    public class CodeTypeWriter : BaseElementWriter<CodeType, ILanguageConventionService>
+    {
+        public CodeTypeWriter(ILanguageConventionService conventionService) : base(conventionService){}
+        public override void WriteCodeElement(CodeType codeElement, LanguageWriter writer)
+        {
+            writer.Write(conventions.GetTypeString(codeElement), includeIndent: false);
+        }
+    }
+}
