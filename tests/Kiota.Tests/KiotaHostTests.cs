94b3a9f9daccf08ec5decf7141a4ef5a69336f3d
Vincent Biret <vibiret@microsoft.com>
2021-05-20 16:24:51 -0400

    - adds additional unit tests for parameters validation

--- tests/Kiota.Tests/KiotaHostTests.cs
+++ tests/Kiota.Tests/KiotaHostTests.cs
@@ -9,12 +9,23 @@ namespace Kiota.Tests
     {
         [Fact]
         public async Task ThrowsOnInvalidOutputPath() {
-            await KiotaHost.GetRootCommand().InvokeAsync(new string[] { "-o", "X:\\doesnexit" });
+            await KiotaHost.GetRootCommand().InvokeAsync(new string[] { "-o", "A:\\doesnexit" });
         }
         [Fact]
         public async Task ThrowsOnInvalidInputPath() {
-            await KiotaHost.GetRootCommand().InvokeAsync(new string[] { "-d", "X:\\doesnexit" });
-            await KiotaHost.GetRootCommand().InvokeAsync(new string[] { "-d", "https://nonexistentdomain.net/doesnexit" });
+            await KiotaHost.GetRootCommand().InvokeAsync(new string[] { "-d", "A:\\doesnexit" });
+        }
+        [Fact]
+        public async Task ThrowsOnInvalidInputUrl() {
+            await KiotaHost.GetRootCommand().InvokeAsync(new string[] { "-d", "https://nonexistentdomain56a535ba-bda6-405e-b5e2-ef5f11bf1003.net/doesnexit" });
+        }
+        [Fact]
+        public async Task ThrowsOnInvalidLanguage() {
+            await KiotaHost.GetRootCommand().InvokeAsync(new string[] { "-l", "Pascal" });
+        }
+        [Fact]
+        public async Task ThrowsOnInvalidClassName() {
+            await KiotaHost.GetRootCommand().InvokeAsync(new string[] { "-c", ".Graph" });
         }
     }
 }
