5a0cf5fc1fd72f1d9c7afb5da081cd8243e5696f
Vincent Biret <vibiret@microsoft.com>
2021-05-21 12:38:05 -0400

    - updates abstration library to remove the json assumption

--- abstractions/dotnet/src/RequestInfo.cs
+++ abstractions/dotnet/src/RequestInfo.cs
@@ -12,20 +12,20 @@ namespace Kiota.Abstractions
         public IDictionary<string, object> QueryParameters { get; set; } = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
         public IDictionary<string, string> Headers { get; set; } = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
         public Stream Content { get; set; }
-        private const string jsonContentType = "application/json";
         private const string binaryContentType = "application/octet-stream";
         private const string contentTypeHeader = "Content-Type";
         public void SetStreamContent(Stream content) {
             Content = content;
             Headers.Add(contentTypeHeader, binaryContentType);
         }
-        public void SetJsonContentFromParsable<T>(T item, ISerializationWriterFactory writerFactory) where T : class, IParsable<T>, new() {
-            if(writerFactory != null) {
-                using var writer = writerFactory.GetSerializationWriter(jsonContentType);
-                writer.WriteObjectValue(null, item);
-                Headers.Add(contentTypeHeader, jsonContentType);
-                Content = writer.GetSerializedContent();
-            }
+        public void SetContentFromParsable<T>(T item, ISerializationWriterFactory writerFactory, string contentType) where T : class, IParsable<T>, new() {
+            if(string.IsNullOrEmpty(contentType)) throw new ArgumentNullException(nameof(contentType));
+            if(writerFactory == null) throw new ArgumentNullException(writerFactory);
+
+            using var writer = writerFactory.GetSerializationWriter(contentType);
+            writer.WriteObjectValue(null, item);
+            Headers.Add(contentTypeHeader, contentType);
+            Content = writer.GetSerializedContent();
         }
     }
 }
