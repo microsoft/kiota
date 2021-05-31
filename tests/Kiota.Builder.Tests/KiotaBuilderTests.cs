33843f38487754f7da3eab6c26e32223e3c28700
Vincent Biret <vibiret@microsoft.com>
2021-05-31 09:07:56 -0400

    - removes wrongfully commited unit test

--- tests/Kiota.Builder.Tests/KiotaBuilderTests.cs
+++ tests/Kiota.Builder.Tests/KiotaBuilderTests.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+﻿using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
 
@@ -17,50 +15,6 @@ namespace Kiota.Builder.Tests
             var codeModel = builder.CreateSourceModel(node);
 
             Assert.Single(codeModel.GetChildElements(true));
-        }
-
-        [Fact]
-        public void Single_path_with_get_collection()
-        {
-            var node = new OpenApiUrlSpaceNode("");
-            node.Attach("tasks", new OpenApiPathItem() {
-                Operations = {
-                    [OperationType.Get] = new OpenApiOperation() { 
-                        Responses = new OpenApiResponses
-                        {
-                            ["200"] = new OpenApiResponse()
-                            {
-                                Content =
-                                {
-                                    ["application/json"] = new OpenApiMediaType()
-                                    {
-                                        Schema = new OpenApiSchema
-                                        {
-                                            Type = "array",
-                                            Items = new OpenApiSchema
-                                            {
-                                                Type = "int"
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } 
-            }, "");
-            var mockLogger = new Mock<ILogger<KiotaBuilder>>();
-            var builder = new KiotaBuilder(mockLogger.Object, new GenerationConfiguration() { ClientClassName = "Graph" });
-            var codeModel = builder.CreateSourceModel(node);
-
-            var rootNamespace = codeModel.GetChildElements(true).Single();
-            var rootBuilder = rootNamespace.GetChildElements(true).Single(e => e.Name == "Graph");
-            var tasksProperty = rootBuilder.GetChildElements(true).OfType<CodeProperty>().Single(e => e.Name == "Tasks");
-            var tasksRequestBuilder = tasksProperty.Type as CodeType;
-            Assert.NotNull(tasksRequestBuilder);
-            var getMethod = tasksRequestBuilder.TypeDefinition.GetChildElements(true).OfType<CodeMethod>().Single(e => e.Name == "Get");
-            var returnType = getMethod.ReturnType;
-            Assert.Equal(CodeTypeBase.CodeTypeCollectionKind.Array, returnType.CollectionKind);
         }
     }
 }
