d40415c33402619dfd76ffa4c8ae71d5f729372a
Vincent Biret <vibiret@microsoft.com>
2021-06-03 13:37:54 -0400

    - aligns parsable interfaces across languages

--- abstractions/dotnet/src/serialization/IParsable.cs
+++ abstractions/dotnet/src/serialization/IParsable.cs
@@ -2,12 +2,8 @@ using System;
 using System.Collections.Generic;
 
 namespace Microsoft.Kiota.Abstractions.Serialization {
-    public interface IParsable<T> where T : class {
-        IDictionary<string, Action<T, IParseNode>> DeserializeFields
-        {
-            get;
-        }
-
+    public interface IParsable {
+        IDictionary<string, Action<T, IParseNode>> GetFieldDeserializers<T>();
         void Serialize(ISerializationWriter writer);
 
         IDictionary<string, object> AdditionalData { get; }
