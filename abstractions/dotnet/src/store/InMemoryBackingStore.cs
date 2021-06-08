cd86058c1f315c83737f88b3e08c58709b27d74f
Vincent Biret <vibiret@microsoft.com>
2021-06-08 08:41:06 -0400

    - replaces bad use of KeyValuePair by tuple in memeory backing store

--- abstractions/dotnet/src/store/InMemoryBackingStore.cs
+++ abstractions/dotnet/src/store/InMemoryBackingStore.cs
@@ -6,14 +6,14 @@ namespace Microsoft.Kiota.Abstractions.Store {
     public class InMemoryBackingStore : IBackingStore {
         private bool isInitializationComplete;
         public bool ReturnOnlyChangedValues {get; set;}
-        private readonly Dictionary<string, KeyValuePair<bool, object>> store = new();
+        private readonly Dictionary<string, Tuple<bool, object>> store = new();
         private Dictionary<string, Action<string, object, object>> subscriptions = new();
         public T Get<T>(string key) {
             if(string.IsNullOrEmpty(key))
                 throw new ArgumentNullException(nameof(key));
 
             if(store.TryGetValue(key, out var result))
-                return (T)result.Value;
+                return (T)result.Item2;
             else
                 return default;
         }
@@ -21,7 +21,7 @@ namespace Microsoft.Kiota.Abstractions.Store {
             if(string.IsNullOrEmpty(key))
                 throw new ArgumentNullException(nameof(key));
 
-            var valueToAdd = new KeyValuePair<bool, object>(InitilizationCompleted, value);
+            var valueToAdd = new Tuple<bool, object>(InitilizationCompleted, value);
             object oldValue = null;
             if(!store.TryAdd(key, valueToAdd)) {
                 oldValue = store[key];
@@ -31,8 +31,8 @@ namespace Microsoft.Kiota.Abstractions.Store {
                 sub.Invoke(key, value, oldValue);
         }
         public IEnumerable<KeyValuePair<string, object>> Enumerate() {
-            return (ReturnOnlyChangedValues ? store.Where(x => !x.Value.Key) : store)
-                .Select(x => new KeyValuePair<string, object>(x.Key, x.Value.Value));
+            return (ReturnOnlyChangedValues ? store.Where(x => !x.Value.Item1) : store)
+                .Select(x => new KeyValuePair<string, object>(x.Key, x.Value.Item2));
         }
         public string Subscribe(Action<string, object, object> callback) {
             var id = Guid.NewGuid().ToString();
@@ -49,7 +49,8 @@ namespace Microsoft.Kiota.Abstractions.Store {
             get { return isInitializationComplete; } 
             set {
                 isInitializationComplete = value;
-                store.Values.ToList().ForEach(x => x.Key = !value);
+                foreach(var entry in store)
+                    store[entry.Key] = new (!value, entry.Value.Item2);
             }
         }
     }
