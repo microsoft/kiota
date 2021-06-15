3825377a8c333f9f1a27aa7f3a910ed9c88e8359
abfarah <abfarah@MacBook-Pro.local>
2021-06-15 13:26:12 -0500

    updated request info to be used as class rather then module in http_core module

--- abstractions/ruby/microsoft_kiota_abstractions/lib/microsoft_kiota_abstractions/http_core.rb
+++ abstractions/ruby/microsoft_kiota_abstractions/lib/microsoft_kiota_abstractions/http_core.rb
@@ -3,18 +3,17 @@ require_relative 'response_handler'
 
 module MicrosoftKiotaAbstractions
   module HttpCore
-    include RequestInfo
     include ResponseHandler
 
-    def sendAsync(request_info=RequestInfo, response_handler=ResponseHandler)
+    def sendAsync(request_info=RequestInfo.new, response_handler=ResponseHandler)
       raise NotImplementedError.new
     end
 
-    def sendPrimitiveAsync(request_info=RequestInfo, response_handler=ResponseHandler)
+    def sendPrimitiveAsync(request_info=RequestInfo.new, response_handler=ResponseHandler)
       raise NotImplementedError.new
     end
 
-    def sendAsync(request_info=RequestInfo, response_handler=ResponseHandler)
+    def sendAsync(request_info=RequestInfo.new, response_handler=ResponseHandler)
       raise NotImplementedError.new
     end
 
