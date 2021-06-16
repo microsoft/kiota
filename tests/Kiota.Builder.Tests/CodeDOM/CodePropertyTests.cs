5f80a50e37bc0d7f34e675469faccb30e5fb75a3
Vincent Biret <vibiret@microsoft.com>
2021-06-16 15:59:47 -0400

    - adds unit tests for isOfKind methods

---
+++ tests/Kiota.Builder.Tests/CodeDOM/CodePropertyTests.cs
@@ -0,0 +1,27 @@
+using Xunit;
+
+namespace Kiota.Builder.Tests {
+    public class CodePropertyTests {
+        [Fact]
+        public void Defensive() {
+            var root = CodeNamespace.InitRootNamespace();
+            var property = new CodeProperty(root) {
+                Name = "prop",
+            };
+            Assert.False(property.IsOfKind((CodePropertyKind[])null));
+            Assert.False(property.IsOfKind(new CodePropertyKind[] { }));
+        }
+        [Fact]
+        public void IsOfKind() {
+            var root = CodeNamespace.InitRootNamespace();
+            var property = new CodeProperty(root) {
+                Name = "prop",
+            };
+            Assert.False(property.IsOfKind(CodePropertyKind.BackingStore));
+            property.PropertyKind = CodePropertyKind.RequestBuilder;
+            Assert.True(property.IsOfKind(CodePropertyKind.RequestBuilder));
+            Assert.True(property.IsOfKind(CodePropertyKind.RequestBuilder, CodePropertyKind.BackingStore));
+            Assert.False(property.IsOfKind(CodePropertyKind.BackingStore));
+        }
+    }
+}
