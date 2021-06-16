d9f475de8d481cb08187de7d8381936afa1a9c2f
abfarah <abfarah@MacBook-Pro.local>
2021-06-16 14:16:19 -0500

    removed unnecessary test

--- abstractions/ruby/microsoft_kiota_abstractions/spec/microsoft_kiota_abstractions_spec.rb
+++ abstractions/ruby/microsoft_kiota_abstractions/spec/microsoft_kiota_abstractions_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-require 'uri'
 
 RSpec.describe MicrosoftKiotaAbstractions do
   it "has a version number" do
@@ -9,7 +8,5 @@ RSpec.describe MicrosoftKiotaAbstractions do
   it "tests library method" do
     request_obj = MicrosoftKiotaAbstractions::RequestInfo.new
     expect(!request_obj).to eq(false)
-    request_obj.uri = URI("https://www.microsoft.com/")
-    expect(!request_obj.uri).to eq(false)
   end
 end
