a2ab5772fc8a408d09f4342d4d43f467ba1aa18b
abfarah <t-abdulfarah@microsoft.com>
2021-06-17 15:25:35 -0500

    moved string extensions to extension file

--- src/Kiota.Builder/Extensions/StringExtensions.cs
+++ src/Kiota.Builder/Extensions/StringExtensions.cs
@@ -23,5 +23,15 @@ namespace Kiota.Builder.Extensions {
             original?.Replace("$value", "Content");
         public static string TrimQuotes(this string original) =>
             original?.Trim('\'', '"');
+        
+        public static string ToSnakeCase(this string name)
+        {
+            if(string.IsNullOrEmpty(name)) return name;
+            var chunks = name.Split("-", StringSplitOptions.RemoveEmptyEntries);
+            var identifier = String.Join("_", chunks.Take(1)
+                                                  .Union(chunks.Skip(1)
+                                                                .Select(s => ToFirstCharacterLowerCase(s))));
+            return identifier;
+        }
     }
 }
--- src/Kiota.Builder/PathSegmenters/RubyPathSegmenter.cs
+++ src/Kiota.Builder/PathSegmenters/RubyPathSegmenter.cs
@@ -7,32 +7,8 @@ namespace Kiota.Builder {
     {
         public RubyPathSegmenter(string rootPath, string clientNamespaceName) : base(rootPath, clientNamespaceName) { }
         public override string FileSuffix => ".rb";
-        public override string NormalizeFileName(string elementName) => ToSnakeCase(elementName);
-        public override string NormalizeNamespaceSegment(string segmentName) => ToSnakeCase(segmentName);
-
-        public string ToSnakeCase(string text)
-        {
-            if(text == null) {
-                throw new ArgumentNullException(nameof(text));
-            }
-
-            if(text.Length < 2) {
-                return text;
-            }
-            var sb = new StringBuilder();
-            sb.Append(char.ToLowerInvariant(text[0]));
-            for(int i = 1; i < text.Length; ++i) {
-                char c = text[i];
-                if(char.IsUpper(c)) {
-                    sb.Append('_');
-                    sb.Append(char.ToLowerInvariant(c));
-                } else {
-                    sb.Append(c);
-                }
-            }
-
-            return sb.ToString();
-        }
+        public override string NormalizeFileName(string elementName) => elementName.ToSnakeCase();
+        public override string NormalizeNamespaceSegment(string segmentName) => segmentName.ToSnakeCase();
     }
 
     
