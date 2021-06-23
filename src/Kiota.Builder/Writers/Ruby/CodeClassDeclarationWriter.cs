e70b983e539ed048ba1c1928df1b489017aa8d25
abfarah <t-abdulfarah@microsoft.com>
2021-06-23 15:12:13 -0500

    updated class declaration writer

--- src/Kiota.Builder/Writers/Ruby/CodeClassDeclarationWriter.cs
+++ src/Kiota.Builder/Writers/Ruby/CodeClassDeclarationWriter.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Kiota.Builder.Extensions;
 
 namespace  Kiota.Builder.Writers.Ruby {
@@ -12,90 +9,31 @@ namespace  Kiota.Builder.Writers.Ruby {
         // TODO
         public override void WriteCodeElement(CodeClass.Declaration codeElement, LanguageWriter writer)
         {
+
             foreach (var codeUsing in codeElement.Usings
-                                        .Where(x => x.Declaration?.IsExternal ?? false)
+                                        .Where(x => x.Declaration.IsExternal)
+                                        .Distinct()
                                         .GroupBy(x => x.Declaration?.Name)
                                         .OrderBy(x => x.Key))
-            {
-                //TODO: resolve internal dependencies importing
-                writer.WriteLine($"require_relative '{codeUsing.Key}'");
-            }
+                writer.WriteLine($"require '{codeUsing.Key.ToSnakeCase()}'");
             foreach (var codeUsing in codeElement.Usings
-                                        .Where(x => (!x.Declaration?.IsExternal) ?? true)
-                                        .Where(x => !x.Declaration.Name.Equals(codeElement.Name, StringComparison.OrdinalIgnoreCase))
-                                        .Select(x => {
-                                            var relativeImportPath = GetRelativeImportPathForUsing(x, codeElement.GetImmediateParentOfType<CodeNamespace>());
-                                            return new {
-                                                sourceSymbol = $"{relativeImportPath}{(string.IsNullOrEmpty(relativeImportPath) ? x.Name : x.Declaration.Name.ToFirstCharacterLowerCase())}",
-                                                importSymbol = $"{x.Declaration?.Name?.ToFirstCharacterUpperCase() ?? x.Name}",
-                                            };
-                                        })
-                                        .GroupBy(x => x.sourceSymbol)
+                                        .Where(x => !x.Declaration.IsExternal)
+                                        .Distinct()
+                                        .GroupBy(x => x.Declaration?.Name)
                                         .OrderBy(x => x.Key))
-            {
-                // TODO: resolve external dependencies importing             
-                writer.WriteLine($"require '{codeUsing.Select(x => x.importSymbol).Distinct().Aggregate((x,y) => x + ", " + y)}'");
-            }
+                writer.WriteLine($"require_relative '{codeUsing.Key.ToSnakeCase()}'");
             writer.WriteLine();
-            var derivation = (codeElement.Inherits == null ? string.Empty : $" extends {codeElement.Inherits.Name.ToFirstCharacterUpperCase()}") +
-                            (!codeElement.Implements.Any() ? string.Empty : $" implements {codeElement.Implements.Select(x => x.Name).Aggregate((x,y) => x + " ," + y)}");
+            if(codeElement?.Parent?.Parent is CodeNamespace) {
+                writer.WriteLine($"module {codeElement.Parent.Parent.Name.ToCamelCase()}");
+                writer.IncreaseIndent();
+            }
+            var derivation = (codeElement.Inherits == null ? string.Empty : $" < {codeElement.Inherits.Name.ToFirstCharacterUpperCase()}");
             conventions.WriteShortDescription((codeElement.Parent as CodeClass).Description, writer);
-            writer.WriteLine($"export class {codeElement.Name.ToFirstCharacterUpperCase()}{derivation} {{");
+            writer.WriteLine($"class {codeElement.Name.ToFirstCharacterUpperCase()}{derivation}");
             writer.IncreaseIndent();
-        }
-        private static string GetRelativeImportPathForUsing(CodeUsing codeUsing, CodeNamespace currentNamespace) {
-            if(codeUsing.Declaration == null)
-                return string.Empty;//it's an external import, add nothing
-            var typeDef = codeUsing.Declaration.TypeDefinition;
-
-            if(typeDef == null)
-                return "./"; // it's relative to the folder, with no declaration (default failsafe)
-            else
-                return GetImportRelativePathFromNamespaces(currentNamespace, 
-                                                        typeDef.GetImmediateParentOfType<CodeNamespace>());
-        }
-        private static char namespaceNameSeparator = '.';
-        private static string GetImportRelativePathFromNamespaces(CodeNamespace currentNamespace, CodeNamespace importNamespace) {
-            if(currentNamespace == null)
-                throw new ArgumentNullException(nameof(currentNamespace));
-            else if (importNamespace == null)
-                throw new ArgumentNullException(nameof(importNamespace));
-            else if(currentNamespace.Name.Equals(importNamespace.Name, StringComparison.OrdinalIgnoreCase)) // we're in the same namespace
-                return "./";
-            else
-                return GetRelativeImportPathFromSegments(currentNamespace, importNamespace);                
-        }
-        private static string GetRelativeImportPathFromSegments(CodeNamespace currentNamespace, CodeNamespace importNamespace) {
-            var currentNamespaceSegements = currentNamespace
-                                    .Name
-                                    .Split(namespaceNameSeparator, StringSplitOptions.RemoveEmptyEntries);
-            var importNamespaceSegments = importNamespace
-                                .Name
-                                .Split(namespaceNameSeparator, StringSplitOptions.RemoveEmptyEntries);
-            var importNamespaceSegmentsCount = importNamespaceSegments.Length;
-            var currentNamespaceSegementsCount = currentNamespaceSegements.Length;
-            var deeperMostSegmentIndex = 0;
-            while(deeperMostSegmentIndex < Math.Min(importNamespaceSegmentsCount, currentNamespaceSegementsCount)) {
-                if(currentNamespaceSegements.ElementAt(deeperMostSegmentIndex).Equals(importNamespaceSegments.ElementAt(deeperMostSegmentIndex), StringComparison.OrdinalIgnoreCase))
-                    deeperMostSegmentIndex++;
-                else
-                    break;
-            }
-            if (deeperMostSegmentIndex == currentNamespaceSegementsCount) { // we're in a parent namespace and need to import with a relative path
-                return "./" + GetRemainingImportPath(importNamespaceSegments.Skip(deeperMostSegmentIndex));
-            } else { // we're in a sub namespace and need to go "up" with dot dots
-                var upMoves = currentNamespaceSegementsCount - deeperMostSegmentIndex;
-                var upMovesBuilder = new StringBuilder();
-                for(var i = 0; i < upMoves; i++)
-                    upMovesBuilder.Append("../");
-                return upMovesBuilder.ToString() + GetRemainingImportPath(importNamespaceSegments.Skip(deeperMostSegmentIndex));
-            }
-        }
-        private static string GetRemainingImportPath(IEnumerable<string> remainingSegments) {
-            if(remainingSegments.Any())
-                return remainingSegments.Select(x => x.ToFirstCharacterLowerCase()).Aggregate((x, y) => $"{x}/{y}") + '/';
-            else
-                return string.Empty;
+            var mixins = (!codeElement.Implements.Any() ? string.Empty : $"include {codeElement.Implements.Select(x => x.Name).Aggregate((x,y) => x + ", " + y)}");
+            writer.WriteLine($"{mixins}");
+                            
         }
     }
 }
