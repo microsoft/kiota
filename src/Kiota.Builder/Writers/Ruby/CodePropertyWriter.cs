c2d8ed3f167a1b88606521cbcf0220cff1e24b87
abfarah <t-abdulfarah@microsoft.com>
2021-07-02 18:37:19 -0500

    updated property writer

--- src/Kiota.Builder/Writers/Ruby/CodePropertyWriter.cs
+++ src/Kiota.Builder/Writers/Ruby/CodePropertyWriter.cs
@@ -7,7 +7,20 @@ namespace Kiota.Builder.Writers.Ruby {
         public CodePropertyWriter(RubyConventionService conventionService) : base(conventionService){}
         public override void WriteCodeElement(CodeProperty codeElement, LanguageWriter writer)
         {
-            //TODO
+            conventions.WriteShortDescription(codeElement.Description, writer);
+            var returnType = conventions.GetTypeString(codeElement.Type);
+            switch(codeElement.PropertyKind) {
+                case CodePropertyKind.RequestBuilder:
+                    writer.WriteLine($"def {codeElement.Name.ToSnakeCase()}()");
+                    writer.IncreaseIndent();
+                    conventions.AddRequestBuilderBody(returnType, writer);
+                    writer.DecreaseIndent();
+                    writer.WriteLine("end");
+                break;
+                default:
+                    writer.WriteLine($"@{codeElement.NamePrefix}{codeElement.Name.ToSnakeCase()}");
+                break;
+            }
         }
     }
 }
