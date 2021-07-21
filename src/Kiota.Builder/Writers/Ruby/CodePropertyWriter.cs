88759c7009afda89d969833939c9a84d575e0a0b
abfarah <t-abdulfarah@microsoft.com>
2021-07-20 20:42:49 -0500

    removed redundant code

--- src/Kiota.Builder/Writers/Ruby/CodePropertyWriter.cs
+++ src/Kiota.Builder/Writers/Ruby/CodePropertyWriter.cs
@@ -6,12 +6,14 @@ namespace Kiota.Builder.Writers.Ruby {
         public CodePropertyWriter(RubyConventionService conventionService) : base(conventionService){}
         public override void WriteCodeElement(CodeProperty codeElement, LanguageWriter writer)
         {
+            var accessModifier = conventions.GetAccessModifier(codeElement.Access);
             conventions.WriteShortDescription(codeElement.Description, writer);
+            var returnType = conventions.GetTypeString(codeElement.Type);
             switch(codeElement.PropertyKind) {
                 case CodePropertyKind.RequestBuilder:
                     writer.WriteLine($"def {codeElement.Name.ToSnakeCase()}()");
                     writer.IncreaseIndent();
-                    conventions.AddRequestBuilderBody(writer);
+                    conventions.AddRequestBuilderBody(returnType, writer);
                     writer.DecreaseIndent();
                     writer.WriteLine("end");
                 break;
