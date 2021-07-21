2b832ab147b961c66135ad99bffe2d79514b0e73
abfarah <t-abdulfarah@microsoft.com>
2021-07-20 20:44:12 -0500

    unit tests

--- tests/Kiota.Builder.Tests/Writers/CSharp/CodeClassDeclarationWriterTests.cs
+++ tests/Kiota.Builder.Tests/Writers/CSharp/CodeClassDeclarationWriterTests.cs
@@ -22,6 +22,7 @@ namespace Kiota.Builder.Writers.CSharp.Tests {
                 Name = "parentClass"
             };
             root.AddClass(parentClass);
+            root.AddNamespace("test");
         }
         public void Dispose() {
             tw?.Dispose();
