be74eb8d0857e9e6e57337cc5ea0b1d333ca4469
abfarah <t-abdulfarah@microsoft.com>
2021-07-21 10:43:00 -0500

    updated test case

--- tests/Kiota.Builder.Tests/Writers/Ruby/CodeNamespaceWriterTests.cs
+++ tests/Kiota.Builder.Tests/Writers/Ruby/CodeNamespaceWriterTests.cs
@@ -32,60 +32,7 @@ namespace Kiota.Builder.Writers.Ruby.Tests {
             codeElementWriter.WriteCodeElement(childNamespace, writer);
             var result = tw.ToString();
             Assert.Contains("module", result);
-            Assert.Equal(1, Regex.Matches(result, ".*end.*").Count());
+            Assert.Single(Regex.Matches(result, ".*end.*"));
         }
-        // [Fact]
-        // public void WritesImplementation() {
-        //     var declaration = parentClass.StartBlock as CodeClass.Declaration;
-        //     declaration.Implements.Add(new (parentClass){
-        //         Name = "someInterface"
-        //     });
-        //     codeElementWriter.WriteCodeElement(declaration, writer);
-        //     var result = tw.ToString();
-        //     Assert.Contains("include", result);
-        // }
-        // [Fact]
-        // public void WritesInheritance() {
-        //     var declaration = parentClass.StartBlock as CodeClass.Declaration;
-        //     declaration.Inherits = new (parentClass){
-        //         Name = "someInterface"
-        //     };
-        //     codeElementWriter.WriteCodeElement(declaration, writer);
-        //     var result = tw.ToString();
-        //     Assert.Contains("<", result);
-        // }
-        // [Fact]
-        // public void WritesImports() {
-        //     var declaration = parentClass.StartBlock as CodeClass.Declaration;
-        //     declaration.Usings.Add(new (parentClass) {
-        //         Name = "Objects",
-        //         Declaration = new(parentClass) {
-        //             Name = "util",
-        //             IsExternal = true,
-        //         }
-        //     });
-        //     declaration.Usings.Add(new (parentClass) {
-        //         Name = "project-graph",
-        //         Declaration = new(parentClass) {
-        //             Name = "Message",
-        //         }
-        //     });
-        //     codeElementWriter.WriteCodeElement(declaration, writer);
-        //     var result = tw.ToString();
-        //     Assert.Contains("require_relative", result);
-        //     Assert.Contains("message", result);
-        //     Assert.Contains("require", result);
-        // }
-        // [Fact]
-        // public void WritesMixins() {
-        //     var declaration = parentClass.StartBlock as CodeClass.Declaration;
-        //     declaration.Implements.Add(new (parentClass) {
-        //         Name = "test"
-        //     });
-        //     codeElementWriter.WriteCodeElement(declaration, writer);
-        //     var result = tw.ToString();
-        //     Assert.Contains("include", result);
-        //     Assert.Contains("test", result);
-        // }
     }
 }
