684f5d289394a9cd06f0d8534d7751ae7220ca5f
Vincent Biret <vibiret@microsoft.com>
2021-07-27 16:22:12 -0400

    - adds support for usings in go

--- src/Kiota.Builder/Writers/Go/CodeClassDeclarationWriter.cs
+++ src/Kiota.Builder/Writers/Go/CodeClassDeclarationWriter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Kiota.Builder.Extensions;
 
@@ -8,11 +9,36 @@ namespace Kiota.Builder.Writers.Go {
 
         public override void WriteCodeElement(CodeClass.Declaration codeElement, LanguageWriter writer)
         {
-            if(codeElement?.Parent?.Parent is CodeNamespace ns) {
-                writer.WriteLine($"package {ns.Name.Split('.').Last().ToLowerInvariant()}");
-            }//TODO usings
+            if(codeElement?.Parent?.Parent is CodeNamespace ns)
+                writer.WriteLine($"package {ns.Name.GetLastNamespaceSegment()}");
+            var importSegments = codeElement
+                                .Usings
+                                .Where(x => !x.Declaration.IsExternal)
+                                .Select(i => i.Name.GetInternalNamespaceImport())
+                                .Distinct()
+                                .ToList();
+            importSegments.AddRange(codeElement.Usings.Where(x => x.Declaration.IsExternal).Select(i => i.Declaration.Name).Distinct());
+            if(importSegments.Any()) {
+                writer.WriteLine("import (");
+                writer.IncreaseIndent();
+                foreach(var importSegment in importSegments)
+                    writer.WriteLine($"\"{importSegment}\"");
+                writer.DecreaseIndent();
+                writer.WriteLine(")");
+            }
             writer.WriteLine($"type {codeElement.Name.ToFirstCharacterUpperCase()} struct {{");
             writer.IncreaseIndent();
+        }
+        
+    }
+    public static class NamespaceStringExtensions {
+        public static string GetLastNamespaceSegment(this string nsName) { 
+            var urlPrefixIndex = nsName.LastIndexOf('/') + 1;
+            return nsName[urlPrefixIndex..].Split('.').Last().ToLowerInvariant();
+        }
+        public static string GetInternalNamespaceImport(this string nsName) {
+            var urlPrefixIndex = nsName.LastIndexOf('/') + 1;
+            return nsName[0..urlPrefixIndex] + nsName[urlPrefixIndex..].Split('.').Aggregate((x, y) => $"{x}/{y}");
         }
     }
 }
