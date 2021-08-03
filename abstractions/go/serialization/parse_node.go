ad201415b0f152a6e7fb3a37965220eee34144a9
Vincent Biret <vibiret@microsoft.com>
2021-08-03 16:31:45 -0400

    - fixes a casing issue

--- abstractions/go/serialization/parse_node.go
+++ abstractions/go/serialization/parse_node.go
@@ -1,8 +1,8 @@
 package serialization
 
 type ParseNode interface {
-	getChildNode(index string) (ParseNode, error)
-	getCollectionOfObjectValues() []Parsable
-	getCollectionOfPrimitiveValues() []interface{}
-	getObjectValue() Parsable
+	GetChildNode(index string) (ParseNode, error)
+	GetCollectionOfObjectValues() []Parsable
+	GetCollectionOfPrimitiveValues() []interface{}
+	GetObjectValue() Parsable
 }
