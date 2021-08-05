24ef52d0a5c7f37768785020c09982090fa7728d
abfarah <t-abdulfarah@microsoft.com>
2021-08-05 09:57:36 -0500

    fixed bug

--- serialization/ruby/json/lib/json_parse_node.rb
+++ serialization/ruby/json/lib/json_parse_node.rb
@@ -8,5 +8,72 @@ module MicrosoftKiotaSerialization
         def initialize(node)
             @current_node = node
         end
+        def get_string_value()
+            return @current_node.to_s
+        end
+        def get_boolean_value()
+            return @current_node
+        end
+        def get_number_value()
+            return @current_node.to_i
+        end
+        def get_guid_value()
+            return UUIDTools::UUID.parse(@current_node)
+        end
+        def get_date_value()
+            return Time.parse(@current_node)
+        end
+        def get_collection_of_primitive_values()
+            return @current_node.map do |x|
+                current_parse_node = JsonParseNode.new(x)
+                begin
+                    date = current_parse_node.get_date_value()
+                    date
+                rescue ArgumentError
+                    begin
+                        guid = current_parse_node.get_guid_value()
+                        guid
+                    rescue ArgumentError
+                        val = current_parse_node.get_scalar_value()
+                        val
+                    end
+                end
+            end
+        end
+        def get_collection_of_object_values(type)
+            return @current_node.map do |x|
+                current_parse_node = JsonParseNode.new(x)
+                val = current_parse_node.get_object_value(type)
+                val
+            end
+        end
+        def get_object_value(type)
+            begin
+                item = type.new()
+                self.assign_field_values(item)
+                item
+            rescue => exception
+                raise Exception.new 'Error during deserialization'
+            end
+        end
+        def assign_field_values(item)
+            fields = item.get_field_deserializers()
+            @current_node.each do |k, v| 
+                deserializer = fields[k]
+                if deserializer
+                    deserializer.(item, JsonParseNode.new(v))
+                else
+                    if item.additional_data
+                        item.additional_data[k] = v
+                    else
+                        item.additional_data = Hash.new(k => v)
+                    end
+                end
+                    
+            end
+        end
+        def get_enum_value(type)
+            return @current_node.to_sym
+        end
     end
 end
--- serialization/ruby/json/spec/serialization_spec.rb
+++ serialization/ruby/json/spec/serialization_spec.rb
@@ -1,11 +1,12 @@
 require_relative 'spec_helper'
-require_relative 'Json'
+require 'json'
 
 RSpec.describe "Serialization" do
 
   it "can build jsonParseNode" do
-    json_parse_node = MicrosoftKiotaSerialization::JsonParseNode.new(JSON.parse(`{"value": [{"hasAttachments": false}] }`))
+    json_parse_node = MicrosoftKiotaSerialization::JsonParseNode.new(JSON.parse('{"value": [{"hasAttachments": false}] }'))
     expect(json_parse_node).not_to be nil
+    expect(json_parse_node.get_string_value()).to eq("{\"value\"=>[{\"hasAttachments\"=>false}]}")
   end
 
 end
