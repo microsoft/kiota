f0bbf69ffc1763d08ee42be7691379c7bc98366b
abfarah <t-abdulfarah@microsoft.com>
2021-08-05 09:02:26 -0500

    updated spec to include parsed json

--- serialization/ruby/json/spec/serialization_spec.rb
+++ serialization/ruby/json/spec/serialization_spec.rb
@@ -1,9 +1,10 @@
 require_relative 'spec_helper'
+require_relative 'Json'
 
 RSpec.describe "Serialization" do
 
   it "can build jsonParseNode" do
-    json_parse_node = MicrosoftKiotaSerialization::JsonParseNode.new(`{"value": [{"hasAttachments": false}] }`)
+    json_parse_node = MicrosoftKiotaSerialization::JsonParseNode.new(JSON.parse(`{"value": [{"hasAttachments": false}] }`))
     expect(json_parse_node).not_to be nil
   end
 
