0f4002a17671d77be9543938fbfc9c6eb087810d
abfarah <t-abdulfarah@microsoft.com>
2021-08-11 13:42:59 -0500

    initial commit: ruby http nethttp

---
+++ http/ruby/nethttp/microsoft_kiota_nethttp/lib/microsoft_kiota_nethttp.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative "microsoft_kiota_nethttp/version"
+
+module MicrosoftKiotaNethttp
+end
---
+++ http/ruby/nethttp/microsoft_kiota_nethttp/lib/microsoft_kiota_nethttp/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MicrosoftKiotaNethttp
+  VERSION = "0.1.0"
+end
