ee9d2d4c4ba7469d6207b560a21000d35c70a89f
Andrew Omondi <andrew.omondi@microsoft.com>
2021-08-17 18:08:20 +0300

    Adds editor config and doc comments for serialization project

--- serialization/dotnet/json/README.md
+++ serialization/dotnet/json/README.md
@@ -5,7 +5,7 @@
 - [ ] coverage code
 - [ ] analyzers
 - [ ] unit test project
-- [ ] docs comments
+- [x] docs comments
 
 ## Using the JSON Serialization implementations
 
--- serialization/dotnet/json/src/JsonSerializationWriterFactory.cs
+++ serialization/dotnet/json/src/JsonSerializationWriterFactory.cs
@@ -1,10 +1,29 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
 using System;
 using Microsoft.Kiota.Abstractions.Serialization;
 
-namespace Microsoft.Kiota.Serialization.Json {
-    public class JsonSerializationWriterFactory : ISerializationWriterFactory {
+namespace Microsoft.Kiota.Serialization.Json
+{
+    /// <summary>
+    /// The <see cref="ISerializationWriterFactory"/> implementation for the json content type
+    /// </summary>
+    public class JsonSerializationWriterFactory : ISerializationWriterFactory
+    {
+        /// <summary>
+        /// The valid content type for json
+        /// </summary>
         public string ValidContentType { get; } = "application/json";
-        public ISerializationWriter GetSerializationWriter(string contentType) {
+
+        /// <summary>
+        /// Get a valid <see cref="ISerializationWriter"/> for the content type
+        /// </summary>
+        /// <param name="contentType">The content type to search for</param>
+        /// <returns>A <see cref="ISerializationWriter"/> instance for json writing</returns>
+        public ISerializationWriter GetSerializationWriter(string contentType)
+        {
             if(string.IsNullOrEmpty(contentType))
                 throw new ArgumentNullException(nameof(contentType));
             else if(!ValidContentType.Equals(contentType, StringComparison.OrdinalIgnoreCase))
