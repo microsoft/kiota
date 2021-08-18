54485f53018fe9f5db571a2f52f95767c5290136
Vincent Biret <vibiret@microsoft.com>
2021-08-18 11:08:55 -0400

    - aligns authentication infrastructure in go with other languages

---
+++ abstractions/go/authentication/base_bearer_token_authentication_provider.go
@@ -0,0 +1,38 @@
+package authentication
+
+import (
+	"errors"
+
+	abs "github.com/microsoft/kiota/abstractions/go"
+)
+
+const authorizationHeader = "Authorization"
+
+type BaseBearerTokenAuthenticationProvider struct {
+	getAuthorizationToken func(request abs.RequestInfo) (string, error)
+}
+
+func NewBaseBearerTokenAuthenticationProvider(getAuthorizationToken func(request abs.RequestInfo) (string, error)) *BaseBearerTokenAuthenticationProvider {
+	return &BaseBearerTokenAuthenticationProvider{getAuthorizationToken}
+}
+
+func (provider *BaseBearerTokenAuthenticationProvider) Authenticate(request abs.RequestInfo) error {
+	if request.Headers == nil {
+		request.Headers = make(map[string]string)
+	}
+	if provider.getAuthorizationToken == nil {
+		return errors.New("This class is abstract, you need to derive from it and implement the GetAuthorizationToken method.")
+	}
+	if request.Headers[authorizationHeader] == "" {
+		token, err := provider.getAuthorizationToken(request)
+		if err != nil {
+			return err
+		}
+		if token == "" {
+			return errors.New("Could not get an authorization token")
+		}
+		request.Headers[authorizationHeader] = "Bearer " + token
+	}
+
+	return nil
+}
