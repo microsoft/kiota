706503be0166ebf6fcb84ff407bfa199fda5d14c
Vincent Biret <vibiret@microsoft.com>
2021-08-30 10:34:30 -0400

    - fixes #547 changes the default namespace to avoid conflicts in dotnet
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Kiota.Builder/GenerationConfiguration.cs
+++ src/Kiota.Builder/GenerationConfiguration.cs
@@ -5,7 +5,7 @@ namespace Kiota.Builder {
         public string OpenAPIFilePath { get; set; } = "openapi.yaml";
         public string OutputPath { get; set; } = "./output";
         public string ClientClassName { get; set; } = "ApiClient";
-        public string ClientNamespaceName { get; set; } = "ApiClient";
+        public string ClientNamespaceName { get; set; } = "ApiSdk";
         public GenerationLanguage Language { get; set; } = GenerationLanguage.CSharp;
         public string ApiRootUrl { get; set; } = "https://graph.microsoft.com/v1.0";
         public List<string> PropertiesPrefixToStrip { get; set; } = new() { "@odata."};
