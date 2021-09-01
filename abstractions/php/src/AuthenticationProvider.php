0f7cc93f1e6c26d156db9debe84b9f0fbccbd98f
SilasKenneth <silaskenneth1@gmail.com>
2021-09-01 10:27:27 +0300

    Change the authentication classes PHP.

--- abstractions/php/src/AuthenticationProvider.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Microsoft\Kiota\Abstractions;
-
-use Http\Promise\Promise;
-
-interface AuthenticationProvider {
-    public function getAuthorizationToken(string $requestUrl): Promise;
-}
