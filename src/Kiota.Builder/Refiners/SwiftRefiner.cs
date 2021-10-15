54024750cab26516139590f227de05456d476dcb
Mustafa Zengin <muzengin@microsoft.com>
2021-10-15 15:49:46 -0700

    capitalize namespace first letters

--- src/Kiota.Builder/Refiners/SwiftRefiner.cs
+++ src/Kiota.Builder/Refiners/SwiftRefiner.cs
@@ -1,13 +1,22 @@
+using System.Linq;
+using Kiota.Builder.Extensions;
 namespace Kiota.Builder.Refiners {
     public class SwiftRefiner : CommonLanguageRefiner
     {
         public SwiftRefiner(GenerationConfiguration configuration) : base(configuration) {}
         public override void Refine(CodeNamespace generatedCode)
         {
+            CapitalizeNamespacesFirstLetters(generatedCode);
             ReplaceReservedNames(
                 generatedCode,
                 new SwiftReservedNamesProvider(),
                 x => $"{x}_escaped");
+        }
+
+        private static void CapitalizeNamespacesFirstLetters(CodeElement current) {
+            if(current is CodeNamespace currentNamespace)
+                currentNamespace.Name = currentNamespace.Name?.Split('.')?.Select(x => x.ToFirstCharacterUpperCase())?.Aggregate((x, y) => $"{x}.{y}");
+            CrawlTree(current, CapitalizeNamespacesFirstLetters);
         }
     }
 }
