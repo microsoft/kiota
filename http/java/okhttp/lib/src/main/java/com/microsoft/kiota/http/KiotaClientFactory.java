f4c6a94a074a4a42305927daba242c7a5a61c69c
Vincent Biret <vibiret@microsoft.com>
2021-10-27 14:38:43 -0400

    - adds default factories for java http client

--- http/java/okhttp/lib/src/main/java/com/microsoft/kiota/http/KiotaClientFactory.java
+++ http/java/okhttp/lib/src/main/java/com/microsoft/kiota/http/KiotaClientFactory.java
@@ -1,5 +1,9 @@
 package com.microsoft.kiota.http;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 
 /** This class is used to build the HttpClient instance used by the core service. */
@@ -9,8 +13,16 @@ public class KiotaClientFactory {
      * Creates an OkHttpClient Builder with the default configuration and middlewares.
      * @return an OkHttpClient Builder instance.
      */
-    public static OkHttpClient.Builder Create() {
-        return new OkHttpClient.Builder(); //TODO configure the default client options.
-        //TODO add the default middlewares when they are ready
+    public static OkHttpClient.Builder Create(@Nullable final Interceptor[] interceptors) {
+        final OkHttpClient.Builder builder = new OkHttpClient.Builder(); //TODO configure the default client options.
+        final Interceptor[] interceptorsOrDefault = interceptors != null ? interceptors : CreateDefaultInterceptors();
+        for (final Interceptor interceptor : interceptorsOrDefault) {
+            builder.addInterceptor(interceptor);
+        }
+        return builder; 
+    }
+    @Nonnull
+    public static Interceptor[] CreateDefaultInterceptors() {
+        return new Interceptor[] {}; //TODO add the list of default interceptors when they are ready
     }
 }
