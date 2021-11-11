ea509667d7d388e9cc4054bc48082c284dec31ea
Andrew Omondi <andrew.omondi@microsoft.com>
2021-11-11 14:15:30 +0300

    Fix for possible null content

--- http/dotnet/httpclient/src/Middleware/RedirectHandler.cs
+++ http/dotnet/httpclient/src/Middleware/RedirectHandler.cs
@@ -64,7 +64,10 @@ namespace Microsoft.Kiota.Http.HttpClientLibrary.Middleware
                 while(redirectCount < RedirectOption.MaxRedirect)
                 {
                     // Drain response content to free responses.
-                    await response.Content.ReadAsByteArrayAsync();
+                    if(response.Content != null)
+                    {
+                        await response.Content.ReadAsByteArrayAsync();
+                    }
 
                     // general clone request with internal CloneAsync (see CloneAsync for details) extension method
                     var newRequest = await response.RequestMessage.CloneAsync();
