1fcf831d4a8edbdb2bed21e0f6b86a11c664f64c
Darrel Miller <darrel.miller@microsoft.com>
2021-11-14 15:27:06 -0500

    Model with dictionary property

--- tests/Kiota.Builder.IntegrationTests/GenerateSample.cs
+++ tests/Kiota.Builder.IntegrationTests/GenerateSample.cs
@@ -23,12 +23,41 @@ namespace Kiota.Builder.integrationtests
             var backingStoreSuffix = backingStore ? string.Empty : "BackingStore";
             var configuration = new GenerationConfiguration
             {
-                Language = GenerationLanguage.CSharp,
+                Language = language,
                 OpenAPIFilePath = "ToDoApi.yaml",
-                OutputPath = $".\\Generated\\{language}{backingStoreSuffix}",
+                OutputPath = $".\\Generated\\Todo\\{language}{backingStoreSuffix}",
                 UsesBackingStore = backingStore,
             };
             await new KiotaBuilder(logger, configuration).GenerateSDK();
         }
+
+
+        [InlineData(GenerationLanguage.CSharp, false)]
+        [InlineData(GenerationLanguage.Java, false)]
+        [InlineData(GenerationLanguage.TypeScript, false)]
+        [InlineData(GenerationLanguage.Go, false)]
+        [InlineData(GenerationLanguage.Ruby, false)]
+        [InlineData(GenerationLanguage.CSharp, true)]
+        [InlineData(GenerationLanguage.Java, true)]
+        [InlineData(GenerationLanguage.TypeScript, true)]
+        [Theory]
+        public async Task GeneratesModelWithDictionary(GenerationLanguage language, bool backingStore)
+        {
+            var logger = LoggerFactory.Create((builder) => {
+            }).CreateLogger<KiotaBuilder>();
+
+            var backingStoreSuffix = backingStore ? "BackingStore" : string.Empty;
+            var configuration = new GenerationConfiguration
+            {
+                Language = language,
+                OpenAPIFilePath = "ModelWithDictionary.yaml",
+                OutputPath = $".\\Generated\\ModelWithDictionary\\{language}{backingStoreSuffix}",
+                UsesBackingStore = backingStore,
+            };
+            await new KiotaBuilder(logger, configuration).GenerateSDK();
+        }
+
+
+
     }
 }
