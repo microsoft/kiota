ca1110290e2078cce790535e5bf77c8426ea2d25
SilasKenneth <silaskenneth1@gmail.com>
2021-11-25 14:44:28 +0300

    Remove cancellationtoken parameter and refactor code.

--- src/Kiota.Builder/Refiners/PhpRefiner.cs
+++ src/Kiota.Builder/Refiners/PhpRefiner.cs
@@ -11,7 +11,8 @@ namespace Kiota.Builder.Refiners
         
         public override void Refine(CodeNamespace generatedCode)
         {
-            //AddInnerClasses(generatedCode);
+            AddImportsForClassesWithRequestExecutor(generatedCode);
+            RemoveCancellationTokenParameter(generatedCode);
             AddDefaultImports(generatedCode, defaultUsingEvaluators);
             MakeModelPropertiesNullable(generatedCode);
             ReplaceIndexersByMethodsWithParameter(generatedCode, generatedCode, false, "ById");
@@ -26,15 +27,14 @@ namespace Kiota.Builder.Refiners
             AddParsableInheritanceForModelClasses(generatedCode);
             ReplaceBinaryByNativeType(generatedCode, "StreamInterface", "Psr\\Http\\Message", true);
             MoveClassesWithNamespaceNamesUnderNamespace(generatedCode);
-            CorrectCoreType(generatedCode, null, CorrectPropertyType);
-           
+            CorrectCoreType(generatedCode, CorrectMethodType, CorrectPropertyType);
         }
         
         private static readonly AdditionalUsingEvaluator[] defaultUsingEvaluators = { 
             new (x => x is CodeProperty prop && prop.IsOfKind(CodePropertyKind.RequestAdapter),
                 "Microsoft\\Kiota\\Abstractions", "RequestAdapter"),
             new (x => x is CodeMethod method && method.IsOfKind(CodeMethodKind.RequestGenerator),
-                "Microsoft\\Kiota\\Abstractions", "HttpMethod", "RequestInformation", "MiddlewareOption"),
+                "Microsoft\\Kiota\\Abstractions", "HttpMethod", "RequestInformation", "RequestOption"),
             new (x => x is CodeMethod method && method.IsOfKind(CodeMethodKind.RequestExecutor),
                 "Microsoft\\Kiota\\Abstractions", "ResponseHandler"),
             new (x => x is CodeMethod method && method.IsOfKind(CodeMethodKind.Serializer),
@@ -81,6 +81,64 @@ namespace Kiota.Builder.Refiners
                 if(!string.IsNullOrEmpty(currentProperty.DefaultValue))
                     currentProperty.DefaultValue = "[]";
             }
+        }
+
+        private static void CorrectMethodType(CodeMethod method)
+        {
+            if (method.IsOfKind(CodeMethodKind.Deserializer))
+            {
+                method.ReturnType.Name = "array";
+            }
+        }
+
+        private static void AddImportsForClassesWithRequestExecutor(CodeElement codeElement)
+        {
+            var currentClass = codeElement as CodeClass;
+            var containsExecutor = currentClass?.Methods.Any(method => method.IsOfKind(CodeMethodKind.RequestExecutor));
+
+            if (containsExecutor.HasValue && containsExecutor.Value)
+            {
+                var declaration = currentClass.StartBlock as CodeClass.Declaration;
+                declaration?.AddUsings(new CodeUsing()
+                {
+                    Alias = "Promise",
+                    Declaration = new CodeType()
+                    {
+                        IsExternal = true,
+                        IsNullable = false,
+                        Name = "Http\\Promise"
+                    },
+                    Name = "Promise"
+                }, new CodeUsing()
+                {
+                    Alias = "RejectedPromise",
+                    Declaration = new CodeType()
+                    {
+                        IsExternal = true,
+                        IsNullable = false,
+                        Name = "Http\\Promise"
+                    },
+                    Name = "RejectedPromise"
+                }, new CodeUsing()
+                {
+                    Alias = "Exception",
+                    Declaration = new CodeType()
+                    {
+                        IsExternal = true,
+                        IsNullable = false,
+                        Name = ""
+                    },
+                    Name = "Exception"
+                });
+            }
+            CrawlTree(codeElement, AddImportsForClassesWithRequestExecutor);
+        }
+
+        private static void RemoveCancellationTokenParameter(CodeElement codeElement)
+        {
+            var currentMethod = codeElement as CodeMethod;
+            currentMethod?.RemoveParametersByKind(CodeParameterKind.Cancellation);
+            CrawlTree(codeElement, RemoveCancellationTokenParameter);
         }
     }
 }
--- src/Kiota.Builder/Writers/Php/CodeClassDeclarationWriter.cs
+++ src/Kiota.Builder/Writers/Php/CodeClassDeclarationWriter.cs
@@ -15,46 +15,7 @@ namespace Kiota.Builder.Writers.Php
         {
             conventions.WritePhpDocumentStart(writer);
             var parent = codeElement.Parent as CodeClass;
-            // Only import promise if we are having a request executor method from the current class
-            var requestExecutor = parent.GetMethodsOffKind(CodeMethodKind.RequestExecutor).FirstOrDefault();
-            if ( requestExecutor != null)
-            {
-                codeElement.AddUsings(new CodeUsing()
-                {
-                    Alias = "Promise",
-                    Declaration = new CodeType()
-                    {
-                        IsExternal = true,
-                        IsNullable = false,
-                        Name = "Http\\Promise"
-                    },
-                    Name = "Promise"
-                }, new CodeUsing()
-                {
-                    Alias = "RejectedPromise",
-                    Declaration = new CodeType()
-                    {
-                        IsExternal = true,
-                        IsNullable = false,
-                        Name = "Http\\Promise"
-                    },
-                    Name = "RejectedPromise"
-                }, new CodeUsing()
-                {
-                    Alias = "Exception",
-                    Declaration = new CodeType()
-                    {
-                        IsExternal = true,
-                        IsNullable = false,
-                        Name = ""
-                    },
-                    Name = "Exception"
-                });
-            }
             conventions.WriteNamespaceAndImports(codeElement, writer);
-            //TODO: There is bug on creating filenames that makes file class names have multiple dots.
-            // for example class user.LoginRequestBuilder 
-            // instead of class LoginRequestBuilder
             var derivation = (codeElement?.Inherits == null ? string.Empty : $" extends {codeElement.Inherits.Name.ToFirstCharacterUpperCase()}") +
                              (!codeElement.Implements.Any() ? string.Empty : $" implements {codeElement.Implements.Select(x => x.Name).Aggregate((x,y) => x + ", " + y)}");
             writer.WriteLine($"class {codeElement.Name.Split('.').Last().ToFirstCharacterUpperCase()}{derivation} ");
