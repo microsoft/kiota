58036d5ea7e6420eb504b2e949c3de57a66559b9
Vincent Biret <vibiret@microsoft.com>
2021-12-02 09:47:17 -0500

    - fixes a bug where pipeline would not be thread safe

--- http/go/nethttp/pipeline_test.go
+++ http/go/nethttp/pipeline_test.go
@@ -7,10 +7,10 @@ import (
 
 type TestMiddleware struct{}
 
-func (middleware TestMiddleware) Intercept(pipeline Pipeline, req *http.Request) (*http.Response, error) {
+func (middleware TestMiddleware) Intercept(pipeline Pipeline, middlewareIndex int, req *http.Request) (*http.Response, error) {
 	req.Header.Add("test", "test-header")
 
-	return pipeline.Next(req)
+	return pipeline.Next(req, middlewareIndex)
 }
 
 func TestCanInterceptRequests(t *testing.T) {
@@ -23,5 +23,26 @@ func TestCanInterceptRequests(t *testing.T) {
 
 	if expect != got {
 		t.Errorf("Expected: %v, but received: %v", expect, got)
+	}
+}
+
+func TestCanInterceptMultipleRequests(t *testing.T) {
+	transport := NewCustomTransport(&TestMiddleware{})
+	client := &http.Client{Transport: transport}
+	resp, _ := client.Get("https://example.com")
+
+	expect := "test-header"
+	got := resp.Request.Header.Get("test")
+
+	if expect != got {
+		t.Errorf("Expected: %v, but received: %v", expect, got)
+	}
+
+	resp2, _ := client.Get("https://example.com")
+
+	got2 := resp2.Request.Header.Get("test")
+
+	if expect != got2 {
+		t.Errorf("Expected: %v, but received: %v", expect, got2)
 	}
 }
