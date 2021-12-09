dc9b6d38de5c2c014481e7310fa079304bd81373
samwelkanda <samwelkanda@gmail.com>
2021-12-09 18:34:58 +0300

    Add anonymous authentication provider

---
+++ abstractions/python/kiota/abstractions/authentication/anonymous_authentication_provider.py
@@ -0,0 +1,18 @@
+from ..request_information import RequestInformation
+
+from .authentication_provider import AuthenticationProvider
+
+class AnonymousAuthenticationProvider(AuthenticationProvider):
+    """This authentication provider does not perform any authentication
+
+    Args:
+        AuthenticationProvider (ABC): The abstract base class that this class implements
+    """
+    
+    async def authenticate_request(request: RequestInformation) -> None:
+        """Authenticates the provided request information
+
+        Args:
+            request (RequestInformation): Request information object
+        """
+        return None
