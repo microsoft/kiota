e7ca538527e2cc97fbae88621c1f156d45475e8e
samwelkanda <samwelkanda@gmail.com>
2022-01-24 15:59:11 +0300

    Add in memory backing store factory

---
+++ abstractions/python/kiota/abstractions/store/in_memory_backing_store_factory.py
@@ -0,0 +1,10 @@
+from .backing_store import BackingStore
+from .backing_store_factory import BackingStoreFactory
+from .in_memory_backing_store import InMemoryBackingStore
+
+
+class InMemoryBackingStoreFactory(BackingStoreFactory):
+    """This class is used to create instances of InMemoryBackingStore
+    """
+    def create_backing_store(self) -> BackingStore:
+        return InMemoryBackingStore()
