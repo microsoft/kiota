2362a291bb0e1924588454c5f798a1609848ce85
samwelkanda <samwelkanda@gmail.com>
2022-01-24 13:37:29 +0300

    Update authentication classes

--- abstractions/python/kiota/abstractions/authentication/authentication_provider.py
+++ abstractions/python/kiota/abstractions/authentication/authentication_provider.py
@@ -1,13 +1,12 @@
 from abc import ABC, abstractmethod
-from typing import Coroutine
 
 from ..request_information import RequestInformation
+
 
 class AuthenticationProvider(ABC):
     """
     Authenticates the application request
     """
-    
     @abstractmethod
     async def authenticate_request(self, request: RequestInformation) -> None:
         """Authenticates the application request
--- abstractions/python/kiota/abstractions/authentication/base_bearer_token_authentication_provider.py
+++ abstractions/python/kiota/abstractions/authentication/base_bearer_token_authentication_provider.py
@@ -1,10 +1,7 @@
-from abc import abstractmethod
 from typing import Union
 
 from ..request_information import RequestInformation
 from .authentication_provider import AuthenticationProvider
-
-AUTHORIZATION_HEADER = "Authorization"
 
 
 class BaseBearerAuthenticationProvider(AuthenticationProvider):
@@ -13,25 +10,33 @@ class BaseBearerAuthenticationProvider(AuthenticationProvider):
     Args:
         AuthenticationProvider (ABC):The abstract base class that this class implements
     """
+    AUTHORIZATION_HEADER = "Authorization"
+
     async def authenticate_request(self, request: RequestInformation) -> None:
-        """Authenticates the provided RequestInformation instance using the provided authorization token
+        """Authenticates the provided RequestInformation instance using the provided
+        authorization token
 
         Args:
             request (RequestInformation): Request information object
         """
         if not request:
             raise Exception("Request cannot be null")
-        if AUTHORIZATION_HEADER in request.headers:
+        if self.AUTHORIZATION_HEADER in request.headers:
             token = await self.get_authorization_token(request)
             if not token:
                 raise Exception("Failed to get an authorization token")
-            
-            request.headers.update({f'{AUTHORIZATION_HEADER}': f'Bearer {token}'})
-    
-    async def get_authorization_token(self, request: RequestInformation) -> Union[str, None]:
+
+            request.headers.update({f'{self.AUTHORIZATION_HEADER}': f'Bearer {token}'})
+
+    async def get_authorization_token(self, request: RequestInformation) -> str:
         """Gets the authorization token for the given request.
 
         Args:
-            request (RequestInformation): An instance of RequestInformation from which to obtain the token
+            request (RequestInformation): An instance of RequestInformation from which to obtain
+            the token
+
+        Returns:
+            str: Access token to use for the request
         """
         raise NotImplementedError
+
