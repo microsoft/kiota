039681f6555ffd253795402f7fc6f50583c5ef52
samwelkanda <samwelkanda@gmail.com>
2022-01-24 15:56:07 +0300

    Add backed model base class

---
+++ abstractions/python/kiota/abstractions/store/backed_model.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+from .backing_store import BackingStore
+
+
+class BackedModel(ABC):
+    """Defines the contracts for a model that is backed by a store.
+    """
+    @abstractmethod
+    def get_backing_store(self) -> BackingStore:
+        """Gets the store that is backing the model
+
+        Returns:
+            BackingStore: The store backing the model
+        """
+        pass
