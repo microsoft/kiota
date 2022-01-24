c1bb8e7a7fe9b26231268a3f0f8703ad35cc1758
samwelkanda <samwelkanda@gmail.com>
2022-01-24 16:17:38 +0300

    Add function to get path parameters

---
+++ abstractions/python/kiota/abstractions/get_path_parameters.py
@@ -0,0 +1,13 @@
+from typing import Any, Dict, Optional, Union
+
+from .request_information import RequestInformation
+
+
+def get_path_parameters(self, parameters: Union[Dict[str, Any], Optional[str]]) -> Dict[str, Any]:
+    result: Dict[str, Any] = {}
+    if isinstance(parameters, str):
+        result[RequestInformation.RAW_URL_KEY] = parameters
+    elif isinstance(parameters, dict):
+        for key, val in parameters.items():
+            result[key] = val
+    return result
