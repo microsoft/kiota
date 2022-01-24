6e9c77dd2676180820fa6b2a40eddf47ddacbaf5
samwelkanda <samwelkanda@gmail.com>
2022-01-24 16:16:15 +0300

    Add request option base class

---
+++ abstractions/python/kiota/abstractions/request_option.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+
+class RequestOption(ABC):
+    """Represents a request option
+    """
+    @abstractmethod
+    def get_key(self) -> str:
+        """Gets the option key for when adding it to a request. Must be unique
+
+        Returns:
+            str: The option key
+        """
+        pass
