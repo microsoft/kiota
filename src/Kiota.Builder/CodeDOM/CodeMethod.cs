4bbef382df2a73acbb4fc6f9b36050f5d47229c1
Vincent Biret <vibiret@microsoft.com>
2022-02-01 15:30:30 -0500

    - adds support for builder getting error mappings
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Kiota.Builder/CodeDOM/CodeMethod.cs
+++ src/Kiota.Builder/CodeDOM/CodeMethod.cs
@@ -2,122 +2,126 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Kiota.Builder
+namespace Kiota.Builder;
+
+public enum CodeMethodKind
 {
-    public enum CodeMethodKind
-    {
-        Custom,
-        IndexerBackwardCompatibility,
-        RequestExecutor,
-        RequestGenerator,
-        Serializer,
-        Deserializer,
-        Constructor,
-        Getter,
-        Setter,
-        ClientConstructor,
-        RequestBuilderBackwardCompatibility,
-        RequestBuilderWithParameters,
-        RawUrlConstructor,
-        NullCheck,
+    Custom,
+    IndexerBackwardCompatibility,
+    RequestExecutor,
+    RequestGenerator,
+    Serializer,
+    Deserializer,
+    Constructor,
+    Getter,
+    Setter,
+    ClientConstructor,
+    RequestBuilderBackwardCompatibility,
+    RequestBuilderWithParameters,
+    RawUrlConstructor,
+    NullCheck,
+}
+public enum HttpMethod {
+    Get,
+    Post,
+    Patch,
+    Put,
+    Delete,
+    Options,
+    Connect,
+    Head,
+    Trace
+}
+
+public class CodeMethod : CodeTerminal, ICloneable, IDocumentedElement
+{
+    public HttpMethod? HttpMethod {get;set;}
+    public CodeMethodKind MethodKind {get;set;} = CodeMethodKind.Custom;
+    public string ContentType { get; set; }
+    public AccessModifier Access {get;set;} = AccessModifier.Public;
+    private CodeTypeBase returnType;
+    public CodeTypeBase ReturnType {get => returnType;set {
+        EnsureElementsAreChildren(value);
+        returnType = value;
+    }}
+    private readonly List<CodeParameter> parameters = new ();
+    public void RemoveParametersByKind(params CodeParameterKind[] kinds) {
+        parameters.RemoveAll(p => p.IsOfKind(kinds));
     }
-    public enum HttpMethod {
-        Get,
-        Post,
-        Patch,
-        Put,
-        Delete,
-        Options,
-        Connect,
-        Head,
-        Trace
+    public IEnumerable<CodeParameter> Parameters { get => parameters; }
+    public bool IsStatic {get;set;} = false;
+    public bool IsAsync {get;set;} = true;
+    public string Description {get; set;}
+    /// <summary>
+    /// The property this method accesses to when it's a getter or setter.
+    /// </summary>
+    public CodeProperty AccessedProperty { get; set; }
+    public bool IsOfKind(params CodeMethodKind[] kinds) {
+        return kinds?.Contains(MethodKind) ?? false;
+    }
+    public bool IsAccessor { 
+        get => IsOfKind(CodeMethodKind.Getter, CodeMethodKind.Setter);
+    }
+    public bool IsSerializationMethod {
+        get => IsOfKind(CodeMethodKind.Serializer, CodeMethodKind.Deserializer);
+    }
+    public List<string> SerializerModules { get; set; }
+    public List<string> DeserializerModules { get; set; }
+    /// <summary>
+    /// Indicates whether this method is an overload for another method.
+    /// </summary>
+    public bool IsOverload { get { return OriginalMethod != null; } }
+    /// <summary>
+    /// Provides a reference to the original method that this method is an overload of.
+    /// </summary>
+    public CodeMethod OriginalMethod { get; set; }
+    /// <summary>
+    /// The original indexer codedom element this method replaces when it is of kind IndexerBackwardCompatibility.
+    /// </summary>
+    public CodeIndexer OriginalIndexer { get; set; }
+    /// <summary>
+    /// The base url for every request read from the servers property on the description.
+    /// Only provided for constructor on Api client
+    /// </summary>
+    public string BaseUrl { get; set; }
+    /// <summary>
+    /// Mapping of the error code and response types for this method.
+    /// </summary>
+    public Dictionary<string, CodeTypeBase> ErrorMappings { get; set; } = new ();
+
+    public object Clone()
+    {
+        var method = new CodeMethod {
+            MethodKind = MethodKind,
+            ReturnType = ReturnType?.Clone() as CodeTypeBase,
+            Name = Name.Clone() as string,
+            HttpMethod = HttpMethod,
+            IsAsync = IsAsync,
+            Access = Access,
+            IsStatic = IsStatic,
+            Description = Description?.Clone() as string,
+            ContentType = ContentType?.Clone() as string,
+            BaseUrl = BaseUrl?.Clone() as string,
+            AccessedProperty = AccessedProperty,
+            SerializerModules = SerializerModules == null ? null : new (SerializerModules),
+            DeserializerModules = DeserializerModules == null ? null : new (DeserializerModules),
+            OriginalMethod = OriginalMethod,
+            Parent = Parent,
+            OriginalIndexer = OriginalIndexer,
+            ErrorMappings = ErrorMappings == null ? null : new (ErrorMappings)
+        };
+        if(Parameters?.Any() ?? false)
+            method.AddParameter(Parameters.Select(x => x.Clone() as CodeParameter).ToArray());
+        return method;
     }
 
-    public class CodeMethod : CodeTerminal, ICloneable, IDocumentedElement
+    public void AddParameter(params CodeParameter[] methodParameters)
     {
-        public HttpMethod? HttpMethod {get;set;}
-        public CodeMethodKind MethodKind {get;set;} = CodeMethodKind.Custom;
-        public string ContentType { get; set; }
-        public AccessModifier Access {get;set;} = AccessModifier.Public;
-        private CodeTypeBase returnType;
-        public CodeTypeBase ReturnType {get => returnType;set {
-            EnsureElementsAreChildren(value);
-            returnType = value;
-        }}
-        private readonly List<CodeParameter> parameters = new ();
-        public void RemoveParametersByKind(params CodeParameterKind[] kinds) {
-            parameters.RemoveAll(p => p.IsOfKind(kinds));
-        }
-        public IEnumerable<CodeParameter> Parameters { get => parameters; }
-        public bool IsStatic {get;set;} = false;
-        public bool IsAsync {get;set;} = true;
-        public string Description {get; set;}
-        /// <summary>
-        /// The property this method accesses to when it's a getter or setter.
-        /// </summary>
-        public CodeProperty AccessedProperty { get; set; }
-        public bool IsOfKind(params CodeMethodKind[] kinds) {
-            return kinds?.Contains(MethodKind) ?? false;
-        }
-        public bool IsAccessor { 
-            get => IsOfKind(CodeMethodKind.Getter, CodeMethodKind.Setter);
-        }
-        public bool IsSerializationMethod {
-            get => IsOfKind(CodeMethodKind.Serializer, CodeMethodKind.Deserializer);
-        }
-        public List<string> SerializerModules { get; set; }
-        public List<string> DeserializerModules { get; set; }
-        /// <summary>
-        /// Indicates whether this method is an overload for another method.
-        /// </summary>
-        public bool IsOverload { get { return OriginalMethod != null; } }
-        /// <summary>
-        /// Provides a reference to the original method that this method is an overload of.
-        /// </summary>
-        public CodeMethod OriginalMethod { get; set; }
-        /// <summary>
-        /// The original indexer codedom element this method replaces when it is of kind IndexerBackwardCompatibility.
-        /// </summary>
-        public CodeIndexer OriginalIndexer { get; set; }
-        /// <summary>
-        /// The base url for every request read from the servers property on the description.
-        /// Only provided for constructor on Api client
-        /// </summary>
-        public string BaseUrl { get; set; }
-
-        public object Clone()
-        {
-            var method = new CodeMethod {
-                MethodKind = MethodKind,
-                ReturnType = ReturnType?.Clone() as CodeTypeBase,
-                Name = Name.Clone() as string,
-                HttpMethod = HttpMethod,
-                IsAsync = IsAsync,
-                Access = Access,
-                IsStatic = IsStatic,
-                Description = Description?.Clone() as string,
-                ContentType = ContentType?.Clone() as string,
-                BaseUrl = BaseUrl?.Clone() as string,
-                AccessedProperty = AccessedProperty,
-                SerializerModules = SerializerModules == null ? null : new (SerializerModules),
-                DeserializerModules = DeserializerModules == null ? null : new (DeserializerModules),
-                OriginalMethod = OriginalMethod,
-                Parent = Parent,
-                OriginalIndexer = OriginalIndexer,
-            };
-            if(Parameters?.Any() ?? false)
-                method.AddParameter(Parameters.Select(x => x.Clone() as CodeParameter).ToArray());
-            return method;
-        }
-
-        public void AddParameter(params CodeParameter[] methodParameters)
-        {
-            if(methodParameters == null || methodParameters.Any(x => x == null))
-                throw new ArgumentNullException(nameof(methodParameters));
-            if(!methodParameters.Any())
-                throw new ArgumentOutOfRangeException(nameof(methodParameters));
-            EnsureElementsAreChildren(methodParameters);
-            parameters.AddRange(methodParameters);
-        }
+        if(methodParameters == null || methodParameters.Any(x => x == null))
+            throw new ArgumentNullException(nameof(methodParameters));
+        if(!methodParameters.Any())
+            throw new ArgumentOutOfRangeException(nameof(methodParameters));
+        EnsureElementsAreChildren(methodParameters);
+        parameters.AddRange(methodParameters);
     }
 }
