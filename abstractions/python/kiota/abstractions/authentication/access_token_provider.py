1bef8a82df4b9c9e285d9221ced6dd17e6592f74
samwelkanda <samwelkanda@gmail.com>
2022-02-01 21:46:07 +0300

    Add getter method for allowed host validator

--- abstractions/python/kiota/abstractions/authentication/access_token_provider.py
+++ abstractions/python/kiota/abstractions/authentication/access_token_provider.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from .allowed_hosts_validator import AllowedHostsValidator
+
 
 class AccessTokenProvider(ABC):
     """Defines a contract for obtaining access tokens for a given url.
@@ -13,5 +15,14 @@ class AccessTokenProvider(ABC):
             uri (str): The target URI to get an access token for.
         Returns:
             str: The access token to use for the request.
+        """
+        pass
+
+    @abstractmethod
+    def get_allowed_hosts_validator(self) -> AllowedHostsValidator:
+        """Retrieves the allowed hosts validator.
+
+        Returns:
+            AllowedHostsValidator: The allowed hosts validator.
         """
         pass
