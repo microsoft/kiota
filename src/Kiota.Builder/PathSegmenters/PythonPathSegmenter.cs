1056db417579f7ad059ab9db213c8bb5bbc3fde3
samwelkanda <samwelkanda@gmail.com>
2022-02-08 16:32:21 +0300

    Add path segmenter for python

---
+++ src/Kiota.Builder/PathSegmenters/PythonPathSegmenter.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Kiota.Builder.Extensions;
+
+namespace Kiota.Builder {
+    public class PythonPathSegmenter : CommonPathSegmenter
+    {
+        public PythonPathSegmenter(string rootPath, string clientNamespaceName) : base (rootPath, clientNamespaceName) { }
+        public override string FileSuffix =>  ".py";
+        public override string NormalizeFileName(CodeElement currentElement) {
+            return currentElement switch
+            {
+                CodeNamespace => "index",
+                _ => GetDefaultFileName(currentElement),
+            };
+        }
+        private static string GetDefaultFileName(CodeElement currentElement) => GetLastFileNameSegment(currentElement).ToFirstCharacterLowerCase();
+        public override string NormalizeNamespaceSegment(string segmentName) => segmentName.ToFirstCharacterLowerCase();
+    }
+}
