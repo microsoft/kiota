c5b990e8a7720645a6ec45efade6ffa3972ca7a6
samwelkanda <samwelkanda@gmail.com>
2022-02-10 17:22:56 +0300

    Add json parse node factory

---
+++ serialization/python/json/serialization/json_parse_node_factory.py
@@ -0,0 +1,39 @@
+import json
+from io import BytesIO
+
+from kiota.abstractions import ParseNode, ParseNodeFactory
+
+from .json_parse_node import JsonParseNode
+
+
+class JsonParseNodeFactory(ParseNodeFactory):
+    """Factory that is used to create JsonParseNodes.
+    """
+    def get_valid_content_type(self) -> str:
+        """Returns the content type this factory's parse nodes can deserialize
+        Returns:
+            str: The content type to be deserialized
+        """
+        return 'application/json'
+
+    def get_root_parse_node(self, content_type: str,
+                            content: BytesIO) -> ParseNode:
+        """Creates a ParseNode from the given binary stream and content type
+        Args:
+            content_type (str): The content type of the binary stream
+            content (BytesIO): The array buffer to read from
+        Returns:
+            ParseNode: A ParseNode that can deserialize the given binary stream
+        """
+        if not content_type:
+            raise TypeError("Content Type cannot be null")
+        if self.get_valid_content_type() != content_type:
+            raise TypeError(
+                f"Expected {self.get_valid_content_type()} as content type")
+
+        if not content:
+            raise TypeError("Content cannot be null")
+
+        content_as_str = content.decode()
+        content_as_json = json.loads(content_as_str)
+        return JsonParseNode(content_as_json)
