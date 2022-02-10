992d206f09c90abc91c021aee0d2c48ea58e2e1e
Vincent Biret <vibiret@microsoft.com>
2022-02-10 10:35:38 -0800

    Update abstractions/typescript/src/nativeResponseHandler.ts
    Co-authored-by: Nikitha Chettiar <nikithauc@gmail.com>

--- abstractions/typescript/src/nativeResponseHandler.ts
+++ abstractions/typescript/src/nativeResponseHandler.ts
@@ -5,7 +5,7 @@ import { Parsable } from "./serialization";
 export class NativeResponseHandler implements ResponseHandler {
   /** Native response object as returned by the core service */
   public value?: any;
-  /** The error mappings for the response to use when deserializing failed responses bodies. Where an error code like 401 applies specifically to that status code, a class code like 4XX applies to all status codes within the range if an the specific error code is not present. */
+  /** The error mappings for the response to use when deserializing failed responses bodies. Where an error code like 401 applies specifically to that status code, a class code like 4XX applies to all status codes within the range if a specific error code is not present. */
   public errorMappings: Record<string, new () => Parsable> | undefined
   public handleResponseAsync<NativeResponseType, ModelType>(
     response: NativeResponseType,
