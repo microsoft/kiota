0ac97f376b76b265bcad85b7775df036578e2ee0
Caleb Kiage <caleb.kiage@gmail.com>
2022-02-11 16:42:15 +0300

    Remove unnecessary extension method

--- src/Kiota.Builder/Extensions/CodeParametersEnumerableExtensions.cs
+++ src/Kiota.Builder/Extensions/CodeParametersEnumerableExtensions.cs
@@ -6,10 +6,5 @@ namespace Kiota.Builder {
         public static CodeParameter OfKind(this IEnumerable<CodeParameter> parameters, CodeParameterKind kind) {
             return parameters.FirstOrDefault(x => x.IsOfKind(kind));
         }
-
-        public static bool IsOfKind(this CodeParameter parameter, CodeParameterKind kind)
-        {
-            return parameter.ParameterKind == kind;
-        }
     }
 }
