efb2a5cbed6f5226dd6da403a066c2c6f4f58594
samwelkanda <samwelkanda@gmail.com>
2022-02-14 16:49:25 +0300

    Update path segmenter for python files

--- src/Kiota.Builder/PathSegmenters/PythonPathSegmenter.cs
+++ src/Kiota.Builder/PathSegmenters/PythonPathSegmenter.cs
@@ -19,3 +19,25 @@ namespace Kiota.Builder {
         public override string NormalizeNamespaceSegment(string segmentName) => segmentName.ToFirstCharacterLowerCase();
     }
 }
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Kiota.Builder.Extensions;
+
+namespace Kiota.Builder {
+    public class PythonPathSegmenter : CommonPathSegmenter
+    {
+        public PythonPathSegmenter(string rootPath, string clientNamespaceName) : base(rootPath, clientNamespaceName) { }
+        public override IEnumerable<string> GetAdditionalSegment(CodeElement currentElement, string fileName)
+        {
+            return currentElement switch
+            {
+                CodeNamespace => new string[] { fileName },// We put barrels inside namespace folders
+                _ => Enumerable.Empty<string>(),
+            };
+        }
+        public override string FileSuffix => ".py";
+        public override string NormalizeFileName(CodeElement currentElement) => GetLastFileNameSegment(currentElement).ToSnakeCase();
+        public override string NormalizeNamespaceSegment(string segmentName) => segmentName.ToSnakeCase();
+    }
+}
