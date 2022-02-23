69dd41576596087d479922957bb8af178e8b8c59
Vincent Biret <vibiret@microsoft.com>
2022-02-23 09:40:57 -0500

    - code linting
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Kiota.Builder/Writers/Ruby/CodeBlockEndWriter.cs
+++ src/Kiota.Builder/Writers/Ruby/CodeBlockEndWriter.cs
@@ -1,14 +1,14 @@
 namespace Kiota.Builder.Writers.Ruby {
     public class CodeBlockEndWriter : BaseElementWriter<BlockEnd, RubyConventionService>
     {
-        private string end = "end";
+        private const string End = "end";
         public CodeBlockEndWriter(RubyConventionService conventionService):base(conventionService){}
         public override void WriteCodeElement(BlockEnd codeElement, LanguageWriter writer)
         {
             if(codeElement.Parent is CodeEnum) return;
-            writer.CloseBlock(end);
+            writer.CloseBlock(End);
             if(codeElement?.Parent is CodeClass codeClass && codeClass.Parent is CodeNamespace) {
-                writer.CloseBlock(end);
+                writer.CloseBlock(End);
             }
         }
     }
