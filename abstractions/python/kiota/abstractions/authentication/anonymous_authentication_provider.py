b7c770a92355c22895c453f59facdb49694c1189
samwelkanda <samwelkanda@gmail.com>
2022-02-28 19:19:32 +0300

    Revert unnecessary changes to anonymous auth provider

--- abstractions/python/kiota/abstractions/authentication/anonymous_authentication_provider.py
+++ abstractions/python/kiota/abstractions/authentication/anonymous_authentication_provider.py
@@ -1,17 +1,17 @@
-from abc import ABC, abstractmethod
-
 from ..request_information import RequestInformation
+from .authentication_provider import AuthenticationProvider
 
 
-class AnonymousAuthenticationProvider(ABC):
+class AnonymousAuthenticationProvider(AuthenticationProvider):
+    """This authentication provider does not perform any authentication
+
+    Args:
+        AuthenticationProvider (ABC): The abstract base class that this class implements
     """
-    Base class for providing authentication information for a request.
-    """
-    @abstractmethod
     async def authenticate_request(self, request: RequestInformation) -> None:
-        """Authenticates the application request
+        """Authenticates the provided request information
 
         Args:
-            request (RequestInformation): The request to authenticate
+            request (RequestInformation): Request information object
         """
         return
