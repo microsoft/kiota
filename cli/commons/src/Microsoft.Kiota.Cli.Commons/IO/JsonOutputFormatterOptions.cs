91b1c6dad6f0c440b0092b1a2837fe81e1b08288
Caleb Kiage <caleb.kiage@gmail.com>
2022-03-03 16:05:39 +0300

    Add output options to allow changing formatter behavior

---
+++ cli/commons/src/Microsoft.Kiota.Cli.Commons/IO/JsonOutputFormatterOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.Kiota.Cli.Commons.IO;
+
+/// <summary>
+/// The JSON output formatter options
+/// </summary>
+public class JsonOutputFormatterOptions : OutputFormatterOptions
+{
+    /// <summary>
+    /// Gets or sets a value that defines whether JSON should use pretty printing. By
+    /// default, JSON is serialized without any extra white space.
+    /// </summary>
+    public bool OutputIndented { get; set; } = true
+}
