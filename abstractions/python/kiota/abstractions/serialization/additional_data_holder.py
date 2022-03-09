e952d8a63e813b9d533320ab116c481e0ab1f53e
samwelkanda <samwelkanda@gmail.com>
2022-03-09 17:26:13 +0300

    Fix type checking issues with property methods

--- abstractions/python/kiota/abstractions/serialization/additional_data_holder.py
+++ abstractions/python/kiota/abstractions/serialization/additional_data_holder.py
@@ -1,20 +1,21 @@
 from abc import ABC, abstractmethod
 from typing import Any, Dict
 
+
 class AdditionalDataHolder(ABC):
     """Defines a contract for models that can hold additional data besides the described properties.
     """
-    @property
+    @property #type:ignore
     @abstractmethod
     def additional_data(self) -> Dict[str, Any]:
         """Stores the additional data for this object that did not belong to the properties.
-        
+
         Returns:
             Dict[str, Any]: The additional data for this object
         """
         pass
-    
-    @additional_data.setter
+
+    @additional_data.setter #type:ignore
     @abstractmethod
     def additional_data(self, value):
         pass
