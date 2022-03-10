8ad2c737dd659f33d3ff8485cdde07d439f86d61
Caleb Kiage <caleb.kiage@gmail.com>
2022-03-10 18:24:30 +0300

    Add async FilterOutputAsync function

--- cli/commons/src/Microsoft.Kiota.Cli.Commons/IO/IOutputFilter.cs
+++ cli/commons/src/Microsoft.Kiota.Cli.Commons/IO/IOutputFilter.cs
@@ -10,8 +10,9 @@ public interface IOutputFilter
     /// </summary>
     /// <param name="content">Stream content to filter</param>
     /// <param name="query">Query to run against the content</param>
+    /// <param name="cancellationToken">The cancellation token</param>
     /// <returns>A filtered stream</returns>
-    Stream FilterOutput(Stream content, string query);
+    Task<Stream> FilterOutputAsync(Stream content, string query, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Run a filter on string content based on a query. The query format is determined by the implementation
