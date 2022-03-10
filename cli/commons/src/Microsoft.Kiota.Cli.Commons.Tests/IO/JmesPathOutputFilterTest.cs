4f226c9e7c3b3b08505dfe91c70538817e4ddd2d
Caleb Kiage <caleb.kiage@gmail.com>
2022-03-10 18:52:49 +0300

    Update output filter test

--- cli/commons/src/Microsoft.Kiota.Cli.Commons.Tests/IO/JmesPathOutputFilterTest.cs
+++ cli/commons/src/Microsoft.Kiota.Cli.Commons.Tests/IO/JmesPathOutputFilterTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.CommandLine;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 using DevLab.JmesPath;
 using Microsoft.Kiota.Cli.Commons.IO;
 using Moq;
@@ -23,6 +24,25 @@ public class JmesPathOutputFilterTest
             var result = filter.FilterOutput("{\"a\": 1, \"b\": true}", "a");
 
             Assert.Equal("1", result);
+        }
+    }
+
+    public class FilterOutputAsyncFunction_Should
+    {
+        [Fact]
+        public async Task Call_JmesPath_Transform_Function()
+        {
+            var jmesPath = new JmesPath();
+            var filter = new JmesPathOutputFilter(jmesPath);
+            var buffer = Encoding.ASCII.GetBytes("{\"a\": 1, \"b\": true}");
+            using var ms = new MemoryStream(buffer);
+
+            using var result = await filter.FilterOutputAsync(ms, "a");
+
+            using var reader = new StreamReader(result);
+            var str = await reader.ReadToEndAsync();
+
+            Assert.Equal("1", str);
         }
     }
 }
