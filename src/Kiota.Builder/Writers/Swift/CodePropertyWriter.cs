2c5e0e03948161252ffecc7b341069d5ffb47d51
Vincent Biret <vibiret@microsoft.com>
2022-03-25 10:26:29 -0400

    - fixes a bug where swift would not add access modifier to classes
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Kiota.Builder/Writers/Swift/CodePropertyWriter.cs
+++ src/Kiota.Builder/Writers/Swift/CodePropertyWriter.cs
@@ -1,19 +1,18 @@
 using System;
 using Kiota.Builder.Extensions;
 
-namespace Kiota.Builder.Writers.Swift {
-    public class CodePropertyWriter : BaseElementWriter<CodeProperty, SwiftConventionService>
+namespace Kiota.Builder.Writers.Swift;
+public class CodePropertyWriter : BaseElementWriter<CodeProperty, SwiftConventionService>
+{
+    public CodePropertyWriter(SwiftConventionService conventionService) : base(conventionService) {}
+    public override void WriteCodeElement(CodeProperty codeElement, LanguageWriter writer)
     {
-        public CodePropertyWriter(SwiftConventionService conventionService) : base(conventionService) {}
-        public override void WriteCodeElement(CodeProperty codeElement, LanguageWriter writer)
-        {
-            var propertyName = codeElement.Access == AccessModifier.Public ? codeElement.Name.ToFirstCharacterUpperCase() : codeElement.Name.ToFirstCharacterLowerCase();
-            var returnType = conventions.GetTypeString(codeElement.Type, codeElement.Parent);
-            switch(codeElement.Kind) {
-                default:
-                    writer.WriteLine($"var {propertyName}: {returnType}?");
-                break;
-            }
+        var returnType = conventions.GetTypeString(codeElement.Type, codeElement.Parent);
+        var accessModifier = conventions.GetAccessModifier(codeElement.Access);
+        switch(codeElement.Kind) {
+            default:
+                writer.WriteLine($"{accessModifier} var {codeElement.Name.ToFirstCharacterLowerCase()}: {returnType}");
+            break;
         }
     }
 }
