7bc9164290a730b211120051d5528afad0258517
samwelkanda <samwelkanda@gmail.com>
2022-04-07 16:30:59 +0300

    Update error mapping type to use parsablefactory

--- abstractions/python/kiota/abstractions/response_handler.py
+++ abstractions/python/kiota/abstractions/response_handler.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Callable, Dict, Optional, TypeVar
 
-from .serialization import Parsable
+from .serialization import Parsable, ParsableFactory
 
 NativeResponseType = TypeVar("NativeResponseType")
 ModelType = TypeVar("ModelType")
@@ -12,13 +12,13 @@ class ResponseHandler(ABC):
     """
     @abstractmethod
     async def handle_response_async(
-        self, response: NativeResponseType, error_map: Dict[str, Optional[Callable[[], Parsable]]]
+        self, response: NativeResponseType, error_map: Dict[str, Optional[ParsableFactory]]
     ) -> ModelType:
         """Callback method that is invoked when a response is received.
 
         Args:
             response (NativeResponseType): The type of the native response object.
-            error_map (Dict[str, Optional[Callable[[], Parsable]]]): the error dict to use
+            error_map (Dict[str, Optional[ParsableFactory]]): the error dict to use
             in case of a failed request.
 
         Returns:
