c5207ef5deeca2471f4868b4930f93b5ca002f7d
samwelkanda <samwelkanda@gmail.com>
2022-04-27 15:39:00 +0300

    Add writer for code block ends

---
+++ src/Kiota.Builder/Writers/Python/CodeBlockEndWriter.cs
@@ -0,0 +1,10 @@
+namespace Kiota.Builder.Writers.Python {
+    public class CodeBlockEndWriter : ICodeElementWriter<BlockEnd>
+    {
+        public void WriteCodeElement(BlockEnd codeElement, LanguageWriter writer)
+        {
+            if(codeElement.Parent is CodeNamespace) return;
+            writer.CloseBlock("");
+        }
+    }
+}
