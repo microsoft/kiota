ca29e875830935c3773bf2b59b0469707f593702
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2022-05-17 17:35:12 +0300

    Add paging extension

---
+++ tests/Kiota.Builder.Tests/OpenApiExtensions/OpenApiPagingExtensionsTests.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using Kiota.Builder.OpenApiExtensions;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Writers;
+using Xunit;
+
+namespace Kiota.Builder.Tests.OpenApiExtensions;
+
+public class OpenApiPagingExtensionsTests
+{
+    [Fact]
+    public void ExtensionNameMatchesExpected()
+    {
+        // Arrange
+        OpenApiPagingExtension extension = new();
+
+        // Act
+        string name = OpenApiPagingExtension.Name;
+        var expectedName = "x-ms-pageable";
+
+        // Assert
+        Assert.Equal(expectedName, name);
+    }
+
+    [Fact]
+    public void WritesNothingWhenNoValues()
+    {
+        // Arrange
+        OpenApiPagingExtension extension = new();
+        using TextWriter sWriter = new StringWriter();
+        OpenApiJsonWriter writer = new(sWriter);
+
+        // Act
+        extension.Write(writer, OpenApiSpecVersion.OpenApi3_0);
+        var result = sWriter.ToString();
+
+        // Assert
+        Assert.Equal("value", extension.ItemName);
+        Assert.Equal("nextLink", extension.NextLinkName);
+        Assert.Null(extension.OperationName);
+    }
+    [Fact]
+    public void WritesPagingInfo()
+    {
+        // Arrange
+        OpenApiPagingExtension extension = new();
+        extension.NextLinkName = "nextLink";
+        extension.OperationName = "usersGet";
+        using TextWriter sWriter = new StringWriter();
+        OpenApiJsonWriter writer = new(sWriter);
+
+        // Act
+        extension.Write(writer, OpenApiSpecVersion.OpenApi3_0);
+        var result = sWriter.ToString();
+
+        // Assert
+        Assert.Contains("value", result);
+        Assert.Contains("itemName\": \"value", result);
+        Assert.Contains("nextLinkName\": \"nextLink", result);
+        Assert.Contains("operationName\": \"usersGet", result);
+    }
+}
