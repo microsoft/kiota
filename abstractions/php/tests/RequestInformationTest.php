8b49feaa4fd1d1f79d042fa97f701597ec2cfe84
Philip Gichuhi <pgichuhi@microsoft.com>
2022-05-19 00:47:25 +0300

    Add custom QueryParameter annotation

--- abstractions/php/tests/RequestInformationTest.php
+++ abstractions/php/tests/RequestInformationTest.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace Microsoft\Kiota\Abstractions\Tests;
-use DateTime;
 use Microsoft\Kiota\Abstractions\RequestInformation;
 use PHPUnit\Framework\TestCase;
+use Microsoft\Kiota\Abstractions\QueryParameter;
+use Rize\UriTemplate;
 
 class RequestInformationTest extends TestCase {
     private RequestInformation $requestInformation;
@@ -15,12 +16,32 @@ class RequestInformationTest extends TestCase {
     public function testSetUri(): void{
         $pathParameters = [
             'baseUrl' => 'https://google.com',
-            'user' => 'silas',
+            'user%2Did' => 'silas',
         ];
-        $queryParameters = ['startDate' => (new DateTime('2022-01-17'))->format('Y-m-d')];
-        $this->requestInformation->urlTemplate = '{+baseUrl}/{user}/mails/?startDate=\'{startDate}\'';
+        $queryParameters = ['%24select' => ['subject', 'importance']];
+        $this->requestInformation->urlTemplate = '{+baseUrl}/{user%2Did}/mails{?%24select}';
         $this->requestInformation->pathParameters = $pathParameters;
         $this->requestInformation->queryParameters = $queryParameters;
-        $this->assertEquals("https://google.com/silas/mails/?startDate='2022-01-17'", $this->requestInformation->getUri());
+        $this->assertEquals("https://google.com/silas/mails?%24select=subject,importance", $this->requestInformation->getUri());
     }
+
+    public function testSetQueryParameters(): void {
+        $this->requestInformation->urlTemplate = '{?%24select,top}';
+
+        $queryParam = new TestQueryParameter();
+        $queryParam->select = ['displayName', 'age'];
+        $this->requestInformation->setQueryParameters($queryParam);
+        $this->assertArrayHasKey('%24select', $this->requestInformation->queryParameters);
+        $this->assertEquals(['displayName', 'age'], $this->requestInformation->queryParameters['%24select']);
+        $this->assertArrayHasKey('top', $this->requestInformation->queryParameters);
+        $this->assertEquals('?%24select=displayName,age&top=10', $this->requestInformation->getUri());
+    }
+}
+
+class TestQueryParameter {
+    /**
+     * @QueryParameter("%24select")
+     */
+    public array $select = [];
+    public int $top = 10; // no annotation
 }
