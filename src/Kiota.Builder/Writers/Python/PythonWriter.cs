e1ae3529b3eeb5135ee8be71b3b6c6542d3361fa
samwelkanda <samwelkanda@gmail.com>
2022-05-23 17:26:48 +0300

    Update language writer for python

---
+++ src/Kiota.Builder/Writers/Python/PythonWriter.cs
@@ -0,0 +1,18 @@
+namespace Kiota.Builder.Writers.Python
+{
+    public class PythonWriter : LanguageWriter
+    {
+        public PythonWriter(string rootPath, string clientNamespaceName)
+        {
+            PathSegmenter = new PythonPathSegmenter(rootPath, clientNamespaceName);
+            var conventionService = new PythonConventionService(null);
+            AddOrReplaceCodeElementWriter(new CodeClassDeclarationWriter(conventionService, clientNamespaceName));
+            AddOrReplaceCodeElementWriter(new CodeBlockEndWriter());
+            AddOrReplaceCodeElementWriter(new CodeEnumWriter(conventionService));
+            AddOrReplaceCodeElementWriter(new CodeMethodWriter(conventionService));
+            AddOrReplaceCodeElementWriter(new CodePropertyWriter(conventionService));
+            AddOrReplaceCodeElementWriter(new CodeTypeWriter(conventionService));
+            AddOrReplaceCodeElementWriter(new CodeNameSpaceWriter(conventionService));
+        }
+    }
+}
