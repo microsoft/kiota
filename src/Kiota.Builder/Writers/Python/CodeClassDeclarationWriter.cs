3259e91afb7ecf87cbf46fe9e26a05d89c7c2651
samwelkanda <samwelkanda@gmail.com>
2022-05-23 17:22:36 +0300

    Update class declaration not to add default imports for inner classes

--- src/Kiota.Builder/Writers/Python/CodeClassDeclarationWriter.cs
+++ src/Kiota.Builder/Writers/Python/CodeClassDeclarationWriter.cs
@@ -17,40 +17,43 @@ namespace Kiota.Builder.Writers.Python {
                                                             .Select(x => (x.Name, string.Empty, x.Declaration?.Name))
                                                             .GroupBy(x => x.Item3)
                                                             .OrderBy(x => x.Key);
-            foreach (var codeUsing in externalImportSymbolsAndPaths) // external imports before internal imports
+            if(externalImportSymbolsAndPaths.Any()){
+                foreach (var codeUsing in externalImportSymbolsAndPaths) // external imports before internal imports
                 if (!string.IsNullOrWhiteSpace(codeUsing.Key))
                 {
                     writer.WriteLine($"from {codeUsing.Key.ToSnakeCase()} import {codeUsing.Select(x => GetAliasedSymbol(x.Item1, x.Item2)).Distinct().OrderBy(x => x).Aggregate((x,y) => x + ", " + y)}");
                 }
-            writer.WriteLine();
+                writer.WriteLine();
+            }
             var internalImportSymbolsAndPaths = codeElement.Usings
                                                             .Where(x => !x.IsExternal)
                                                             .Select(x => _relativeImportManager.GetRelativeImportPathForUsing(x, parentNamespace))
                                                             .GroupBy(x => x.Item3)
                                                             .OrderBy(x => x.Key);
-
-            foreach (var codeUsing in internalImportSymbolsAndPaths)
-                if (!string.IsNullOrWhiteSpace(codeUsing.Key))
-                {
-                    writer.WriteLine($"from {codeUsing.Key.ToSnakeCase()} import {codeUsing.Select(x => GetAliasedSymbol(x.Item1, x.Item2)).Distinct().OrderBy(x => x).Aggregate((x,y) => x + ", " + y)}");
-                }
-            writer.WriteLine();
+            if(internalImportSymbolsAndPaths.Any()){
+                foreach (var codeUsing in internalImportSymbolsAndPaths)
+                    if (!string.IsNullOrWhiteSpace(codeUsing.Key))
+                    {
+                        writer.WriteLine($"from {codeUsing.Key.ToSnakeCase()} import {codeUsing.Select(x => GetAliasedSymbol(x.Item1, x.Item2)).Distinct().OrderBy(x => x).Aggregate((x,y) => x + ", " + y)}");
+                    }
+                writer.WriteLine();
+            }
             var inheritSymbol = conventions.GetTypeString(codeElement.Inherits, codeElement);
-            var abcClass = !codeElement.Implements.Any() ? string.Empty : $"{codeElement.Implements.Select(x => x.Name).Aggregate((x,y) => x + ", " + y)}";
+            var abcClass = !codeElement.Implements.Any() ? string.Empty : $"{codeElement.Implements.Select(x => x.Name.ToFirstCharacterUpperCase()).Aggregate((x,y) => x + ", " + y)}";
             var derivation = inheritSymbol == null ? abcClass : $"{inheritSymbol.ToFirstCharacterUpperCase()}";
             writer.WriteLine($"class {codeElement.Name.ToFirstCharacterUpperCase()}({derivation}):");
             writer.IncreaseIndent();
             conventions.WriteShortDescription((codeElement.Parent as CodeClass).Description, writer);
-            
         }
         private static string GetAliasedSymbol(string symbol, string alias) {
             return string.IsNullOrEmpty(alias) ? symbol : $"{symbol} as {alias}";
         }
 
         private static void AddStandardImports(LanguageWriter writer) {
-            writer.WriteLine("from __future__ import annotations");
-            writer.WriteLine("from typing import Any, Callable, Dict, List, Optional, Union");
-            writer.WriteLine();
+            if(string.IsNullOrEmpty(writer.GetIndent())){  // Don't add for inner classes
+                writer.WriteLine("from __future__ import annotations");
+                writer.WriteLine("from typing import Any, Callable, Dict, List, Optional, Union");
+            }            
         }
     }
 }
