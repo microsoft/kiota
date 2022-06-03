05dfdaf3b07212de7aef922f9e6bc64d3feee602
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2022-06-03 17:37:59 +0300

    Revert "Update System.CommandLine to beta 4"
    This reverts commit 1878cd5b402b110c55bb3729f4d28bd4b209e8c3.

--- cli/commons/src/Microsoft.Kiota.Cli.Commons.Tests/Binding/TypeBindingTests.cs
+++ cli/commons/src/Microsoft.Kiota.Cli.Commons.Tests/Binding/TypeBindingTests.cs
@@ -3,26 +3,22 @@ using System.CommandLine;
 using System.CommandLine.Binding;
 using System.CommandLine.Builder;
 using System.CommandLine.Parsing;
-using System.Threading.Tasks;
 using Moq;
 using Xunit;
 
 namespace Microsoft.Kiota.Cli.Commons.Binding;
 
-public class TypeBindingTests
-{
+public class TypeBindingTests {
     [Fact]
-    public void Should_Resolve_Service_On_Get_Bound_Value()
-    {
+    public void Should_Resolve_Service_On_Get_Bound_Value() {
         var binding = new TypeBinding(typeof(string));
         var cmd = new RootCommand();
         var builder = new CommandLineBuilder(cmd);
-        builder.AddMiddleware(invocation =>
-        {
-            invocation.BindingContext.AddService(_ => "Test");
+        builder.AddMiddleware(invocation => {
+            invocation.BindingContext.AddService<string>(_ => "Test");
         });
         string? result = null;
-        cmd.SetHandler((strType) => result = strType as string, binding);
+        cmd.SetHandler<string>(strType => result = strType, binding);
 
         builder.Build().Invoke("");
 
@@ -31,13 +27,12 @@ public class TypeBindingTests
     }
 
     [Fact]
-    public void Should_Throw_Exception_On_Service_Not_Found()
-    {
+    public void Should_Throw_Exception_On_Service_Not_Found() {
         var binding = new TypeBinding(typeof(string));
         var cmd = new RootCommand();
         var builder = new CommandLineBuilder(cmd);
         string? result = null;
-        cmd.SetHandler((strType) => result = strType as string, binding);
+        cmd.SetHandler<string>(strType => result = strType, binding);
 
         Assert.Throws<ArgumentException>(() => builder.Build().Invoke(""));
     }
