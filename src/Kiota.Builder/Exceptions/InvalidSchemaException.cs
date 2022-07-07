472953a8ebf27fd8b0bbf9a44a3f1bbed6af6c68
Vincent Biret <vibiret@microsoft.com>
2022-07-07 15:59:25 -0400

    - fixes coverage and linting feedback
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Kiota.Builder/Exceptions/InvalidSchemaException.cs
+++ src/Kiota.Builder/Exceptions/InvalidSchemaException.cs
@@ -3,9 +3,12 @@ using System;
 
 namespace Kiota.Builder.Exceptions;
 
+[Serializable]
 public class InvalidSchemaException : InvalidOperationException
 {
-    public InvalidSchemaException(string message) : base(message)
-    {
-    }
+    public InvalidSchemaException():base(){}
+    public InvalidSchemaException(string message) : base(message){}
+    #nullable enable
+    public InvalidSchemaException(string? message, Exception? innerException):base(message, innerException){}
+    #nullable disable
 }
