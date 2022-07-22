ea0744c30ae07b63bc9082765b5b6d94abd50fcd
samwelkanda <samwelkanda@gmail.com>
2022-07-22 18:30:36 +0300

    update parse node factory to work with content as python dict

--- serialization/python/json/serialization_json/json_parse_node_factory.py
+++ serialization/python/json/serialization_json/json_parse_node_factory.py
@@ -33,6 +33,7 @@ class JsonParseNodeFactory(ParseNodeFactory):
 
         if not content:
             raise TypeError("Content cannot be null")
-
+        
         content_as_str = content.decode('utf-8')
-        return JsonParseNode(content_as_str)
+        content_dict = json.loads(content_as_str)
+        return JsonParseNode(content_dict)
