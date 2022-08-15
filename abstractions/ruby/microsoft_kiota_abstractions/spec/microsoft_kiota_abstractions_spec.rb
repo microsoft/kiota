bcb4467c8384d51352ee33a969988293e5647e03
Osose Ewaleifoh <t-oewaleifoh@microsoft.com>
2022-08-15 10:46:49 -0700

    Deleting outdated tests

--- abstractions/ruby/microsoft_kiota_abstractions/spec/microsoft_kiota_abstractions_spec.rb
+++ abstractions/ruby/microsoft_kiota_abstractions/spec/microsoft_kiota_abstractions_spec.rb
@@ -18,16 +18,6 @@ RSpec.describe MicrosoftKiotaAbstractions do
     expect(token_provider).not_to be nil
   end
 
-  it "creates a bearer token provider" do
-    token_provider = MicrosoftKiotaAbstractions::BaseBearerTokenAuthenticationProvider.new()
-    expect(token_provider).not_to be nil
-  end
-
-  it "throws if the token method is not implemented" do
-    token_provider = MicrosoftKiotaAbstractions::BaseBearerTokenAuthenticationProvider.new()
-    expect { token_provider.authenticate_request(MicrosoftKiotaAbstractions::RequestInformation.new()) }.to raise_error(NotImplementedError)
-  end
-
   it "returns the raw URI when set via setter" do
     request_obj = MicrosoftKiotaAbstractions::RequestInformation.new
     request_obj.path_parameters["term"] = "search"
