f3f67d188a921ffc292e92716da859fdce346a0a
Osose Ewaleifoh <t-oewaleifoh@microsoft.com>
2022-08-15 11:11:31 -0700

    Changes/update to Access Token Provider Interface

--- abstractions/ruby/microsoft_kiota_abstractions/lib/microsoft_kiota_abstractions/authentication/access_token_provider.rb
+++ abstractions/ruby/microsoft_kiota_abstractions/lib/microsoft_kiota_abstractions/authentication/access_token_provider.rb
@@ -4,26 +4,9 @@ require 'concurrent'
 require_relative 'allowed_hosts_validator'
 
 module MicrosoftKiotaAbstractions
-  # Access Token Provider class implementation
-  class AccessTokenProvider
+  # Access Token Provider Module implementation
+  module AccessTokenProvider
     include Concurrent::Async
-    # This is the initializer for AccessTokenProvider.
-    # :params
-    #   token_request_context: a instance of one of our token request context
-    #   allowed_hosts: an array of strings, where each string is an allowed host, default is empty
-    #   scopes: an array of strings, where each string is a scope, default is empty array 
-    #   auth_code: a string containting the auth code; default is nil, can be updated post-initialization
-    def initialize(allowed_hosts = [], scopes = [])
-      base_constructor = nil 
-      @host_validator = if allowed_hosts.nil? || allowed_hosts.size.zero?
-                          MicrsoftKiotaAbstractions::AllowedHostsValidator.new(['graph.microsoft.com', 'graph.microsoft.us', 'dod-graph.microsoft.us',
-                                                                                'graph.microsoft.de', 'microsoftgraph.chinacloudapi.cn',
-                                                                                'canary.graph.microsoft.com'])
-                        else
-                          MicrsoftKiotaAbstractions::AllocatedHostsValidator.new(allowed_hosts)
-                        end
-    end
-
     # This function obtains the authorization token.
     # :params
     #   uri: a string containing the uri 
@@ -33,11 +16,7 @@ module MicrosoftKiotaAbstractions
       raise NotImplementedError.new
     end
 
-    attr_reader :scopes, :cached_token, :host_validator
-    
-    private
-
-    attr_writer :host_validator, :token_credential, :scopes
+    attr_accessor :scopes, :host_validator
 
   end
 end
