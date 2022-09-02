b0c3c5c552fe1d4526a3d5c4ec7d38beda056868
Vincent Biret <vibiret@microsoft.com>
2022-09-02 15:52:05 -0400

    - fixes a bug where java would be missing inner classes imports - fixes a bug where java inner models would not instantiate in some cases
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Kiota.Builder/Writers/Java/CodeClassDeclarationWriter.cs
+++ src/Kiota.Builder/Writers/Java/CodeClassDeclarationWriter.cs
@@ -12,6 +12,7 @@ public class CodeClassDeclarationWriter : BaseElementWriter<ClassDeclaration, Ja
             writer.WriteLine($"package {ns.Name};");
             writer.WriteLine();
             codeElement.Usings
+                .Union(codeElement.Parent is CodeClass cClass ? cClass.InnerClasses.SelectMany(static x => x.Usings) : Enumerable.Empty<CodeUsing>())
                 .Where(static x => x.Declaration != null)
                 .Where(x => x.Declaration.IsExternal || !x.Declaration.Name.Equals(codeElement.Name, StringComparison.OrdinalIgnoreCase)) // needed for circular requests patterns like message folder
                 .Select(static x => x.Declaration.IsExternal ?
@@ -27,7 +28,8 @@ public class CodeClassDeclarationWriter : BaseElementWriter<ClassDeclaration, Ja
         var derivation = (codeElement.Inherits == null ? string.Empty : $" extends {codeElement.Inherits.Name.ToFirstCharacterUpperCase()}") +
                         (!codeElement.Implements.Any() ? string.Empty : $" implements {codeElement.Implements.Select(x => x.Name).Aggregate((x,y) => x + ", " + y)}");
         conventions.WriteShortDescription((codeElement.Parent as CodeClass)?.Description, writer);
-        writer.WriteLine($"public class {codeElement.Name.ToFirstCharacterUpperCase()}{derivation} {{");
+        var innerClassStatic = codeElement.Parent is CodeClass currentClass && currentClass.IsOfKind(CodeClassKind.Model) && currentClass.Parent is CodeClass ? "static " : string.Empty; //https://stackoverflow.com/questions/47541459/no-enclosing-instance-is-accessible-must-qualify-the-allocation-with-an-enclosi
+        writer.WriteLine($"public {innerClassStatic}class {codeElement.Name.ToFirstCharacterUpperCase()}{derivation} {{");
         writer.IncreaseIndent();
     }
 }
