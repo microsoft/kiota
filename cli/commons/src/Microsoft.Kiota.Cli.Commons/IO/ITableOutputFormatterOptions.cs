d6a1a1b5ffeb4e307b2d049090bd041846b6c5f8
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2022-11-23 17:18:22 +0300

    Remove CLI commons

--- cli/commons/src/Microsoft.Kiota.Cli.Commons/IO/ITableOutputFormatterOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace Microsoft.Kiota.Cli.Commons.IO;
-
-/// <summary>
-/// The table output formatter options
-/// </summary>
-public interface ITableOutputFormatterOptions : IOutputFormatterOptions
-{
-}
--- cli/commons/src/Microsoft.Kiota.Cli.Commons/IO/OutputFormatterOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace Microsoft.Kiota.Cli.Commons.IO;
-
-/// <summary>
-/// The output formatter options base
-/// </summary>
-public class OutputFormatterOptions : IOutputFormatterOptions
-{
-}
