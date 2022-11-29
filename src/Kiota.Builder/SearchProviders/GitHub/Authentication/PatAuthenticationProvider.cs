8e2468e1fbb984c70414d35a77d2753427521b03
Vincent Biret <vibiret@microsoft.com>
2022-11-29 15:34:51 -0500

    - adds missing unit tests for github authentication
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Kiota.Builder/SearchProviders/GitHub/Authentication/PatAuthenticationProvider.cs
+++ src/Kiota.Builder/SearchProviders/GitHub/Authentication/PatAuthenticationProvider.cs
@@ -4,10 +4,10 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 
 namespace Kiota.Builder.SearchProviders.GitHub.Authentication;
-public class PatAuthenticationProvider : BaseAuthenticationProvider<PatProvider>
+public class PatAuthenticationProvider : BaseAuthenticationProvider<PatAccessTokenProvider>
 {
     public PatAuthenticationProvider(string clientId, string scope, IEnumerable<string> validHosts, ILogger logger, ITokenStorageService StorageService) :
-        base(clientId, scope, validHosts, logger, (clientId, scope, validHosts) => new PatProvider {
+        base(clientId, scope, validHosts, logger, (clientId, scope, validHosts) => new PatAccessTokenProvider {
             StorageService = StorageService,
             AllowedHostsValidator = new (validHosts),
         }, false)
---
+++ tests/Kiota.Builder.Tests/SearchProviders/GitHub/Authentication/PatAuthenticationProviderTests.cs
@@ -0,0 +1,18 @@
+
+using System;
+using Kiota.Builder.SearchProviders.GitHub.Authentication;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace Kiota.Builder.Tests.SearchProviders.GitHub.Authentication;
+public class PatAuthenticationProviderTests {
+    [Fact]
+    public void Defensive() {
+        Assert.Throws<ArgumentNullException>(() => new PatAuthenticationProvider(null, "foo", new string[] {"foo"}, new Mock<ILogger>().Object, new Mock<ITokenStorageService>().Object));
+        Assert.Throws<ArgumentNullException>(() => new PatAuthenticationProvider("foo", null, new string[] {"foo"}, new Mock<ILogger>().Object, new Mock<ITokenStorageService>().Object));
+        Assert.Throws<ArgumentNullException>(() => new PatAuthenticationProvider("foo", "foo", new string[] {"foo"}, null, new Mock<ITokenStorageService>().Object));
+        Assert.Throws<ArgumentNullException>(() => new PatAuthenticationProvider("foo", "foo", new string[] {"foo"}, new Mock<ILogger>().Object, null));
+    }
+
+}
