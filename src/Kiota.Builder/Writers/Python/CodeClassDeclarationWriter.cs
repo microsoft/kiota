3eb8398a53d8635ccec8c0e904b00bfde81d1e46
samwelkanda <samwelkanda@gmail.com>
2022-12-05 15:40:01 +0300

    Update internal imports to lazy load

--- src/Kiota.Builder/Writers/Python/CodeClassDeclarationWriter.cs
+++ src/Kiota.Builder/Writers/Python/CodeClassDeclarationWriter.cs
@@ -7,9 +7,8 @@ using Kiota.Builder.Extensions;
 namespace Kiota.Builder.Writers.Python;
 public class CodeClassDeclarationWriter : BaseElementWriter<ClassDeclaration, PythonConventionService>
 {
-    private readonly PythonRelativeImportManager _relativeImportManager;
-    public CodeClassDeclarationWriter(PythonConventionService conventionService, string clientNamespaceName) : base(conventionService){
-        _relativeImportManager = new PythonRelativeImportManager(clientNamespaceName, '.');
+
+    public CodeClassDeclarationWriter(PythonConventionService conventionService) : base(conventionService){
     }
     public override void WriteCodeElement(ClassDeclaration codeElement, LanguageWriter writer)
     {
@@ -17,7 +16,7 @@ public class CodeClassDeclarationWriter : BaseElementWriter<ClassDeclaration, Py
         if(writer == null) throw new ArgumentNullException(nameof(writer));
         var parentNamespace = codeElement.GetImmediateParentOfType<CodeNamespace>();
         WriteExternalImports(codeElement, writer); // external imports before internal imports
-        WriteInternalImports(codeElement, writer, parentNamespace);
+        WriteInternalImports(codeElement, writer);
         
         var inheritSymbol = conventions.GetTypeString(codeElement.Inherits, codeElement);
         var abcClass = !codeElement.Implements.Any() ? string.Empty : $"{codeElement.Implements.Select(x => x.Name.ToFirstCharacterUpperCase()).Aggregate((x,y) => x + ", " + y)}";
@@ -49,20 +48,44 @@ public class CodeClassDeclarationWriter : BaseElementWriter<ClassDeclaration, Py
         }
     }
 
-    private void WriteInternalImports(ClassDeclaration codeElement, LanguageWriter writer, CodeNamespace parentNameSpace) {
+    private static void WriteInternalImports(ClassDeclaration codeElement, LanguageWriter writer) {
         var internalImportSymbolsAndPaths = codeElement.Usings
                                                         .Where(x => !x.IsExternal)
-                                                        .Select(x => _relativeImportManager.GetRelativeImportPathForUsing(x, parentNameSpace))
+                                                        .Select(x => GetImportPathForUsing(x))
                                                         .GroupBy(x => x.Item3)
                                                         .Where(x => !string.IsNullOrEmpty(x.Key))
                                                         .OrderBy(x => x.Key);
         if(internalImportSymbolsAndPaths.Any()){
             foreach (var codeUsing in internalImportSymbolsAndPaths)
-                writer.WriteLine($"from {codeUsing.Key.ToSnakeCase()} import {codeUsing.Select(x => GetAliasedSymbol(x.Item1, x.Item2)).Distinct().OrderBy(x => x).Aggregate((x,y) => x + ", " + y)}");
+                foreach(var symbol in codeUsing.Select(x => GetAliasedSymbol(x.Item1, x.Item2)).Distinct().OrderBy(x => x))
+                    writer.WriteLine($"{symbol} = lazy_import('{codeUsing.Key.ToSnakeCase().Replace("._", ".")}.{symbol}')"); 
             writer.WriteLine();
         }
     }
     private static string GetAliasedSymbol(string symbol, string alias) {
         return string.IsNullOrEmpty(alias) ? symbol : $"{symbol} as {alias}";
+    }
+
+        /// <summary>
+    /// Returns the import path for the given using and import context namespace.
+    /// </summary>
+    /// <param name="codeUsing">The using to import into the current namespace context</param>
+    /// <returns>The import symbol, it's alias if any and the import path</returns>
+    private static (string, string, string) GetImportPathForUsing(CodeUsing codeUsing)
+    {
+        if (codeUsing?.IsExternal ?? true)
+            return (string.Empty, string.Empty, string.Empty);//it's an external import, add nothing
+        var typeDef = codeUsing.Declaration.TypeDefinition;
+
+        var importSymbol = codeUsing.Declaration == null ? codeUsing.Name : codeUsing.Declaration.TypeDefinition switch
+        {
+            CodeFunction f => f.Name.ToFirstCharacterLowerCase(),
+            _ => codeUsing.Declaration.TypeDefinition.Name.ToSnakeCase(),
+        };
+
+        if (typeDef == null)
+            return (importSymbol, codeUsing.Alias, ""); // it's relative to the folder, with no declaration (default failsafe)
+        var importPath = typeDef.GetImmediateParentOfType<CodeNamespace>().Name;
+        return (importSymbol, codeUsing.Alias, importPath);
     }
 }
