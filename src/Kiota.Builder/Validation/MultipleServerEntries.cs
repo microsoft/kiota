30eed4f6d6655da934e79872099982c45e4442f5
Vincent Biret <vibiret@microsoft.com>
2022-12-05 10:43:48 -0500

    - adds validation rule for multiple server entries - adds validation for no server entry
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

---
+++ src/Kiota.Builder/Validation/MultipleServerEntries.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Validations;
+
+public class MultipleServerEntries : ValidationRule<OpenApiDocument>
+{
+    public MultipleServerEntries() : base(static (context, document) =>
+    {
+        if (document.Servers.GroupBy(static x => x.Url, StringComparer.OrdinalIgnoreCase).Count() > 1)
+            context.CreateWarning(nameof(MultipleServerEntries),
+                                "Multiple servers entries were found in the OpenAPI description. Only the first one will be used. The root URL can be set manually with the request adapter.");
+    })
+    {
+    }
+}
