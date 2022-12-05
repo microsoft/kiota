5b20cee6a491a88f7860bf5646f58a5afabb6d03
Vincent Biret <vibiret@microsoft.com>
2022-12-05 13:33:50 -0500

    - adds a warning for known unsupported formats
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

---
+++ src/Kiota.Builder/Validation/KnownAndNotSupportedFormats.cs
@@ -0,0 +1,33 @@
+
+using System;
+using System.Collections.Generic;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Validations;
+
+namespace Kiota.Builder.Validation;
+
+public class KnownAndNotSupportedFormats : ValidationRule<OpenApiSchema>
+{
+    private static readonly HashSet<string> knownAndUnsupportedFormats = new(StringComparer.OrdinalIgnoreCase) {
+        "email",
+        "idn-email",
+        "hostname",
+        "idn-hostname",
+        "ipv4",
+        "ipv6",
+        "uri",
+        "uri-reference",
+        "iri",
+        "iri-reference",
+        "uri-template",
+        "json-pointer",
+        "relative-json-pointer",
+        "regex",
+    };
+    public KnownAndNotSupportedFormats() : base(static (context, schema) => {
+        if (!string.IsNullOrEmpty(schema.Format) && knownAndUnsupportedFormats.Contains(schema.Format))
+            context.CreateWarning(nameof(KnownAndNotSupportedFormats), $"The format {schema.Format} is not supported by Kiota and the string type will be used.");
+    })
+    {
+    }
+}
