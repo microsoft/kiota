98d8668a1c2badfd429b998cd88174d3a907c579
Vincent Biret <vibiret@microsoft.com>
2022-12-05 13:06:18 -0500

    - adds a no content with body validation rule
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

---
+++ tests/Kiota.Builder.Tests/Validation/NoContentWithBodyTests.cs
@@ -0,0 +1,86 @@
+using System.IO;
+using System.Text;
+using Kiota.Builder.Validation;
+using Microsoft.OpenApi.Readers;
+using Microsoft.OpenApi.Validations;
+using Xunit;
+
+namespace Kiota.Builder.Tests.Validation;
+
+public class NoContentWithBodyTests {
+    [Fact]
+    public void AddsAWarningWhen204WithBody() {
+        var rule = new NoContentWithBody();
+        var documentTxt = @"openapi: 3.0.1
+info:
+  title: OData Service for namespace microsoft.graph
+  description: This OData service is located at https://graph.microsoft.com/v1.0
+  version: 1.0.1
+paths:
+  /enumeration:
+    get:
+      responses:
+        '204':
+          content:
+            application/json:";
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(documentTxt));
+        var reader = new OpenApiStreamReader(new OpenApiReaderSettings
+        {
+            RuleSet = new (new ValidationRule[] { rule }),
+        });
+        var doc = reader.Read(stream, out var diag);
+        // TODO https://github.com/microsoft/OpenAPI.NET/pull/1097 is merged
+        // Assert.Single(diag.Warnings);
+    }
+    [Fact]
+    public void DoesntAddAWarningWhen204WithNoBody() {
+        var rule = new NoContentWithBody();
+        var documentTxt = @"openapi: 3.0.1
+info:
+  title: OData Service for namespace microsoft.graph
+  description: This OData service is located at https://graph.microsoft.com/v1.0
+  version: 1.0.1
+servers:
+  - url: https://graph.microsoft.com/v1.0
+paths:
+  /enumeration:
+    get:
+      responses:
+        '204':";
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(documentTxt));
+        var reader = new OpenApiStreamReader(new OpenApiReaderSettings
+        {
+            RuleSet = new (new ValidationRule[] { rule }),
+        });
+        var doc = reader.Read(stream, out var diag);
+        Assert.Empty(diag.Warnings);
+    }
+    [Fact]
+    public void DoesntAddAWarningWhen200WithBody() {
+        var rule = new NoContentWithBody();
+        var documentTxt = @"openapi: 3.0.1
+info:
+  title: OData Service for namespace microsoft.graph
+  description: This OData service is located at https://graph.microsoft.com/v1.0
+  version: 1.0.1
+paths:
+  /enumeration:
+    post:
+      requestBody:
+        description: New navigation property
+        content:
+          application/json:
+      responses:
+        '200':
+          content:
+            application/json:";
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(documentTxt));
+        var reader = new OpenApiStreamReader(new OpenApiReaderSettings
+        {
+            RuleSet = new (new ValidationRule[] { rule }),
+        });
+        var doc = reader.Read(stream, out var diag);
+        Assert.Empty(diag.Warnings);
+    }
+
+}
