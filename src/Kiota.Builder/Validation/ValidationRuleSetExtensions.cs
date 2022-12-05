6153b8c5d03907968c558344352e63ed678af0fc
Vincent Biret <vibiret@microsoft.com>
2022-12-05 12:56:27 -0500

    - adds get with body validation rule
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Kiota.Builder/Validation/ValidationRuleSetExtensions.cs
+++ src/Kiota.Builder/Validation/ValidationRuleSetExtensions.cs
@@ -1,10 +1,13 @@
+using System;
 using Microsoft.OpenApi.Validations;
 
 namespace Kiota.Builder.Validation;
 
 public static class ValidationRuleSetExtensions {
     public static void AddKiotaValidationRules(this ValidationRuleSet ruleSet) {
+        ArgumentNullException.ThrowIfNull(ruleSet);
         ruleSet.Add(new NoServerEntry());
         ruleSet.Add(new MultipleServerEntries());
+        ruleSet.Add(new GetWithBody());
     }
 }
