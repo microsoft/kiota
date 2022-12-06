bfd53b75cf5117bb59ada99f3967e1653e6377ca
Vincent Biret <vibiret@microsoft.com>
2022-12-06 09:52:34 -0500

    - code linting
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Kiota.Builder/Validation/UrlFormEncodedComplex.cs
+++ src/Kiota.Builder/Validation/UrlFormEncodedComplex.cs
@@ -15,19 +15,16 @@ public class UrlFormEncodedComplex : ValidationRule<OpenApiOperation>
     public UrlFormEncodedComplex() : base(static (context, operation) => {
         var requestSchema = operation.GetRequestSchema(validContentTypes);
         var responseSchema = operation.GetResponseSchema(validContentTypes);
-        if (requestSchema != null) {
-            if (!requestSchema.IsObject())
-                context.CreateWarning(nameof(UrlFormEncodedComplex), $"The operation {operation.OperationId} has a request body which is not an object type. This is not supported by Kiota and serialization will fail.");
-            if (requestSchema.Properties.Any(static x => x.Value.IsObject() || x.Value.IsArray()))
-                context.CreateWarning(nameof(UrlFormEncodedComplex), $"The operation {operation.OperationId} has a request body with a complex properties and the url form encoded content type. This is not supported by Kiota and serialization of complex properties will fail.");
-        }
-        if(responseSchema != null) {
-            if (!responseSchema.IsObject())
-                context.CreateWarning(nameof(UrlFormEncodedComplex), $"The operation {operation.OperationId} has a response body which is not an object type. This is not supported by Kiota and deserialization will fail.");
-            if (responseSchema.Properties.Any(static x => x.Value.IsObject() || x.Value.IsArray()))
-               context.CreateWarning(nameof(UrlFormEncodedComplex), $"The operation {operation.OperationId} has a response body with a complex properties and the url form encoded content type. This is not supported by Kiota and deserialization of complex properties will fail.");
-        }
+        ValidateSchema(requestSchema, context, operation.OperationId, "request body");
+        ValidateSchema(responseSchema, context, operation.OperationId, "response body");
     })
     {
+    }
+    private static void ValidateSchema(OpenApiSchema schema, IValidationContext context, string operationId, string schemaName) {
+        if(schema == null) return;
+        if (!schema.IsObject())
+            context.CreateWarning(nameof(UrlFormEncodedComplex), $"The operation {operationId} has a {schemaName} which is not an object type. This is not supported by Kiota and serialization will fail.");
+        if (schema.Properties.Any(static x => x.Value.IsObject() || x.Value.IsArray()))
+            context.CreateWarning(nameof(UrlFormEncodedComplex), $"The operation {operationId} has a {schemaName} with a complex properties and the url form encoded content type. This is not supported by Kiota and serialization of complex properties will fail.");
     }
 }
