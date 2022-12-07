87a9a231f3737c4efc0a30906917cd5a2d2aece9
Vincent Biret <vibiret@microsoft.com>
2022-12-07 13:04:52 -0500

    - adds external doc support for Go
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- tests/Kiota.Builder.Tests/Writers/Go/CodeMethodWriterTests.cs
+++ tests/Kiota.Builder.Tests/Writers/Go/CodeMethodWriterTests.cs
@@ -1160,6 +1160,29 @@ public class CodeMethodWriterTests : IDisposable {
         AssertExtensions.CurlyBracesAreClosed(result);
     }
     [Fact]
+    public void WritesMethodDescriptionLink() {
+        method.Documentation.Description = MethodDescription;
+        method.Documentation.DocumentationLabel = "see more";
+        method.Documentation.DocumentationLink = new("https://foo.org/docs");
+        method.IsAsync = false;
+        var parameter = new CodeParameter
+        {
+            Documentation = new() {
+                Description = ParamDescription,
+            },
+            Name = ParamName,
+            Type = new CodeType
+            {
+                Name = "string"
+            }
+        };
+        method.AddParameter(parameter);
+        writer.Write(method);
+        var result = tw.ToString();
+        Assert.Contains("[see more]: ", result);
+        AssertExtensions.CurlyBracesAreClosed(result);
+    }
+    [Fact]
     public void Defensive() {
         var codeMethodWriter = new CodeMethodWriter(new GoConventionService());
         Assert.Throws<ArgumentNullException>(() => codeMethodWriter.WriteCodeElement(null, writer));
