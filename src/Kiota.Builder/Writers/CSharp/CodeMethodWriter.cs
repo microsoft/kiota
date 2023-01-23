7b90d55f8cdb45a329e197c927dd71f377690261
Vincent Biret <vibiret@microsoft.com>
2023-01-23 15:02:17 -0500

    - fixes a race condition in namespace disambiguation for CSharp.
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Kiota.Builder/Writers/CSharp/CodeMethodWriter.cs
+++ src/Kiota.Builder/Writers/CSharp/CodeMethodWriter.cs
@@ -89,8 +89,7 @@ public class CodeMethodWriter : BaseElementWriter<CodeMethod, CSharpConventionSe
     private static readonly CodePropertyTypeComparer CodePropertyTypeForwardComparer = new();
     private static readonly CodePropertyTypeComparer CodePropertyTypeBackwardComparer = new(true);
     private void WriteFactoryMethodBodyForInheritedModel(CodeMethod codeElement, CodeClass parentClass, LanguageWriter writer) {
-        writer.WriteLine($"return {DiscriminatorMappingVarName} switch {{");
-        writer.IncreaseIndent();
+        writer.StartBlock($"return {DiscriminatorMappingVarName} switch {{");
         foreach(var mappedType in parentClass.DiscriminatorInformation.DiscriminatorMappings) {
             writer.WriteLine($"\"{mappedType.Key}\" => new {conventions.GetTypeString(mappedType.Value.AllTypes.First(), codeElement)}(),");
         }
