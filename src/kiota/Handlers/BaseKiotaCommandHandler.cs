0eed09dc7a05b73e4fbc8aa759d60fea4dd4b660
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2023-01-24 19:58:21 +0000

    Add a Source Generator to have built-in defaults

--- src/kiota/Handlers/BaseKiotaCommandHandler.cs
+++ src/kiota/Handlers/BaseKiotaCommandHandler.cs
@@ -34,7 +34,8 @@ internal abstract class BaseKiotaCommandHandler : ICommandHandler
     protected KiotaConfiguration Configuration { get => ConfigurationFactory.Value; }
     private readonly Lazy<KiotaConfiguration> ConfigurationFactory = new (() => {
         var builder = new ConfigurationBuilder();
-        var configuration = builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+        var configuration = builder.AddJsonStream(new MemoryStream(Kiota.Generated.KiotaAppSettings.Default()))
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
                 .AddEnvironmentVariables(prefix: "KIOTA_")
                 .Build();
         var configObject = new KiotaConfiguration();
