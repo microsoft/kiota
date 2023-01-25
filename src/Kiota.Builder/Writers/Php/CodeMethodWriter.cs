895b17620b2a3d271e7a940aa094dffd1e168225
silaskenneth <silaskenneth1@gmail.com>
2023-01-25 12:30:00 +0300

    Make regex compiled.

--- src/Kiota.Builder/Writers/Php/CodeMethodWriter.cs
+++ src/Kiota.Builder/Writers/Php/CodeMethodWriter.cs
@@ -10,7 +10,7 @@ namespace Kiota.Builder.Writers.Php
     {
 
         protected readonly bool UseBackingStore = false;
-        private static readonly Regex factoryMethodIndexParser = new(@"_(?<idx>\d+)");
+        private static readonly Regex factoryMethodIndexParser = new(@"_(?<idx>\d+)", RegexOptions.Compiled);
         public CodeMethodWriter(PhpConventionService conventionService, bool useBackingStore = false) : base(conventionService)
         {
             UseBackingStore = useBackingStore;
