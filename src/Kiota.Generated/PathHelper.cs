31de9c80dae6e85390dc98bdf52ec5209437f32b
Vincent Biret <vibiret@microsoft.com>
2023-02-01 14:20:01 -0500

    - fixes a bug where vs debugging would fail for project

---
+++ src/Kiota.Generated/PathHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+internal class PathHelper
+{
+    internal static string Join(string first, string second)
+    {
+        if(string.IsNullOrEmpty(second)) return first;
+        if(string.IsNullOrEmpty(first)) return second;
+        return first + 
+            (first.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.OrdinalIgnoreCase) || first.EndsWith(Path.AltDirectorySeparatorChar.ToString(), StringComparison.OrdinalIgnoreCase) ? 
+                string.Empty :
+                Path.DirectorySeparatorChar) +
+            second;
+    }
+}
