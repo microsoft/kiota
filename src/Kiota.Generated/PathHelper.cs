e5dfea99261a3ee07dd32eaf7d8c74e41f7d0a3f
Vincent Biret <vibiret@microsoft.com>
2023-02-01 14:30:03 -0500

    - fixes formatting

--- src/Kiota.Generated/PathHelper.cs
+++ src/Kiota.Generated/PathHelper.cs
@@ -5,10 +5,10 @@ internal class PathHelper
 {
     internal static string Join(string first, string second)
     {
-        if(string.IsNullOrEmpty(second)) return first;
-        if(string.IsNullOrEmpty(first)) return second;
-        return first + 
-            (first.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.OrdinalIgnoreCase) || first.EndsWith(Path.AltDirectorySeparatorChar.ToString(), StringComparison.OrdinalIgnoreCase) ? 
+        if (string.IsNullOrEmpty(second)) return first;
+        if (string.IsNullOrEmpty(first)) return second;
+        return first +
+            (first.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.OrdinalIgnoreCase) || first.EndsWith(Path.AltDirectorySeparatorChar.ToString(), StringComparison.OrdinalIgnoreCase) ?
                 string.Empty :
                 Path.DirectorySeparatorChar) +
             second;
