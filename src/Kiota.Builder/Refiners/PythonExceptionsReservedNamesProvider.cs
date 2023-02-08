2d11608791c20a6149d880c6562882d4f2ee67ab
samwelkanda <samwelkanda@gmail.com>
2023-02-08 19:36:41 +0300

    Escape exceptions reserved names

---
+++ src/Kiota.Builder/Refiners/PythonExceptionsReservedNamesProvider.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace Kiota.Builder.Refiners;
+public class PythonExceptionsReservedNamesProvider : IReservedNamesProvider
+{
+    private readonly Lazy<HashSet<string>> _reservedNames = new(static () => new(StringComparer.OrdinalIgnoreCase)
+    {
+        "mro",
+        "with_traceback",
+        "response_status_code",
+    });
+    public HashSet<string> ReservedNames => _reservedNames.Value;
+}
