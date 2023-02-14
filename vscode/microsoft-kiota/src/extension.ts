d3ff5875faed8aae191bbea72bac64ceae92a0f6
Vincent Biret <vibiret@microsoft.com>
2023-02-14 09:07:57 -0500

    - finalizes status bar

--- vscode/microsoft-kiota/src/extension.ts
+++ vscode/microsoft-kiota/src/extension.ts
@@ -6,14 +6,22 @@ let kiotaStatusBarItem: vscode.StatusBarItem;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) : Promise<void> {
   const statusBarCommandId = "microsoft-kiota.status";
   context.subscriptions.push(
-    vscode.commands.registerCommand(statusBarCommandId, () => {
-      // const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
-      vscode.window.showInformationMessage(
-        `Yeah, 10 line(s) selected... Keep going!`
+    vscode.commands.registerCommand(statusBarCommandId, async () => {
+      const response = await vscode.window.showInformationMessage(
+        `Open installation instructions for kiota?`,
+        "Yes",
+        "No"
       );
+      if (response === "Yes") {
+        vscode.env.openExternal(
+          vscode.Uri.parse(
+            "https://aka.ms/get/kiota"
+          )
+        );
+      }
     })
   );
 
@@ -28,11 +36,11 @@ export function activate(context: vscode.ExtensionContext) {
   // register some listener that make sure the status bar
   // item always up-to-date
   context.subscriptions.push(
-    vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem)
+    vscode.window.onDidChangeActiveTextEditor(async() => await updateStatusBarItem())
   );
 
   // update status bar item once at start
-  updateStatusBarItem();
+  await updateStatusBarItem();
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log(
@@ -54,23 +62,34 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
-function updateStatusBarItem(): void {
-  const cp = require("child_process");
-  cp.exec("kiota2 --version", (err: any, stdout: string, stderr: any) => {
-    if (stdout) {
-      const version = stdout.split("+")[0];
-      if (version) {
-        kiotaStatusBarItem.text = `$(extensions-info-message) kiota ${version}`;
-        kiotaStatusBarItem.show();
-        return;
+function getKiotaVersion(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const cp = require("child_process");
+    cp.exec("kiota --version", (err: any, stdout: string, stderr: any) => {
+      if (stdout) {
+        const version = stdout.split("+")[0];
+        if (version) {
+          resolve(version);
+          return;
+        }
       }
-    }
+      reject();
+    });
+  });
+}
+
+async function updateStatusBarItem(): Promise<void> {
+  try {
+    const version = await getKiotaVersion();
+    kiotaStatusBarItem.text = `$(extensions-info-message) kiota ${version}`;
+    kiotaStatusBarItem.show();
+  } catch (error) {
     kiotaStatusBarItem.text = `$(extensions-warning-message) kiota not found`;
     kiotaStatusBarItem.backgroundColor = new vscode.ThemeColor(
       "statusBarItem.errorBackground"
     );
-    kiotaStatusBarItem.show();
-  });
+  }
+  kiotaStatusBarItem.show();
 }
 
 // This method is called when your extension is deactivated
