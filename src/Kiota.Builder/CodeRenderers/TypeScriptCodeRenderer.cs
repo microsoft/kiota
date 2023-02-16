93b566f6407e6be657271c502281176eaac446f0
Nikitha Chettiar <nikithauc@gmail.com>
2023-02-15 20:21:09 -0800

    format

--- src/Kiota.Builder/CodeRenderers/TypeScriptCodeRenderer.cs
+++ src/Kiota.Builder/CodeRenderers/TypeScriptCodeRenderer.cs
@@ -3,11 +3,11 @@ using Kiota.Builder.CodeDOM;
 using Kiota.Builder.Configuration;
 
 namespace Kiota.Builder.CodeRenderers;
-    public class TypeScriptCodeRenderer : CodeRenderer
+public class TypeScriptCodeRenderer : CodeRenderer
+{
+    public TypeScriptCodeRenderer(GenerationConfiguration configuration) : base(configuration) { }
+    public override bool ShouldRenderNamespaceFile(CodeNamespace codeNamespace)
     {
-        public TypeScriptCodeRenderer(GenerationConfiguration configuration) : base(configuration) { }
-        public override bool ShouldRenderNamespaceFile(CodeNamespace codeNamespace)
-        {
-            return codeNamespace.CodeInterfaces.Any();
-        }
+        return codeNamespace.CodeInterfaces.Any();
     }
+}
