ace6e4e3c552cfd6e827bbed2ea29cd189c4e4be
Vincent Biret <vibiret@microsoft.com>
2023-02-24 15:59:11 -0500

    - moves integration tests outside of main workflow
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- it/java/generate.sh
+++ it/java/generate.sh
@@ -3,11 +3,11 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROOT_DIR=${SCRIPT_DIR}/../..
 
-${ROOT_DIR}/src/kiota/bin/Debug/net7.0/kiota generate --language Java \
+${ROOT_DIR}/publish/kiota generate --language Java \
   --openapi ${ROOT_DIR}/tests/Kiota.Builder.IntegrationTests/InheritingErrors.yaml \
   --output ${ROOT_DIR}/it/java/src
 
-${ROOT_DIR}/src/kiota/bin/Debug/net7.0/kiota generate --language Java \
+${ROOT_DIR}/publish/kiota generate --language Java \
   --openapi ${ROOT_DIR}/tests/Kiota.Builder.IntegrationTests/NoUnderscoresInModel.yaml \
   --output ${ROOT_DIR}/it/java/src \
   --namespace-name "no.underscores"
--- it/java/test.java
+++ it/java/test.java
@@ -1,9 +1,9 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 
-//DEPS com.microsoft.kiota:microsoft-kiota-abstractions:0.2.0
-//DEPS com.microsoft.kiota:microsoft-kiota-serialization-json:0.2.0
-//DEPS com.microsoft.kiota:microsoft-kiota-serialization-text:0.2.0
-//DEPS com.microsoft.kiota:microsoft-kiota-serialization-form:0.2.0
+//DEPS com.microsoft.kiota:microsoft-kiota-abstractions:0.3.0
+//DEPS com.microsoft.kiota:microsoft-kiota-serialization-json:0.3.0
+//DEPS com.microsoft.kiota:microsoft-kiota-serialization-text:0.3.0
+//DEPS com.microsoft.kiota:microsoft-kiota-serialization-form:0.3.0
 //DEPS com.google.code.findbugs:jsr305:3.0.0
 //SOURCES src/**/*.java
 
