0e954c41eab14f1dd62f9b98030ba533cec04aab
Vincent Biret <vibiret@microsoft.com>
2023-03-01 14:45:18 -0500

    - fixes command generation
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- it/get-additional-arguments.ps1
+++ it/get-additional-arguments.ps1
@@ -13,39 +13,35 @@ if ([string]::IsNullOrEmpty($language)) {
     exit 1
 }
 
-$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$configPath = Join-Path -Path $scriptPath -ChildPath "config.json"
-
-$jsonValue = Get-Content -Path $configPath -Raw | ConvertFrom-Json
-
-$descriptionValue = $jsonValue.psobject.properties.Where({ $_.name -eq $descriptionUrl }).value
-if ($null -eq $descriptionValue) {
-    Write-Information "No configuration found for $descriptionUrl"
-    exit 0
-}
-
-$command = "--output './it/$language'";
+$command = " --output './it/$language'";
 if ($language -eq "csharp") {
-    $command = "--output './it/$language/client'"
+    $command = " --output './it/$language/client'"
 }
 elseif ($language -eq "java") {
-    $command = "--output './it/$language/src'"
+    $command = " --output './it/$language/src'"
 }
 
-if ($descriptionValue.PSObject.Properties.Name -contains "ExcludePatterns") {
-    $descriptionValue.ExcludePatterns | ForEach-Object {
-        $command += " -e '$($_.Pattern)'"
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$configPath = Join-Path -Path $scriptPath -ChildPath "config.json"
+$jsonValue = Get-Content -Path $configPath -Raw | ConvertFrom-Json
+$descriptionValue = $jsonValue.psobject.properties.Where({ $_.name -eq $descriptionUrl }).value
+if ($null -ne $descriptionValue) {
+    Write-Information "No configuration found for $descriptionUrl"
+    if ($descriptionValue.PSObject.Properties.Name -contains "ExcludePatterns") {
+        $descriptionValue.ExcludePatterns | ForEach-Object {
+            $command += " -e '$($_.Pattern)'"
+        }
     }
-}
 
-if ($descriptionValue.PSObject.Properties.Name -contains "IncludePatterns") {
-    $descriptionValue.IncludePatterns | ForEach-Object {
-        $command += " -i '$($_.Pattern)'"
+    if ($descriptionValue.PSObject.Properties.Name -contains "IncludePatterns") {
+        $descriptionValue.IncludePatterns | ForEach-Object {
+            $command += " -i '$($_.Pattern)'"
+        }
     }
 }
 
 if ($language -eq "csharp") {
-    $command += " --namespace-name 'app.first'"
+    $command += " --namespace-name 'app.client'"
 }
 
 return $command
