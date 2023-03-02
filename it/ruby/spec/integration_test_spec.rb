4740c40f9fb3612e066a29811c676a69c8688524
Vincent Biret <vibiret@microsoft.com>
2023-03-02 14:48:34 -0500

    - additional set of fixes for ruby integration tests
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- it/ruby/spec/integration_test_spec.rb
+++ it/ruby/spec/integration_test_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Integration_test do
   it "does something useful" do
     context = MicrosoftKiotaAuthenticationOAuth::ClientCredentialContext.new("9A2BF795-AB23-46CF-BA7D-08F48912CEE0", "E4650AC0-9E59-4997-8215-31D3A42B9A8B", "foo")
     auth_provider = MicrosoftKiotaAuthenticationOAuth::OAuthAuthenticationProvider.new(context, nil, nil)
-    api = Integration_test::Client.new(MicrosoftKiotaFaraday::FaradayRequestAdapter.new(auth_provider))
+    api = Integration_test::Client::ApiClient.new(MicrosoftKiotaFaraday::FaradayRequestAdapter.new(auth_provider))
     expect(api).to_not be nil
   end
 end
