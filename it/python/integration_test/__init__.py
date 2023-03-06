71d3408997936877e8f90ce73d85d0b660c51cd8
Vincent Biret <vibiret@microsoft.com>
2023-03-06 13:20:54 -0500

    - fixes missing dependencies for python
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- it/python/integration_test/__init__.py
+++ it/python/integration_test/__init__.py
@@ -1,7 +1,7 @@
-from .client.api_client import ApiClient
 from azure.identity.aio import DefaultAzureCredential
 from kiota_authentication_azure.azure_identity_authentication_provider import AzureIdentityAuthenticationProvider
-from kiota_http import HttpxRequestAdapter
+from kiota_http.httpx_request_adapter import HttpxRequestAdapter
+from .client.api_client import ApiClient
 
 credential=DefaultAzureCredential()
 auth_provider = AzureIdentityAuthenticationProvider(credential)
