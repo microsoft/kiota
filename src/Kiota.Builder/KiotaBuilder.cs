b502203c6b48145899dfefd1e175d6785111677a
Vincent Biret <vibiret@microsoft.com>
2023-03-09 12:11:16 -0500

    - fixes the mapping
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Kiota.Builder/KiotaBuilder.cs
+++ src/Kiota.Builder/KiotaBuilder.cs
@@ -903,7 +903,7 @@ public class KiotaBuilder
                 ("number" or "integer", "uint8") => "byte",
                 ("number" or "integer", "int64") => "int64",
                 ("number", "int32") => "integer",
-                ("number", _) => "float64",
+                ("number", _) => "double",
                 ("integer", _) => "integer",
                 ("boolean", _) => "boolean",
                 (_, "byte") => "base64",
