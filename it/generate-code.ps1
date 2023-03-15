da2d77353c2def46298e0562d611317f2962b17e
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2023-03-15 10:25:30 +0000

    Add a built-in dev mode to generate-code script

--- it/generate-code.ps1
+++ it/generate-code.ps1
@@ -2,7 +2,8 @@
 
 param(
     [Parameter(Mandatory = $true)][string]$descriptionUrl,
-    [Parameter(Mandatory = $true)][string]$language
+    [Parameter(Mandatory = $true)][string]$language,
+    [Parameter(Mandatory = $false)][switch]$dev
 )
 
 if ([string]::IsNullOrEmpty($descriptionUrl)) {
@@ -19,6 +20,23 @@ $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $additionalArgumentCmd = Join-Path -Path $scriptPath -ChildPath "get-additional-arguments.ps1"
 $additionalArguments = Invoke-Expression "$additionalArgumentCmd -descriptionUrl $descriptionUrl -language $language"
 $rootPath = Join-Path -Path $scriptPath -ChildPath ".."
-$kiotaExec = Join-Path -Path $rootPath -ChildPath "publish"
-$kiotaExec = Join-Path -Path $kiotaExec -ChildPath "kiota"
-Start-Process "$kiotaExec" -ArgumentList "generate --language ${language} --openapi ${descriptionUrl}${additionalArguments}" -Wait -NoNewWindow
+
+switch ($dev)
+{
+    $true {
+        Write-Warning "Using kiota in dev mode"
+        $kiotaExec = Join-Path -Path $rootPath -ChildPath "src"
+        $kiotaExec = Join-Path -Path $kiotaExec -ChildPath "kiota"
+        $kiotaExec = Join-Path -Path $kiotaExec -ChildPath "bin"
+        $kiotaExec = Join-Path -Path $kiotaExec -ChildPath "Debug"
+        $kiotaExec = Join-Path -Path $kiotaExec -ChildPath "net7.0"
+        $kiotaExec = Join-Path -Path $kiotaExec -ChildPath "kiota"
+        break
+    }
+    default { 
+        $kiotaExec = Join-Path -Path $rootPath -ChildPath "publish"
+        $kiotaExec = Join-Path -Path $kiotaExec -ChildPath "kiota"
+        break
+    }
+}
+Start-Process "$kiotaExec" -ArgumentList "generate --clean-output --language ${language} --openapi ${descriptionUrl}${additionalArguments}" -Wait -NoNewWindow
