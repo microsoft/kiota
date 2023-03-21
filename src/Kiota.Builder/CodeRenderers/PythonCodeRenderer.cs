17078da77a16689da4eabb81c2cd89ab42e15af0
samwelkanda <samwelkanda@gmail.com>
2023-03-21 17:39:25 +0300

    Add code renderer for python

---
+++ src/Kiota.Builder/CodeRenderers/PythonCodeRenderer.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+using Kiota.Builder.CodeDOM;
+using Kiota.Builder.Configuration;
+
+namespace Kiota.Builder.CodeRenderers;
+public class PythonCodeRenderer : CodeRenderer
+{
+    public PythonCodeRenderer(GenerationConfiguration configuration) : base(configuration, new CodeElementOrderComparerPython()) { }
+}
