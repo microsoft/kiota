b3d0f1b20a70a792e0e8d3e9e5037813f8420599
samwelkanda <samwelkanda@gmail.com>
2023-03-22 00:33:54 +0300

    Write methods before properties

--- src/Kiota.Builder/CodeElementOrderComparerPython.cs
+++ src/Kiota.Builder/CodeElementOrderComparerPython.cs
@@ -3,6 +3,21 @@
 namespace Kiota.Builder;
 public class CodeElementOrderComparerPython : CodeElementOrderComparer
 {
+    protected override int GetTypeFactor(CodeElement element)
+    {
+        return element switch
+        {
+            CodeUsing => 1,
+            ClassDeclaration => 2,
+            InterfaceDeclaration => 3,
+            CodeMethod => 4,
+            CodeIndexer => 5,
+            CodeProperty => 6,
+            CodeClass => 7,
+            BlockEnd => 8,
+            _ => 0,
+        };
+    }
     protected override int methodKindWeight { get; } = 200;
     protected override int GetMethodKindFactor(CodeElement element)
     {
