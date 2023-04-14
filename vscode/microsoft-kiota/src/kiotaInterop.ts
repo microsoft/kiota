ab66eb23db4a95e5057355a877e6587c4bdf7a3b
Vincent Biret <vibiret@microsoft.com>
2023-04-14 13:00:50 -0400

    - fixes a bug where the extension filter would not include matching nodes with children

--- vscode/microsoft-kiota/src/kiotaInterop.ts
+++ vscode/microsoft-kiota/src/kiotaInterop.ts
@@ -33,7 +33,7 @@ export interface KiotaOpenApiNode {
     segment: string,
     path: string,
     children: KiotaOpenApiNode[],
-    selected: boolean,
+    selected?: boolean,
 }
 
 export interface KiotaShowConfiguration {
