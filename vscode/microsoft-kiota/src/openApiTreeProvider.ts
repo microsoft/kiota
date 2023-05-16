029dc7de908b536647f3c157a23d8ee5101ffa9a
Vincent Biret <vibiret@microsoft.com>
2023-05-16 09:42:07 -0400

    - fixes flaky selection with multiple indexers in the extension

--- vscode/microsoft-kiota/src/openApiTreeProvider.ts
+++ vscode/microsoft-kiota/src/openApiTreeProvider.ts
@@ -110,9 +110,15 @@ export class OpenApiTreeProvider implements vscode.TreeDataProvider<OpenApiTreeN
         }
         const segment = segments.shift();
         if (segment) {
-            const child = currentNode.children.find(x => x.segment === segment);
+            let child = currentNode.children.find(x => x.segment === segment);
             if (child) {
                 return this.findApiNode(segments, child);
+            } else if (segment.startsWith('{') && segment.endsWith('}')) {
+                // in case there are multiple single parameters nodes with different names at the same level
+                child = currentNode.children.find(x => x.segment.startsWith('{') && x.segment.endsWith('}'));
+                if (child) {
+                    return this.findApiNode(segments, child);
+                }
             }
         }
         return undefined;
