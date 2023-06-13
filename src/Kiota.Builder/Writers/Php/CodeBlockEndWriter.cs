61d5a04f89a4c4e0edbb0c8a283fb02da83d7f2a
Vincent Biret <vibiret@microsoft.com>
2023-06-13 13:41:43 -0400

    - cleans up python and php writers

--- src/Kiota.Builder/Writers/Php/CodeBlockEndWriter.cs
+++ src/Kiota.Builder/Writers/Php/CodeBlockEndWriter.cs
@@ -1,10 +1,12 @@
-﻿using Kiota.Builder.CodeDOM;
+﻿using System;
+using Kiota.Builder.CodeDOM;
 
 namespace Kiota.Builder.Writers.Php;
 public class CodeBlockEndWriter : ICodeElementWriter<BlockEnd>
 {
     public void WriteCodeElement(BlockEnd codeElement, LanguageWriter writer)
     {
+        ArgumentNullException.ThrowIfNull(writer);
         writer.CloseBlock();
     }
 }
