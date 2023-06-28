cb37419eb5f98db41a386eb5cdb8d11f9e1cce57
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2023-06-28 15:48:11 +0300

    Fix CLI generation bug

--- src/Kiota.Builder/CodeDOM/CodeType.cs
+++ src/Kiota.Builder/CodeDOM/CodeType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Kiota.Builder.CodeDOM;
 public class CodeType : CodeTypeBase, ICloneable
@@ -20,7 +21,10 @@ public class CodeType : CodeTypeBase, ICloneable
         {
             TypeDefinition = TypeDefinition, // not cloning the type definition as it's a code element that lives in the tree and we don't want to fork the tree
             IsExternal = IsExternal,
-            GenericTypeParameterValues = new(GenericTypeParameterValues),
+            // Clone the list so that modifications on cloned objects' property are localized
+            // e.g. var y = x.Clone(); var z = y.Clone(); y.GenericTypeParameterValues.Add(value);
+            // shouldn't modify x.GenericTypeParameterValues or z.GenericTypeParameterValues
+            GenericTypeParameterValues = new(GenericTypeParameterValues.ToList()),
         }.BaseClone<CodeType>(this);
     }
     public Collection<CodeType> GenericTypeParameterValues { get; init; } = new();
