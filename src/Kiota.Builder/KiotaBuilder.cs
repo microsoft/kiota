1ec8bf4e2a8751ed25724c48182d546792ea0aea
Emond Papegaaij <emond.papegaaij@gmail.com>
2023-07-10 16:04:40 +0200

    Search through all sub schemas for a description
    Co-authored-by: Vincent Biret <vincentbiret@hotmail.com>

--- src/Kiota.Builder/KiotaBuilder.cs
+++ src/Kiota.Builder/KiotaBuilder.cs
@@ -1636,7 +1636,7 @@ public partial class KiotaBuilder
             {
                 DocumentationLabel = schema.ExternalDocs?.Description ?? string.Empty,
                 DocumentationLink = schema.ExternalDocs?.Url,
-                Description = (string.IsNullOrEmpty(schema.Description) ? schema.AllOf.LastOrDefault()?.Description : schema.Description).CleanupDescription(),
+                Description = (string.IsNullOrEmpty(schema.Description) ? schema.AllOf.OrderBy(static x => x?.Reference.Id, StringComparer.OrdinalIgnoreCase).LastOrDefault(static x => !string.IsNullOrEmpty(x.Description))?.Description : schema.Description).CleanupDescription(),
             },
             Deprecation = schema.GetDeprecationInformation(),
         };
