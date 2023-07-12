8cf71fd5034adf603bf71069d4e3630423c121ff
Vincent Biret <vibiret@microsoft.com>
2023-07-12 15:35:55 -0400

    - adds script to get pre-release version
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>
    
    - github habbits...
    
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

---
+++ scripts/get-version-from-csproj.ps1
@@ -0,0 +1,6 @@
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$csprojPath = Join-Path $scriptPath "../src/kiota/kiota.csproj"
+$xml = [Xml] (Get-Content $csprojPath)
+$version = $xml.Project.PropertyGroup.Version[0]
+Write-Output "csproj version is $version"
+Write-Host "##vso[task.setvariable variable=artifactVersion]$version"
