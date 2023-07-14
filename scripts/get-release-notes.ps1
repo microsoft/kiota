3f5fdb69ae562abf0859cf4a7111e359635710af
Vincent Biret <vibiret@microsoft.com>
2023-07-14 13:07:17 -0400

    - updates the script so it checks the presence of release notes
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- scripts/get-release-notes.ps1
+++ scripts/get-release-notes.ps1
@@ -2,17 +2,31 @@ param (
     [string]
     [Parameter(Mandatory = $true)]
     $version,
-    [string]
-    [Parameter(Mandatory = $true)]
-    $filePath
+    [switch]
+    [bool]
+    $check
 )
 
 $version = $version.TrimStart("v")
 
-$changeLogContent = Get-Content $filePath -Raw
+# Get current script directory
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$changelogPath = Join-Path -Path $scriptPath -ChildPath "../CHANGELOG.md"
+
+$changeLogContent = Get-Content $changelogPath -Raw
 $headerLine = "## [$version]"
-$headerLineNumber = $changeLogContent.IndexOf("`n" , $changeLogContent.IndexOf($headerLine)) + 3
+$versionLineNumber = $changeLogContent.IndexOf($headerLine)
+$headerLineNumber = $changeLogContent.IndexOf("`n" , $versionLineNumber) + 3
+if ($version -ne "Unreleased" -and $versionLineNumber -lt 0) {
+    throw "Version $version not found in CHANGELOG.md make sure you've updated the changelog"
+}
 $endLineNumber = $changeLogContent.IndexOf("## [", $headerLineNumber) - 3
 $releaseNotes = $changeLogContent.Substring($headerLineNumber, $endLineNumber - $headerLineNumber)
-$targetFile = Join-Path (Split-Path $filePath -Parent) "release-notes.txt"
-Set-Content -Path $targetFile -Value $releaseNotes -Verbose
+if ($version -eq "Unreleased" -and [string]::IsNullOrWhiteSpace($releaseNotes.Replace("### Changed", "").Replace("### Added", ""))) {
+    throw "Version $version not found in CHANGELOG.md make sure you've updated the changelog"
+}
+if ($check -eq $false) {
+    $targetFile = Join-Path (Split-Path $filePath -Parent) "release-notes.txt"
+    Set-Content -Path $targetFile -Value $releaseNotes -Verbose
+}
