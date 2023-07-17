0e5852776e772a24b2372ecc93fcd873179adc48
Finn Poppinga <fpoppinga@cc.systems>
2023-07-17 11:03:01 +0200

    fix: add 'method' to CSharpReservedClassNamesProvider
    - this fixes an issue, where generated class and namespace names would collide with
      the HTTP Method enum in Microsoft.Kiota.Abstractions

--- src/Kiota.Builder/Refiners/CSharpReservedClassNamesProvider.cs
+++ src/Kiota.Builder/Refiners/CSharpReservedClassNamesProvider.cs
@@ -8,7 +8,8 @@ public class CSharpReservedClassNamesProvider : IReservedNamesProvider
     private readonly Lazy<HashSet<string>> _reservedNames = new(static () => new(StringComparer.OrdinalIgnoreCase)
     {
         "BaseRequestBuilder",
-        "BaseCliRequestBuilder"
+        "BaseCliRequestBuilder",
+        "Method"
     });
     public HashSet<string> ReservedNames => _reservedNames.Value;
 }
