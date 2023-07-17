32c575b209b885c0ec409cd3bc55616f9a1b3eff
Vincent Biret <vibiret@microsoft.com>
2023-07-17 10:54:23 -0400

    - adds missing steps to the docker image build
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- scripts/get-prerelease-version.ps1
+++ scripts/get-prerelease-version.ps1
@@ -7,7 +7,10 @@ param (
     $previewBranch,
     [switch]
     [bool]
-    $excludeHeadingDash
+    $excludeHeadingDash,
+    [switch]
+    [bool]
+    $isGHA
 )
 if ($currentBranch -eq $previewBranch) {
     $buildSequenceNumber = [int]($Env:BUILD_BUILDNUMBER -split "\." | Select-Object -Last 1)
@@ -15,12 +18,24 @@ if ($currentBranch -eq $previewBranch) {
     if ($excludeHeadingDash) {
         $versionSuffix = $versionSuffix.Substring(1)
     }
-    Write-Host "##vso[task.setvariable variable=versionSuffix]$versionSuffix"
-    Write-Host "##vso[task.setvariable variable=isPrerelease]true"
+    if ($isGHA -eq $true) {
+        Write-Output "::set-output name=versionSuffix::${versionSuffix}"
+        Write-Output "::set-output name=isPrerelease::true"
+    }
+    else {
+        Write-Host "##vso[task.setvariable variable=versionSuffix]$versionSuffix"
+        Write-Host "##vso[task.setvariable variable=isPrerelease]true"
+    }
     Write-Output "Version suffix set to $versionSuffix"
 }
 else {
+    if ($isGHA -eq $true) {
+        Write-Output "::set-output name=versionSuffix::"
+        Write-Output "::set-output name=isPrerelease::false"
+    }
+    else {
+        Write-Host "##vso[task.setvariable variable=versionSuffix]"
+        Write-Host "##vso[task.setvariable variable=isPrerelease]false"
+    }
     Write-Output "Version suffix set to empty"
-    Write-Host "##vso[task.setvariable variable=versionSuffix]"
-    Write-Host "##vso[task.setvariable variable=isPrerelease]false"
 }
