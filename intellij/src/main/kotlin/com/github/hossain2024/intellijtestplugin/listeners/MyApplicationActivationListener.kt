71934322fc5ce8dd1ed4881073e2b3a61283252a
Maliha2002 <Hossain2024@users.noreply.github.com>
2023-07-21 05:46:04 -0700

    deleted kotlin files

--- intellij/src/main/kotlin/com/github/hossain2024/intellijtestplugin/listeners/MyApplicationActivationListener.kt
+++ intellij/src/main/kotlin/com/github/hossain2024/intellijtestplugin/listeners/MyApplicationActivationListener.kt
@@ -1,6 +1,6 @@
 package com.github.hossain2024.intellijtestplugin.listeners
 
-import com.github.hossain2024.intellijtestplugin.toolWindow.MyToolWindowFactory
+import com.github.hossain2024.intellijtestplugin.toolWindow.MyKiotaToolFactory
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.wm.IdeFrame
@@ -9,6 +9,6 @@ internal class MyApplicationActivationListener : ApplicationActivationListener {
 
     override fun applicationActivated(ideFrame: IdeFrame) {
         thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
-        
+
     }
 }
