77186ec1b6ac24d24f4999431dd3ac13be4c14a3
Vincent Biret <vibiret@microsoft.com>
2023-08-28 15:43:45 -0400

    - code linting
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Kiota.Builder/Refiners/TypeScriptRefiner.cs
+++ src/Kiota.Builder/Refiners/TypeScriptRefiner.cs
@@ -305,10 +305,9 @@ public class TypeScriptRefiner : CommonLanguageRefiner, ILanguageRefiner
         }
         else if (currentMethod.IsOfKind(CodeMethodKind.Factory) && currentMethod.Parameters.OfKind(CodeParameterKind.ParseNode) is CodeParameter parseNodeParam)
             parseNodeParam.Type.Name = parseNodeParam.Type.Name[1..];
-        else if (currentMethod.IsOfKind(CodeMethodKind.RawUrlBuilder))
+        else if (currentMethod.IsOfKind(CodeMethodKind.RawUrlBuilder) && currentMethod.Parameters.OfKind(CodeParameterKind.RawUrl) is { } parameter)
         {
-            if (currentMethod.Parameters.OfKind(CodeParameterKind.RawUrl) is { } parameter)
-                parameter.Type.IsNullable = false;
+            parameter.Type.IsNullable = false;
         }
         CorrectCoreTypes(currentMethod.Parent as CodeClass, DateTypesReplacements, currentMethod.Parameters
                                                 .Select(x => x.Type)
