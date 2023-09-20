eb3062df3b0d25fab77a3104efa2194026cdd510
rkodev <43806892+rkodev@users.noreply.github.com>
2023-09-20 17:12:20 +0300

    Order items

--- src/Kiota.Builder/CodeDOM/CodeFile.cs
+++ src/Kiota.Builder/CodeDOM/CodeFile.cs
@@ -6,7 +6,7 @@ namespace Kiota.Builder.CodeDOM;
 
 public class CodeFile : CodeBlock<CodeFileDeclaration, CodeFileBlockEnd>
 {
-    public IEnumerable<CodeInterface> Interfaces => InnerChildElements.Values.OfType<CodeInterface>();
+    public IEnumerable<CodeInterface> Interfaces => InnerChildElements.Values.OfType<CodeInterface>().OrderBy(static x => x.Name);
 
     public IEnumerable<T> AddElements<T>(params T[] elements) where T : CodeElement
     {
