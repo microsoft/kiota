cdb013c2327776e8ba197d82b017a51a2f6ff211
Vincent Biret <vibiret@microsoft.com>
2023-09-20 15:02:34 -0400

    - fixes a conflict when adding types wrappers

--- src/Kiota.Builder/CodeDOM/CodeBlock.cs
+++ src/Kiota.Builder/CodeDOM/CodeBlock.cs
@@ -75,6 +75,7 @@ public class CodeBlock<TBlockDeclaration, TBlockEnd> : CodeElement, IBlock where
         if (returnedValue == element)
             return element;
         if (element is CodeMethod currentMethod)
+        {
             if (currentMethod.IsOfKind(CodeMethodKind.IndexerBackwardCompatibility) &&
                 returnedValue is CodeProperty cProp &&
                 cProp.IsOfKind(CodePropertyKind.RequestBuilder) &&
@@ -99,6 +100,21 @@ public class CodeBlock<TBlockDeclaration, TBlockEnd> : CodeElement, IBlock where
                         return result2;
                 }
             }
+        }
+        else if (element is CodeProperty currentProperty)
+        {
+            if (currentProperty.Kind is CodePropertyKind.Custom &&
+                returnedValue is CodeClass returnedClass && returnedClass.Kind is CodeClassKind.Model &&
+                InnerChildElements.TryAdd($"{element.Name}-property", currentProperty))
+                return element; // inline type property: transforming union type to wrapper class
+        }
+        else if (element is CodeClass currentClass)
+        {
+            if (currentClass.Kind is CodeClassKind.Model &&
+                returnedValue is CodeProperty returnedProperty && returnedProperty.Kind is CodePropertyKind.Custom &&
+                InnerChildElements.TryAdd($"{element.Name}-model", currentClass))
+                return element; // inline type property: transforming wrapper class to union type
+        }
 
         if (element.GetType() == returnedValue.GetType())
             return (T)returnedValue;
