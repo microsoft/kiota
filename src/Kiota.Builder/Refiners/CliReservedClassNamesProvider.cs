31b23beba2a6abfa58bb7c07a5bcb959cd7ad345
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2023-11-28 19:12:23 +0300

    Reserve System.CommandLine names

--- src/Kiota.Builder/Refiners/CliReservedClassNamesProvider.cs
+++ src/Kiota.Builder/Refiners/CliReservedClassNamesProvider.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 
 namespace Kiota.Builder.Refiners;
-
-public class CSharpReservedClassNamesProvider : IReservedNamesProvider
+public class CliReservedClassNamesProvider : IReservedNamesProvider
 {
     private readonly Lazy<HashSet<string>> _reservedNames = new(static () => new(StringComparer.OrdinalIgnoreCase)
     {
-        "BaseRequestBuilder"
+        "BaseRequestBuilder",
+        "BaseCliRequestBuilder",
+        "Command",
+        "Option",
     });
     public HashSet<string> ReservedNames => _reservedNames.Value;
 }
