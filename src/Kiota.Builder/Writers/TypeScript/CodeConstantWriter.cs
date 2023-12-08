95d86967498b103e0ab4a12534b829f683557bc4
koros <korosmatick@gmail.com>
2023-12-08 19:37:03 +0300

    address pr coments

--- src/Kiota.Builder/Writers/TypeScript/CodeConstantWriter.cs
+++ src/Kiota.Builder/Writers/TypeScript/CodeConstantWriter.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Kiota.Builder.CodeDOM;
 using Kiota.Builder.Extensions;
+using Kiota.Builder.Writers.Go;
 
 namespace Kiota.Builder.Writers.TypeScript;
 public class CodeConstantWriter : BaseElementWriter<CodeConstant, TypeScriptConventionService>
@@ -15,33 +16,41 @@ public class CodeConstantWriter : BaseElementWriter<CodeConstant, TypeScriptConv
         switch (codeElement.Kind)
         {
             case CodeConstantKind.QueryParametersMapper:
-                if (codeElement.OriginalCodeElement is not CodeInterface codeInterface) throw new InvalidOperationException("Original CodeElement cannot be null");
-                writer.StartBlock($"const {codeElement.Name.ToFirstCharacterLowerCase()}: Record<string, string> = {{");
-                foreach (var property in codeInterface
-                                                        .Properties
-                                                        .OfKind(CodePropertyKind.QueryParameter)
-                                                        .Where(static x => !string.IsNullOrEmpty(x.SerializationName))
-                                                        .OrderBy(static x => x.SerializationName, StringComparer.OrdinalIgnoreCase))
-                {
-                    writer.WriteLine($"\"{property.Name.ToFirstCharacterLowerCase()}\": \"{property.SerializationName}\",");
-                }
-                writer.CloseBlock("};");
+                WriteQueryParametersMapperConstant(codeElement, writer);
                 break;
             case CodeConstantKind.EnumObject:
-                if (codeElement.OriginalCodeElement is not CodeEnum codeEnum) throw new InvalidOperationException("Original CodeElement cannot be null");
-                if (!codeEnum.Options.Any())
-                    return;
-                conventions.WriteLongDescription(codeEnum, writer);
-                writer.WriteLine($"export const {codeElement.Name.ToFirstCharacterUpperCase()} = {{");
-                writer.IncreaseIndent();
-                codeEnum.Options.ToList().ForEach(x =>
-                {
-                    conventions.WriteShortDescription(x.Documentation.Description, writer);
-                    writer.WriteLine($"{x.Name.ToFirstCharacterUpperCase()}: \"{x.WireName}\",");
-                });
-                writer.DecreaseIndent();
-                writer.WriteLine("}  as const;");
+                WriteEnumObjectConstant(codeElement, writer);
                 break;
         }
+    }
+    private static void WriteQueryParametersMapperConstant(CodeConstant codeElement, LanguageWriter writer)
+    {
+        if (codeElement.OriginalCodeElement is not CodeInterface codeInterface) throw new InvalidOperationException("Original CodeElement cannot be null");
+        writer.StartBlock($"const {codeElement.Name.ToFirstCharacterLowerCase()}: Record<string, string> = {{");
+        foreach (var property in codeInterface
+                                                .Properties
+                                                .OfKind(CodePropertyKind.QueryParameter)
+                                                .Where(static x => !string.IsNullOrEmpty(x.SerializationName))
+                                                .OrderBy(static x => x.SerializationName, StringComparer.OrdinalIgnoreCase))
+        {
+            writer.WriteLine($"\"{property.Name.ToFirstCharacterLowerCase()}\": \"{property.SerializationName}\",");
+        }
+        writer.CloseBlock("};");
+    }
+    private void WriteEnumObjectConstant(CodeConstant codeElement, LanguageWriter writer)
+    {
+        if (codeElement.OriginalCodeElement is not CodeEnum codeEnum) throw new InvalidOperationException("Original CodeElement cannot be null");
+        if (!codeEnum.Options.Any())
+            return;
+        conventions.WriteLongDescription(codeEnum, writer);
+        writer.WriteLine($"export const {codeElement.Name.ToFirstCharacterUpperCase()} = {{");
+        writer.IncreaseIndent();
+        codeEnum.Options.ToList().ForEach(x =>
+        {
+            conventions.WriteShortDescription(x.Documentation.Description, writer);
+            writer.WriteLine($"{x.Name.ToFirstCharacterUpperCase()}: \"{x.WireName}\",");
+        });
+        writer.DecreaseIndent();
+        writer.WriteLine("}  as const;");
     }
 }
--- src/Kiota.Builder/Writers/TypeScript/CodeFunctionWriter.cs
+++ src/Kiota.Builder/Writers/TypeScript/CodeFunctionWriter.cs
@@ -223,8 +223,8 @@ public class CodeFunctionWriter : BaseElementWriter<CodeFunction, TypeScriptConv
         var propertyType = conventions.GetTypeString(propType, codeFunction, false);
         if (!string.IsNullOrEmpty(propertyType) && propType is CodeType currentType)
         {
-            if (currentType.TypeDefinition is CodeEnum currentEnum)
-                return $"{(currentEnum.Flags || isCollection ? "getCollectionOfEnumValues" : "getEnumValue")}<{currentEnum.Name.ToFirstCharacterUpperCase()}>({currentEnum.CodeEnumObject?.Name.ToFirstCharacterUpperCase()})";
+            if (currentType.TypeDefinition is CodeEnum currentEnum && currentEnum.CodeEnumObject is not null)
+                return $"{(currentEnum.Flags || isCollection ? "getCollectionOfEnumValues" : "getEnumValue")}<{currentEnum.Name.ToFirstCharacterUpperCase()}>({currentEnum.CodeEnumObject.Name.ToFirstCharacterUpperCase()})";
             else if (conventions.StreamTypeName.Equals(propertyType, StringComparison.OrdinalIgnoreCase))
                 return "getByteArrayValue";
             else if (isCollection)
