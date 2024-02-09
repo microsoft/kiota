5ca222d2d7f0f6b056024044cbbaf1d0da6f34ea
Vincent Biret <vibiret@microsoft.com>
2024-02-09 10:47:18 -0500

    - adds unit tests for worskpace management service
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

---
+++ tests/Kiota.Builder.Tests/WorkspaceManagement/WorkspaceConfigurationStorageServiceTests.cs
@@ -0,0 +1,53 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Kiota.Builder.WorkspaceManagement;
+using Xunit;
+
+namespace Kiota.Builder.Tests.WorkspaceManagement;
+public sealed class WorkspaceConfigurationStorageServiceTests : IDisposable
+{
+    [Fact]
+    public async Task DefensiveProgramming()
+    {
+        Assert.Throws<ArgumentException>(() => new WorkspaceConfigurationStorageService(string.Empty));
+        var service = new WorkspaceConfigurationStorageService();
+        await Assert.ThrowsAsync<ArgumentNullException>(() => service.UpdateWorkspaceConfigurationAsync(null));
+    }
+    private readonly string tempPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+    [Fact]
+    public async Task InitializesAsync()
+    {
+        var service = new WorkspaceConfigurationStorageService(tempPath);
+        await service.InitializeAsync();
+        Assert.True(File.Exists(Path.Combine(tempPath, "kiota-config.json")));
+    }
+    [Fact]
+    public async Task FailsOnDoubleInit()
+    {
+        var service = new WorkspaceConfigurationStorageService(tempPath);
+        await service.InitializeAsync();
+        await Assert.ThrowsAsync<InvalidOperationException>(() => service.InitializeAsync());
+    }
+    [Fact]
+    public async Task ReturnsNullOnNonInitialized()
+    {
+        var service = new WorkspaceConfigurationStorageService(tempPath);
+        var result = await service.GetWorkspaceConfigurationAsync();
+        Assert.Null(result);
+    }
+    [Fact]
+    public async Task ReturnsConfigurationWhenInitialized()
+    {
+        var service = new WorkspaceConfigurationStorageService(tempPath);
+        await service.InitializeAsync();
+        var result = await service.GetWorkspaceConfigurationAsync();
+        Assert.NotNull(result);
+    }
+    public void Dispose()
+    {
+        if (Directory.Exists(tempPath))
+            Directory.Delete(tempPath, true);
+        GC.SuppressFinalize(this);
+    }
+}
