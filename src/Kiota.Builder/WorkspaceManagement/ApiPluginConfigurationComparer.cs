6a71f06691a4f8a36c9798ba9934f3446eab29f0
Vincent Biret <vibiret@microsoft.com>
2024-03-28 10:19:59 -0400

    - adds unit tests for plugin comparer

--- src/Kiota.Builder/WorkspaceManagement/ApiPluginConfigurationComparer.cs
+++ src/Kiota.Builder/WorkspaceManagement/ApiPluginConfigurationComparer.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Kiota.Builder.Lock;
@@ -8,23 +7,15 @@ namespace Kiota.Builder.WorkspaceManagement;
 /// <summary>
 /// Compares two <see cref="ApiPluginConfiguration"/> instances.
 /// </summary>
-public class ApiPluginConfigurationComparer : IEqualityComparer<ApiPluginConfiguration>
+public class ApiPluginConfigurationComparer : BaseApiConsumerConfigurationComparer<ApiPluginConfiguration>
 {
     private static readonly StringIEnumerableDeepComparer _stringIEnumerableDeepComparer = new();
     /// <inheritdoc/>
-    public bool Equals(ApiPluginConfiguration? x, ApiPluginConfiguration? y)
-    {
-        return x == null && y == null || x != null && y != null && GetHashCode(x) == GetHashCode(y);
-    }
-    /// <inheritdoc/>
-    public int GetHashCode([DisallowNull] ApiPluginConfiguration obj)
+    public override int GetHashCode([DisallowNull] ApiPluginConfiguration obj)
     {
         if (obj == null) return 0;
         return
-            (string.IsNullOrEmpty(obj.DescriptionLocation) ? 0 : obj.DescriptionLocation.GetHashCode(StringComparison.OrdinalIgnoreCase)) * 11 +
-            (string.IsNullOrEmpty(obj.OutputPath) ? 0 : obj.OutputPath.GetHashCode(StringComparison.OrdinalIgnoreCase)) * 7 +
-            _stringIEnumerableDeepComparer.GetHashCode(obj.Types?.Order(StringComparer.OrdinalIgnoreCase) ?? Enumerable.Empty<string>()) * 5 +
-            _stringIEnumerableDeepComparer.GetHashCode(obj.IncludePatterns?.Order(StringComparer.OrdinalIgnoreCase) ?? Enumerable.Empty<string>()) * 3 +
-            _stringIEnumerableDeepComparer.GetHashCode(obj.ExcludePatterns?.Order(StringComparer.OrdinalIgnoreCase) ?? Enumerable.Empty<string>()) * 2;
+            _stringIEnumerableDeepComparer.GetHashCode(obj.Types?.Order(StringComparer.OrdinalIgnoreCase) ?? Enumerable.Empty<string>()) * 11 +
+            base.GetHashCode(obj);
     }
 }
