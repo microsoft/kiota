35e1b491a3c88bb2ec1a44aa23cfcaebdcbce0e7
Andrew Omondi <anomondi@microsoft.com>
2024-04-11 15:03:56 +0300

    Update logo extension

--- src/Kiota.Builder/OpenApiExtensions/OpenApiLogoExtension.cs
+++ src/Kiota.Builder/OpenApiExtensions/OpenApiLogoExtension.cs
@@ -1,20 +1,41 @@
-﻿using Microsoft.OpenApi.Any;
+﻿using System;
+using Kiota.Builder.Extensions;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Interfaces;
+using Microsoft.OpenApi.Writers;
 
 namespace Kiota.Builder.OpenApiExtensions;
 
-public class OpenApiLogoExtension : OpenApiSimpleStringExtension
+public class OpenApiLogoExtension : IOpenApiExtension
 {
     public static string Name => "x-logo";
-    public string? Logo
+#pragma warning disable CA1056
+    public string? Url
+#pragma warning restore CA1056
     {
         get; set;
     }
-    protected override string? ValueSelector => Logo;
     public static OpenApiLogoExtension Parse(IOpenApiAny source)
     {
-        return new OpenApiLogoExtension
+        if (source is not OpenApiObject rawObject) throw new ArgumentOutOfRangeException(nameof(source));
+        var extension = new OpenApiLogoExtension();
+        if (rawObject.TryGetValue(nameof(Url).ToFirstCharacterLowerCase(), out var url) && url is OpenApiString urlValue)
         {
-            Logo = ParseString(source)
-        };
+            extension.Url = urlValue.Value;
+        }
+        return extension;
+    }
+
+    public void Write(IOpenApiWriter writer, OpenApiSpecVersion specVersion)
+    {
+        ArgumentNullException.ThrowIfNull(writer);
+        if (!string.IsNullOrEmpty(Url))
+        {
+            writer.WriteStartObject();
+            writer.WritePropertyName(nameof(Url).ToFirstCharacterLowerCase());
+            writer.WriteValue(Url);
+            writer.WriteEndObject();
+        }
     }
 }
