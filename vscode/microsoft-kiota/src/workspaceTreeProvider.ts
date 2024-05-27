d14cdab26bdacebc7efdd9155ea975c81c2f5095
ElinorW <elkaguon@microsoft.com>
2024-05-27 16:05:10 +0300

    localize string

--- vscode/microsoft-kiota/src/workspaceTreeProvider.ts
+++ vscode/microsoft-kiota/src/workspaceTreeProvider.ts
@@ -19,7 +19,7 @@ export class WorkspaceTreeProvider implements vscode.TreeDataProvider<vscode.Tre
 
     getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
         if (element) {
-            element.command = { command: 'kiota.workspace.openWorkspaceFile', title: "Open File", arguments: [vscode.Uri.file(workspaceJsonPath)], };
+            element.command = { command: 'kiota.workspace.openWorkspaceFile', title: vscode.l10n.t("Open File"), arguments: [vscode.Uri.file(workspaceJsonPath)], };
             element.contextValue = 'file';
         }
         return element;
