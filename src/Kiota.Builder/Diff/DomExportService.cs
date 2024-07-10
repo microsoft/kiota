9ac23390d74ff618a7fd8d0f4639e461bf0b581c
Vincent Biret <vibiret@microsoft.com>
2024-07-10 12:58:31 -0400

    feat: base infrastructure for dom export

---
+++ src/Kiota.Builder/Diff/DomExportService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Kiota.Builder.CodeDOM;
+
+namespace Kiota.Builder.Diff;
+
+public class DomExportService
+{
+    public DomExportService(string outputDirectoryPath)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(outputDirectoryPath);
+        OutputDirectoryPath = outputDirectoryPath;
+    }
+    private readonly string OutputDirectoryPath;
+    private const string DomExportFileName = "kiota-dom-export.txt";
+    public async Task SerializeDomAsync(CodeNamespace rootNamespace, CancellationToken cancellationToken = default)
+    {
+        //TODO there are things were order matter like enum options and parameters
+        var filePath = Path.Combine(OutputDirectoryPath, DomExportFileName);
+        using var fileStream = File.Create(filePath);
+        await Task.Delay(1, cancellationToken).ConfigureAwait(false);
+        //TODO implement linear export
+    }
+}
