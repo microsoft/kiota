c47f6229d61351b2c91cda8ebdd8c9ef9a8fa366
Vincent Biret <vibiret@microsoft.com>
2024-07-11 14:49:52 -0400

    fix: additional workspace location fixes

--- vscode/microsoft-kiota/src/workspaceTreeProvider.ts
+++ vscode/microsoft-kiota/src/workspaceTreeProvider.ts
@@ -3,13 +3,17 @@ import * as path from 'path';
 import * as fs from 'fs';
 import { KIOTA_DIRECTORY, KIOTA_WORKSPACE_FILE } from './constants';
 
-const workspaceJsonPath = path.join(vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0 ? 
+export function getWorkspaceJsonPath(): string {
+    return path.join(vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0 ? 
                                         vscode.workspace.workspaceFolders[0].uri.fsPath :
-                                        '~/', 
+                                        process.env.HOME ?? process.env.USERPROFILE ?? process.cwd(), 
                                         KIOTA_DIRECTORY,
                                         KIOTA_WORKSPACE_FILE);
+};
 
 export class WorkspaceTreeProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+    constructor(public isWSPresent: boolean) {
+    }
     async getChildren(element?: vscode.TreeItem): Promise<vscode.TreeItem[]> {
         if (!element) {
             return [new vscode.TreeItem(KIOTA_WORKSPACE_FILE, vscode.TreeItemCollapsibleState.None)];
@@ -19,7 +23,7 @@ export class WorkspaceTreeProvider implements vscode.TreeDataProvider<vscode.Tre
 
     getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
         if (element) {
-            element.command = { command: 'kiota.workspace.openWorkspaceFile', title: vscode.l10n.t("Open File"), arguments: [vscode.Uri.file(workspaceJsonPath)], };
+            element.command = { command: 'kiota.workspace.openWorkspaceFile', title: vscode.l10n.t("Open File"), arguments: [vscode.Uri.file(getWorkspaceJsonPath())], };
             element.contextValue = 'file';
         }
         return element;
@@ -27,30 +31,31 @@ export class WorkspaceTreeProvider implements vscode.TreeDataProvider<vscode.Tre
 }
 
 async function openResource(resource: vscode.Uri): Promise<void> {
+    const workspaceJsonPath = getWorkspaceJsonPath();
     try{
         await vscode.window.showTextDocument(resource);
     } catch (error) {
+        const dirPath = path.dirname(workspaceJsonPath);
+        await fs.promises.mkdir(dirPath, { recursive: true });
         await fs.promises.writeFile(workspaceJsonPath, Buffer.from('{}'));
         await vscode.window.showTextDocument(resource);
     }
 }
-async function ensureKiotaDirectory(): Promise<void> {
-    if (!vscode.workspace.workspaceFolders || vscode.workspace.workspaceFolders.length === 0) {
-        return;
-    }
-    const kiotaDir = path.dirname(workspaceJsonPath);
+async function isKiotaWorkspaceFilePresent(): Promise<boolean> {
+    const workspaceFileDir = path.resolve(getWorkspaceJsonPath());
     try {
-        await fs.promises.access(kiotaDir);
+        await fs.promises.access(workspaceFileDir);
     } catch (error) {
-        await vscode.window.showErrorMessage(
-            vscode.l10n.t("Kiota directory not found")
-        );
+        return false;
     }
+    return true;
 }
 
 export async function loadTreeView(context: vscode.ExtensionContext): Promise<void> {
-    await ensureKiotaDirectory();
-    const treeDataProvider = new WorkspaceTreeProvider();
+    const treeDataProvider = new WorkspaceTreeProvider(await isKiotaWorkspaceFilePresent());
+    vscode.workspace.onDidChangeWorkspaceFolders(async () => {
+        treeDataProvider.isWSPresent = await isKiotaWorkspaceFilePresent();
+    });
     context.subscriptions.push(vscode.window.createTreeView('kiota.workspace', { treeDataProvider }));
-    context.subscriptions.push(vscode.commands.registerCommand('kiota.workspace.openWorkspaceFile', async (resource) => await openResource(resource)));
+    context.subscriptions.push(vscode.commands.registerCommand('kiota.workspace.openWorkspaceFile', openResource));
 }
