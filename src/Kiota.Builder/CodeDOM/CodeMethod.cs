4fdd941c979a3f27bdd40b6f5978a0689fda50ec
koros <korosmatick@gmail.com>
2024-07-29 18:31:24 +0300

    remove unused code in CodeMethod.cs

--- src/Kiota.Builder/CodeDOM/CodeMethod.cs
+++ src/Kiota.Builder/CodeDOM/CodeMethod.cs
@@ -120,27 +120,6 @@ public class CodeMethod : CodeTerminalWithKind<CodeMethodKind>, ICloneable, IDoc
         method.AddParameter(parameter);
         return method;
     }
-    public static CodeMethod FromCodeFunctionAndInterface(CodeInterface codeInterface, CodeFunction function, CodeMethodKind kind)
-    {
-        ArgumentNullException.ThrowIfNull(codeInterface);
-        ArgumentNullException.ThrowIfNull(function);
-        ArgumentNullException.ThrowIfNull(kind);
-        var method = new CodeMethod
-        {
-            Name = function.OriginalLocalMethod.Name,
-            ReturnType = function.OriginalLocalMethod.ReturnType,
-            Kind = kind,
-            Access = function.OriginalLocalMethod.Access,
-            IsAsync = function.OriginalLocalMethod.IsAsync,
-            IsStatic = function.OriginalLocalMethod.IsStatic,
-            Documentation = function.OriginalLocalMethod.Documentation,
-            Parent = codeInterface.OriginalClass,
-        };
-
-        method.AddParameter(function.OriginalLocalMethod.Parameters.ToArray());
-
-        return method;
-    }
     public HttpMethod? HttpMethod
     {
         get; set;
