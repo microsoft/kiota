00a2fa6aeaf121df0140553f6125e2df5fe22b75
Vincent Biret <vibiret@microsoft.com>
2024-08-07 09:11:06 -0400

    fix: version line check

--- scripts/get-release-notes.ps1
+++ scripts/get-release-notes.ps1
@@ -14,10 +14,10 @@ $changelogPath = Join-Path -Path $PSScriptRoot -ChildPath "../CHANGELOG.md"
 $changeLogContent = Get-Content $changelogPath -Raw
 $headerLine = "## [$version]"
 $versionLineNumber = $changeLogContent.IndexOf($headerLine)
-$headerLineNumber = $changeLogContent.IndexOf("`n" , $versionLineNumber) + 3
 if ($version -ne "Unreleased" -and $versionLineNumber -lt 0) {
     throw "Version $version not found in CHANGELOG.md make sure you've updated the changelog"
 }
+$headerLineNumber = $changeLogContent.IndexOf("`n" , $versionLineNumber) + 3
 $endLineNumber = $changeLogContent.IndexOf("## [", $headerLineNumber) - 3
 $releaseNotes = $changeLogContent.Substring($headerLineNumber, $endLineNumber - $headerLineNumber)
 if ($version -eq "Unreleased" -and [string]::IsNullOrWhiteSpace($releaseNotes.Replace("### Changed", "").Replace("### Added", ""))) {
