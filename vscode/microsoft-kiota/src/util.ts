f203c6682f48d74214385e3919cda7e4f8c6530c
Faith Kangai <faithgikundi@microsoft.com>
2024-08-27 11:12:38 +0300

    skip predefined q&a when deeplinking

--- vscode/microsoft-kiota/src/util.ts
+++ vscode/microsoft-kiota/src/util.ts
@@ -1,8 +1,11 @@
-import * as vscode from 'vscode';
-import * as path from 'path';
 import * as fs from 'fs';
+import * as path from 'path';
+import * as vscode from 'vscode';
+import { QuickPickItem } from "vscode";
 import { APIMANIFEST, CLIENT, CLIENTS, KIOTA_DIRECTORY, KIOTA_WORKSPACE_FILE, PLUGIN, PLUGINS } from './constants';
-import { migrateFromLockFile, displayMigrationMessages } from './migrateFromLockFile';
+import { GenerationType, KiotaGenerationLanguage, KiotaPluginType } from './enums';
+import { allGenerationLanguages } from './kiotaInterop';
+import { displayMigrationMessages, migrateFromLockFile } from './migrateFromLockFile';
 
 const clientTypes = [CLIENT, CLIENTS];
 const pluginTypes = [PLUGIN, PLUGINS, APIMANIFEST];
@@ -92,4 +95,118 @@ export async function handleMigration(
           vscode.window.showErrorMessage(vscode.l10n.t(`Migration failed: ${error}`));
       }
   });
+}
+
+export function getSanitizedString(rawValue?: string): string| undefined{
+  return rawValue?.replace(/[^a-zA-Z0-9_]+/g, '');
+};
+
+export function parseGenerationType(generationType: string | QuickPickItem | undefined): GenerationType {
+    if(typeof generationType !== 'string') {
+        throw new Error('generationType has not been selected yet');
+    }
+    switch(generationType) {
+        case "client":
+            return GenerationType.Client;
+        case "plugin":
+            return GenerationType.Plugin;
+        case "apimanifest":
+            return GenerationType.ApiManifest;
+        default:
+            throw new Error(`Unknown generation type ${generationType}`);
+    }
+}
+
+export function parseGenerationLanguage(value: string): KiotaGenerationLanguage {
+    switch (value.toLowerCase()) {
+      case "csharp":
+          return KiotaGenerationLanguage.CSharp;
+      case "java":
+          return KiotaGenerationLanguage.Java;
+      case "typescript":
+          return KiotaGenerationLanguage.TypeScript;
+      case "php":
+          return KiotaGenerationLanguage.PHP;
+      case "python":
+          return KiotaGenerationLanguage.Python;
+      case "go":
+          return KiotaGenerationLanguage.Go;
+      case "swift":
+          return KiotaGenerationLanguage.Swift;
+      case "ruby":
+          return KiotaGenerationLanguage.Ruby;
+      case "cli":
+          return KiotaGenerationLanguage.CLI;
+      default:
+          throw new Error("unknown language");
+    }
+}
+
+export function parsePluginType(values: string[]): KiotaPluginType[] {
+    return values.map(value => {
+        switch (value.toLowerCase()) {
+            case "openai":
+                return KiotaPluginType.OpenAI;
+            case "apimanifest":
+                return KiotaPluginType.ApiManifest;
+            case "apiplugin":
+                return KiotaPluginType.ApiPlugin;
+            default:
+                throw new Error(`unknown plugin type: ${value}`);
+        }
+    });
+}
+
+export function allGenerationLanguagesToString(): string[] {
+  let allSupportedLanguages: string[] = allGenerationLanguages.map(langEnum => KiotaGenerationLanguage[langEnum]);
+  return allSupportedLanguages;
+}
+
+export function validateDeepLinkQueryParams(queryParameters: Record<string, string>):
+  Record<string, string|undefined> 
+{
+  let validQueryParams: Record<string, string|undefined> = {};
+  const descriptionUrl = queryParameters["descriptionurl"];
+  const name = getSanitizedString(queryParameters["name"]);
+  const source = getSanitizedString(queryParameters["source"]);
+  let lowercasedKind: string = queryParameters["kind"]?.toLowerCase();
+  let validKind: string | undefined = ["plugin", "client"].indexOf(lowercasedKind) > -1 ? lowercasedKind : undefined ;
+  if (!validKind){
+    vscode.window.showInformationMessage(
+      "Invalid parameter 'kind' deeplinked. Actual value: " + lowercasedKind + 
+      "Expected values: 'plugin' or 'client'"
+    );
+  }
+  let givenLanguage: string | undefined;
+  try{
+    let languageEnumerator = parseGenerationLanguage(queryParameters["language"].toLowerCase());
+    givenLanguage = KiotaGenerationLanguage[languageEnumerator];
+  }catch (e){
+    givenLanguage = undefined;
+  }
+  if (!givenLanguage && validKind === "client"){
+    let acceptedLanguages: string [] = allGenerationLanguagesToString();
+    vscode.window.showInformationMessage("Invalid 'language' parameter deeplinked. Supported languages are : " + acceptedLanguages.join(","));
+  }
+  let providedType: string | undefined;
+  try{
+    let pluginTypeEnumerator : KiotaPluginType = parsePluginType([queryParameters["type"]])[0];
+    providedType = KiotaPluginType[pluginTypeEnumerator].toLowerCase();
+  }catch(e){
+    providedType = undefined;
+  }
+  if (!providedType && validKind === "plugin"){
+    let acceptedPluginTypes: string [] = Object.keys(KiotaPluginType).filter(x => !Number(x) && x !=='0').map(x => x.toString().toLowerCase());
+    vscode.window.showInformationMessage("Invalid parameter 'type' deeplinked. Expected values: " + acceptedPluginTypes.join(","));
+  }
+
+  validQueryParams = {
+    descriptionUrl: descriptionUrl,
+    name: name,
+    kind: validKind,
+    type: providedType,
+    language: givenLanguage,
+    source: source,
+  };
+  return validQueryParams;
 }
