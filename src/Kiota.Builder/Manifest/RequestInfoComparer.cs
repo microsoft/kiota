ad0d13f67760fee54f160afba6e3ccba3a5faea1
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2024-09-03 19:13:33 +0300

    allows providing a string comparer for RequestInfoComparer

--- src/Kiota.Builder/Manifest/RequestInfoComparer.cs
+++ src/Kiota.Builder/Manifest/RequestInfoComparer.cs
@@ -4,22 +4,29 @@ using System.Diagnostics.CodeAnalysis;
 using Microsoft.OpenApi.ApiManifest;
 
 namespace Kiota.Builder.Manifest;
-public class RequestInfoComparer : IEqualityComparer<RequestInfo>
+
+/// <summary>
+/// <see cref="IEqualityComparer{T}"/> for <see cref="RequestInfo"/> objects.
+/// </summary>
+/// <param name="stringComparer">
+/// The string comparer to use when comparing string properties. Defaults to <see cref="StringComparer.OrdinalIgnoreCase"/>
+/// </param>
+public class RequestInfoComparer(StringComparer? stringComparer = null) : IEqualityComparer<RequestInfo>
 {
+    private readonly StringComparer _stringComparer = stringComparer ?? StringComparer.OrdinalIgnoreCase;
+
     public bool Equals(RequestInfo? x, RequestInfo? y)
     {
         if (x is null || y is null) return object.Equals(x, y);
-        const StringComparison comparison = StringComparison.OrdinalIgnoreCase;
-        return string.Equals(x.Method, y.Method, comparison) && string.Equals(x.UriTemplate, y.UriTemplate, comparison);
+        return _stringComparer.Equals(x.Method, y.Method) && _stringComparer.Equals(x.UriTemplate, y.UriTemplate);
     }
 
     public int GetHashCode([DisallowNull] RequestInfo obj)
     {
         var hash = new HashCode();
         if (obj == null) return hash.ToHashCode();
-        var comparer = StringComparer.OrdinalIgnoreCase;
-        hash.Add(obj.Method, comparer);
-        hash.Add(obj.UriTemplate, comparer);
+        hash.Add(obj.Method, _stringComparer);
+        hash.Add(obj.UriTemplate, _stringComparer);
         return hash.ToHashCode();
     }
 }
