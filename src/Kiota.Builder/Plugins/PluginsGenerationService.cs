036ecb9ee479a9af7ac60df30b001a4ac82aee4d
Vincent Biret <vibiret@microsoft.com>
2024-09-05 15:08:35 -0400

    fix: missing variable reference

--- src/Kiota.Builder/Plugins/PluginsGenerationService.cs
+++ src/Kiota.Builder/Plugins/PluginsGenerationService.cs
@@ -320,9 +320,9 @@ public partial class PluginsGenerationService
     {
         var conversionStarters = functions.Select(static x => x.Description)
                                                               .Where(static x => !string.IsNullOrEmpty(x))
-                                                              .Select(new ConversationStarter
+                                                              .Select(static x => new ConversationStarter
                                                                         {
-                                                                            Text = description.Length < 50 ? description : description[..50],
+                                                                            Text = x.Length < 50 ? x : x[..50],
                                                                         })
                                                               .ToArray();
 
