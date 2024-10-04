45229c1a32a2023468daad60cfef9385694c3fa0
thewahome <thewahome.cw@gmail.com>
2024-10-04 12:19:33 +0300

    replace configurations

--- vscode/microsoft-kiota/src/handlers/configurationHandler.ts
+++ vscode/microsoft-kiota/src/handlers/configurationHandler.ts
@@ -4,5 +4,5 @@ let configuration: Partial<GenerateState>;
 
 export const getGenerationConfiguration = () => configuration;
 export const setGenerationConfiguration = (config: Partial<GenerateState>) => {
-  configuration = { ...configuration, ...config };
+  configuration = config;
 };
