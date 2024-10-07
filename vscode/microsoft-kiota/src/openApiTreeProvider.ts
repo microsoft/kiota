74d681cdccca067323ca2784a75a85343972cfc8
thewahome <thewahome.cw@gmail.com>
2024-10-07 15:43:35 +0300

    remove getter function in tree provider

--- vscode/microsoft-kiota/src/openApiTreeProvider.ts
+++ vscode/microsoft-kiota/src/openApiTreeProvider.ts
@@ -146,10 +146,6 @@ export class OpenApiTreeProvider implements vscode.TreeDataProvider<OpenApiTreeN
         return '';
     }
 
-    public get clientOrPluginName(): string {
-        return this.rawRootNode?.clientNameOrPluginName || '';
-    }
-
     public get language(): string {
         if (this._workspaceFile?.clients) {
             const client = this.getFirstClient();
