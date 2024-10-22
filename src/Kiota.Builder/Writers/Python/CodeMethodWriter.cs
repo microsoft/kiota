449b7d9f30d2d8d9dfb087db9878792e246b301e
shemogumbe <shemogumbe@microsoft.com>
2024-10-22 15:57:10 +0300

    update get factory for enums

--- src/Kiota.Builder/Writers/Python/CodeMethodWriter.cs
+++ src/Kiota.Builder/Writers/Python/CodeMethodWriter.cs
@@ -583,7 +583,7 @@ public class CodeMethodWriter : BaseElementWriter<CodeMethod, PythonConventionSe
         var isStream = conventions.StreamTypeName.Equals(returnType, StringComparison.OrdinalIgnoreCase);
         var returnTypeWithoutCollectionSymbol = GetReturnTypeWithoutCollectionSymbol(codeElement, returnType);
         var genericTypeForSendMethod = GetSendRequestMethodName(isVoid, isStream, codeElement.ReturnType.IsCollection, returnTypeWithoutCollectionSymbol, isEnum);
-        var newFactoryParameter = GetTypeFactory(isVoid, isStream, returnTypeWithoutCollectionSymbol);
+        var newFactoryParameter = GetTypeFactory(isVoid, isStream, isEnum, returnTypeWithoutCollectionSymbol);
         var errorMappingVarName = NoneKeyword;
         if (codeElement.ErrorMappings.Any())
         {
@@ -809,10 +809,10 @@ public class CodeMethodWriter : BaseElementWriter<CodeMethod, PythonConventionSe
             _ => "write_object_value",
         };
     }
-    private string GetTypeFactory(bool isVoid, bool isStream, string returnType)
+    private string GetTypeFactory(bool isVoid, bool isStream, bool isEnum, string returnType)
     {
         if (isVoid) return string.Empty;
-        if (isStream) return $" \"{returnType}\",";
+        if (isStream || isEnum) return $" \"{returnType}\",";
         if (conventions.IsPrimitiveType(returnType)) return $" {returnType},";
         return $" {returnType},";
     }
