04a13a8404f56aa0813e8c55758fddea7f8e5ce2
thewahome <thewahome.cw@gmail.com>
2024-11-13 17:00:16 +0300

    load workspace content inside tree

--- vscode/microsoft-kiota/src/providers/workspaceTreeProvider.ts
+++ vscode/microsoft-kiota/src/providers/workspaceTreeProvider.ts
@@ -5,44 +5,110 @@ import * as fs from 'fs';
 import { KIOTA_WORKSPACE_FILE } from '../constants';
 import { getWorkspaceJsonPath } from '../util';
 
+interface WorkspaceContent {
+  version: string;
+  clients: Record<string, any>;
+  plugins: Record<string, any>;
+}
+
+class WorkspaceTreeItem extends vscode.TreeItem {
+  constructor(
+    public readonly label: string,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    public readonly type: 'root' | 'category' | 'item',
+    public command?: vscode.Command
+  ) {
+    super(label, collapsibleState);
+    this.contextValue = type;
+  }
+}
+
 export class WorkspaceTreeProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
-  public isWSPresent: boolean;
+  public isWorkspacePresent: boolean;
   private _onDidChangeTreeData: vscode.EventEmitter<vscode.TreeItem | undefined | null | void> = new vscode.EventEmitter<vscode.TreeItem | undefined | null | void>();
   readonly onDidChangeTreeData: vscode.Event<vscode.TreeItem | undefined | null | void> = this._onDidChangeTreeData.event;
+  private workspaceContent: WorkspaceContent | null = null;
+
   constructor(isWSPresent: boolean) {
-    this.isWSPresent = isWSPresent;
+    this.isWorkspacePresent = isWSPresent;
+    void this.loadWorkspaceContent();
   }
 
   async refreshView(): Promise<void> {
+    this.loadWorkspaceContent();
     this._onDidChangeTreeData.fire();
   }
 
-  async getChildren(element?: vscode.TreeItem): Promise<vscode.TreeItem[]> {
-    if (!this.isWSPresent) {
+  async getChildren(element?: WorkspaceTreeItem): Promise<WorkspaceTreeItem[]> {
+    if (!this.isWorkspacePresent) {
       return [];
     }
+
     if (!element) {
-      return [new vscode.TreeItem(KIOTA_WORKSPACE_FILE, vscode.TreeItemCollapsibleState.None)];
+      return [
+        new WorkspaceTreeItem(KIOTA_WORKSPACE_FILE, vscode.TreeItemCollapsibleState.Expanded, 'root')
+      ];
+    }
+
+    if (this.workspaceContent) {
+      if (element.label === KIOTA_WORKSPACE_FILE) {
+        return [
+          new WorkspaceTreeItem('Clients', vscode.TreeItemCollapsibleState.Collapsed, 'category'),
+          new WorkspaceTreeItem('Plugins', vscode.TreeItemCollapsibleState.Collapsed, 'category')
+        ];
+      }
+
+      if (element.label === 'Clients') {
+        return Object.keys(this.workspaceContent.clients).map(clientName =>
+          new WorkspaceTreeItem(clientName, vscode.TreeItemCollapsibleState.None, 'item')
+        );
+      }
+
+      if (element.label === 'Plugins') {
+        return Object.keys(this.workspaceContent.plugins).map(pluginName =>
+          new WorkspaceTreeItem(pluginName, vscode.TreeItemCollapsibleState.None, 'item')
+        );
+      }
     }
     return [];
   }
 
-  getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
+  getTreeItem(element: WorkspaceTreeItem): WorkspaceTreeItem {
     if (element) {
-      element.command = {
-        command: 'kiota.workspace.openWorkspaceFile',
-        title: vscode.l10n.t("Open File"),
-        arguments: [vscode.Uri.file(getWorkspaceJsonPath())]
-      };
-      element.contextValue = 'file';
+      if (element.type === 'root') {
+        element.command = {
+          command: 'kiota.workspace.openWorkspaceFile',
+          title: vscode.l10n.t("Open File"),
+          arguments: [vscode.Uri.file(getWorkspaceJsonPath())]
+        };
+        element.contextValue = 'folder';
+      }
     }
     return element;
+  }
+
+  public loadWorkspaceContent(): void {
+    if (!this.isWorkspacePresent) {
+      this.workspaceContent = null;
+      return;
+    }
+    try {
+      const workspaceJson = vscode.workspace.textDocuments.find(doc => doc.fileName.endsWith(KIOTA_WORKSPACE_FILE));
+      if (!workspaceJson) {
+        throw new Error('Workspace file not found');
+      }
+      const content = workspaceJson.getText();
+      this.workspaceContent = JSON.parse(content);
+    } catch (error) {
+      console.error('Error loading workspace.json:', error);
+    }
   }
 }
 
 async function openResource(resource: vscode.Uri): Promise<void> {
   await vscode.window.showTextDocument(resource);
 }
+
 async function isKiotaWorkspaceFilePresent(): Promise<boolean> {
   if (!vscode.workspace.workspaceFolders || vscode.workspace.workspaceFolders.length === 0) {
     return false;
@@ -59,13 +125,13 @@ async function isKiotaWorkspaceFilePresent(): Promise<boolean> {
 export async function loadTreeView(context: vscode.ExtensionContext): Promise<void> {
   const treeDataProvider = new WorkspaceTreeProvider(await isKiotaWorkspaceFilePresent());
   context.subscriptions.push(vscode.workspace.onDidChangeWorkspaceFolders(async () => {
-    treeDataProvider.isWSPresent = await isKiotaWorkspaceFilePresent();
+    treeDataProvider.isWorkspacePresent = await isKiotaWorkspaceFilePresent();
     await vscode.commands.executeCommand('kiota.workspace.refresh'); // Refresh the tree view when workspace folders change
   }));
   context.subscriptions.push(vscode.window.createTreeView('kiota.workspace', { treeDataProvider }));
   context.subscriptions.push(vscode.commands.registerCommand('kiota.workspace.openWorkspaceFile', openResource));
   context.subscriptions.push(vscode.commands.registerCommand('kiota.workspace.refresh', async () => {
-    treeDataProvider.isWSPresent = await isKiotaWorkspaceFilePresent();
+    treeDataProvider.isWorkspacePresent = await isKiotaWorkspaceFilePresent();
     await treeDataProvider.refreshView();
   }));
 }
