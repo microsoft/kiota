41345d56249551ed6192f948675da472d70255ad
koros <korosmatick@gmail.com>
2024-11-21 15:24:16 +0300

    add unit tests for HttpPathSegmenter

---
+++ tests/Kiota.Builder.Tests/PathSegmenters/HttpPathSegmenterTests.cs
@@ -0,0 +1,34 @@
+using Kiota.Builder.CodeDOM;
+using Kiota.Builder.PathSegmenters;
+using Xunit;
+
+namespace Kiota.Builder.Tests.PathSegmenters
+{
+    public class HttpPathSegmenterTests
+    {
+        private readonly HttpPathSegmenter segmenter;
+
+        public HttpPathSegmenterTests()
+        {
+            segmenter = new HttpPathSegmenter("D:\\source\\repos\\kiota-sample", "client");
+        }
+
+        [Fact]
+        public void HttpPathSegmenterGeneratesCorrectFileName()
+        {
+            var fileName = segmenter.NormalizeFileName(new CodeClass
+            {
+                Name = "testClass"
+            });
+            Assert.Equal("TestClass", fileName);// the file name should be Proper case
+        }
+
+        [Fact]
+        public void HttpPathSegmenterGeneratesNamespaceFolderName()
+        {
+            var namespaceName = "microsoft.Graph";
+            var normalizedNamespace = segmenter.NormalizeNamespaceSegment(namespaceName);
+            Assert.Equal("Microsoft.Graph", normalizedNamespace);// the first character is upper case
+        }
+    }
+}
