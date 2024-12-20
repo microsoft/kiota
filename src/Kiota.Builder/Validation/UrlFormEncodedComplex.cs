85c67cd239c5028545d596d81ca7b5f140560807
Vincent Biret <vibiret@microsoft.com>
2024-12-20 15:23:10 -0500

    fix: form encoded validation rule
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Kiota.Builder/Validation/UrlFormEncodedComplex.cs
+++ src/Kiota.Builder/Validation/UrlFormEncodedComplex.cs
@@ -13,21 +13,19 @@ public class UrlFormEncodedComplex : ValidationRule<OpenApiOperation>
     };
     public UrlFormEncodedComplex() : base(nameof(UrlFormEncodedComplex), static (context, operation) =>
     {
-        if (string.IsNullOrEmpty(operation.OperationId))
-            return;
         if (operation.GetRequestSchema(validContentTypes) is OpenApiSchema requestSchema)
-            ValidateSchema(requestSchema, context, operation.OperationId, "request body");
+            ValidateSchema(requestSchema, context, "request body");
         if (operation.GetResponseSchema(validContentTypes) is OpenApiSchema responseSchema)
-            ValidateSchema(responseSchema, context, operation.OperationId, "response body");
+            ValidateSchema(responseSchema, context, "response body");
     })
     {
     }
-    private static void ValidateSchema(OpenApiSchema schema, IValidationContext context, string operationId, string schemaName)
+    private static void ValidateSchema(OpenApiSchema schema, IValidationContext context, string schemaName)
     {
         if (schema == null) return;
         if (!schema.IsObjectType())
-            context.CreateWarning(nameof(UrlFormEncodedComplex), $"The operation {operationId} has a {schemaName} which is not an object type. This is not supported by Kiota and serialization will fail.");
+            context.CreateWarning(nameof(UrlFormEncodedComplex), $"The operation {context.PathString} has a {schemaName} which is not an object type. This is not supported by Kiota and serialization will fail.");
         if (schema.Properties.Any(static x => x.Value.IsObjectType()))
-            context.CreateWarning(nameof(UrlFormEncodedComplex), $"The operation {operationId} has a {schemaName} with a complex properties and the url form encoded content type. This is not supported by Kiota and serialization of complex properties will fail.");
+            context.CreateWarning(nameof(UrlFormEncodedComplex), $"The operation {context.PathString} has a {schemaName} with a complex properties and the url form encoded content type. This is not supported by Kiota and serialization of complex properties will fail.");
     }
 }
