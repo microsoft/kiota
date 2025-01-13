c573932faa8916075f45812bf3095499b82d2b41
thewahome <thewahome.cw@gmail.com>
2025-01-13 16:34:48 +0300

    create adaptive card generator from the operation

---
+++ src/Kiota.Builder/Plugins/AdaptiveCardGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+
+namespace Kiota.Builder.Plugins
+{
+    public class AdaptiveCardGenerator
+    {
+        public AdaptiveCardGenerator() { }
+
+        public string GenerateAdaptiveCard(OpenApiOperation operation)
+        {
+            ArgumentNullException.ThrowIfNull(operation);
+            
+            var responses = operation.Responses;
+            var response = responses["200"];
+            ArgumentNullException.ThrowIfNull(response);
+            
+            var schema = response.Content["application/json"].Schema;
+            ArgumentNullException.ThrowIfNull(schema);
+
+            var properties = schema.Properties;
+            ArgumentNullException.ThrowIfNull(properties);
+            
+            var builder = new StringBuilder();
+            builder.Append("{\"type\":\"AdaptiveCard\",\"$schema\":\"https://adaptivecards.io/schemas/adaptive-card.json\",\"version\":\"1.5\",\"body\":[");
+            foreach (var property in properties)
+            {
+                builder.Append("{\"type\":\"TextBlock\",\"text\":\"" + property.Key + ": ${if(" + property.Key + ", " + property.Key + ", 'N/A')}\"");
+                builder.Append(",\"wrap\":true}");
+                builder.Append(',');
+            }
+            builder.Remove(builder.Length - 1, 1);
+            builder.Append("]}");
+            return builder.ToString();
+        }
+    }
+}
---
+++ tests/Kiota.Builder.Tests/Plugins/AdaptiveCardGeneratorTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kiota.Builder.Plugins;
+using Microsoft.OpenApi.Models;
+using Xunit;
+
+namespace Kiota.Builder.Tests.Plugins
+{
+    public sealed class AdaptiveCardGeneratorTests
+    {
+        [Fact]
+        public void GenerateAdaptiveCardFromOperation()
+        {
+            var sample = new OpenApiOperation
+            {
+                Responses = new OpenApiResponses
+                {
+                    ["200"] = new OpenApiResponse
+                    {
+                        Description = "OK",
+                        Content = new Dictionary<string, OpenApiMediaType>
+                        {
+                            ["application/json"] = new OpenApiMediaType
+                            {
+                                Schema = new OpenApiSchema
+                                {
+                                    Type = "object",
+                                    Properties = new Dictionary<string, OpenApiSchema>
+                                    {
+                                        ["name"] = new OpenApiSchema
+                                        {
+                                            Type = "string"
+                                        },
+                                        ["age"] = new OpenApiSchema
+                                        {
+                                            Type = "number"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            var expected = @"{""type"":""AdaptiveCard"",""$schema"":""https://adaptivecards.io/schemas/adaptive-card.json"",""version"":""1.5"",""body"":[{""type"":""TextBlock"",""text"":""name: ${if(name, name, 'N/A')}"",""wrap"":true},{""type"":""TextBlock"",""text"":""age: ${if(age, age, 'N/A')}"",""wrap"":true}]}";
+
+            var generator = new AdaptiveCardGenerator();
+            var card = generator.GenerateAdaptiveCard(sample);
+            Assert.Equal(expected, card);
+        }
+    }
+}
