13003861fe08ab96599d792d21e6c463d4f6fcd7
thewahome <thewahome.cw@gmail.com>
2025-02-05 13:55:55 +0300

    feat: add follow-redirects dependency and update tests for compatibility

--- vscode/microsoft-kiota/src/kiotaInterop/tests/getKiotaVersion.spec.ts
+++ vscode/microsoft-kiota/src/kiotaInterop/tests/getKiotaVersion.spec.ts
@@ -1,8 +1,21 @@
 import { getKiotaVersion } from "../getKiotaVersion";
+import { https } from "follow-redirects";
 
-jest.setTimeout(60000); 
+jest.setTimeout(60000);
 
 describe("getKiotaVersion", () => {
+  let request: ReturnType<typeof https.get> | undefined;
+
+  beforeEach(() => {
+    request = undefined;
+  });
+
+  afterEach(() => {
+    if (request) {
+      request.destroy();
+    }
+  });
+
   test("should return version when successful", async () => {
     const version = await getKiotaVersion();
     expect(version).toBeDefined();
