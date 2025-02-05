49fd7f0a8f1f7db43a7f9aa3d80c1eda68ad3fc8
thewahome <thewahome.cw@gmail.com>
2025-02-05 23:05:11 +0300

    ensure kiota is present

--- vscode/microsoft-kiota/src/kiotaInterop/tests/getKiotaVersion.spec.ts
+++ vscode/microsoft-kiota/src/kiotaInterop/tests/getKiotaVersion.spec.ts
@@ -1,9 +1,15 @@
 import { getKiotaVersion } from "../getKiotaVersion";
+import { ensureKiotaIsPresent } from "../install";
 
 jest.setTimeout(60000);
 
 describe("getKiotaVersion", () => {
 
+  beforeAll(async () => {
+    console.log("Running setup before all tests");
+    await ensureKiotaIsPresent();
+    console.log("ensured Kiota Is Present");
+  });
 
   test("should return version when successful", async () => {
     const version = await getKiotaVersion();
