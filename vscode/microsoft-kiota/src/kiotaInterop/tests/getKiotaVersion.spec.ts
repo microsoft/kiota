fd33227700d5598c6e7d6ddab27a56549329e31b
thewahome <thewahome.cw@gmail.com>
2025-02-05 16:45:58 +0300

    simplify the test

--- vscode/microsoft-kiota/src/kiotaInterop/tests/getKiotaVersion.spec.ts
+++ vscode/microsoft-kiota/src/kiotaInterop/tests/getKiotaVersion.spec.ts
@@ -1,20 +1,9 @@
 import { getKiotaVersion } from "../getKiotaVersion";
-import { https } from "follow-redirects";
 
 jest.setTimeout(60000);
 
 describe("getKiotaVersion", () => {
-  let request: ReturnType<typeof https.get> | undefined;
 
-  beforeEach(() => {
-    request = undefined;
-  });
-
-  afterEach(() => {
-    if (request) {
-      request.destroy();
-    }
-  });
 
   test("should return version when successful", async () => {
     const version = await getKiotaVersion();
