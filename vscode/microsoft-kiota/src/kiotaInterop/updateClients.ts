8717cd45afd046e2f812bc9664e2439a6095c292
Charles Wahome <thewahome.cw@gmail.com>
2025-02-07 13:54:36 +0300

    Chore: decouple update clients (#6108)

---
+++ vscode/microsoft-kiota/src/kiotaInterop/updateClients.ts
@@ -0,0 +1,25 @@
+import * as rpc from "vscode-jsonrpc/node";
+
+import { KiotaLogEntry } from ".";
+import connectToKiota from "./connect";
+
+interface UpdateClientsConfiguration {
+  cleanOutput: boolean;
+  clearCache: boolean;
+  workspacePath: string;
+}
+
+export function updateClients({ cleanOutput, clearCache, workspacePath }: UpdateClientsConfiguration): Promise<KiotaLogEntry[] | undefined> {
+  return connectToKiota(async (connection) => {
+    const request = new rpc.RequestType3<string, boolean, boolean, KiotaLogEntry[], void>(
+      "Update"
+    );
+    const result = await connection.sendRequest(
+      request,
+      workspacePath,
+      cleanOutput,
+      clearCache,
+    );
+    return result;
+  });
+};
