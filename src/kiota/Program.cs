31241faf31d8c6524ee4b6ce908af53253eb0f5e
Caleb Magiya (from Dev Box) <calebmagiya@microsoft.com>
2025-02-10 11:10:29 +0300

    Add OTel libraries

--- src/kiota/Program.cs
+++ src/kiota/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System.CommandLine;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace kiota;
+
 static class Program
 {
     static async Task<int> Main(string[] args)
     {
+        // TODO: Abstract into telemetry component.
+        var version = KiotaVersion.Current();
+        var resourceBuilder =
+            ResourceBuilder
+                .CreateDefault()
+                .AddService(serviceName: "kiota", serviceNamespace: "Microsoft.OpenApi", serviceVersion: version);
+        Action<OtlpExporterOptions> configureOtlp = options =>
+        {
+            // TODO: Update endpoint
+            options.Endpoint = new Uri("https://localhost:18889");
+        };
+        // TODO: remove console exporter
+        using var meterProvider = Sdk.CreateMeterProviderBuilder()
+            .SetResourceBuilder(resourceBuilder)
+            .AddHttpClientInstrumentation()
+            .AddConsoleExporter()
+            .AddOtlpExporter(configureOtlp)
+            .Build();
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder().SetResourceBuilder(resourceBuilder)
+            .AddHttpClientInstrumentation()
+            .AddConsoleExporter()
+            .AddOtlpExporter(configureOtlp)
+            .Build();
         var rootCommand = KiotaHost.GetRootCommand();
         var result = await rootCommand.InvokeAsync(args);
         DisposeSubCommands(rootCommand);
         return result;
     }
+
     private static void DisposeSubCommands(this Command command)
     {
         if (command.Handler is IDisposable disposableHandler)
