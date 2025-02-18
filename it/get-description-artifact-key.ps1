b85d03b8a96130042872d55c46c19ba9cc8a72f9
Vincent Biret <vibiret@microsoft.com>
2025-02-18 09:01:14 -0500

    chore: comment for magic number
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- it/get-description-artifact-key.ps1
+++ it/get-description-artifact-key.ps1
@@ -7,6 +7,7 @@ if ([string]::IsNullOrEmpty($descriptionUrl)) {
     Write-Error "Description URL is empty"
     exit 1
 }
+# 97 so we don't exceed the 128 character limit for artifact names with the generation-results-typescript prefix
 $replaced = ($descriptionUrl -replace "[-:<>|\*\?\\\/\.]", "_").Substring(0, [Math]::Min(97, $descriptionUrl.Length))
 Write-Output "ARTKEY=$replaced" >> $Env:GITHUB_OUTPUT
 
