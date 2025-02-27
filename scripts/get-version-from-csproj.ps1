47f68ac371037e65af81ccdce07e5b2c4bc67295
Evans Aboge (from Dev Box) <evansaboge@microsoft.com>
2025-02-27 14:03:29 +0300

    Update version generation

--- scripts/get-version-from-csproj.ps1
+++ scripts/get-version-from-csproj.ps1
@@ -13,8 +13,9 @@ $version = $xml.Project.PropertyGroup.VersionPrefix[0]
 Write-Output "csproj version is $version"
 if ($isGHA -eq $true) {
     Write-Output "version=$version" >> $Env:GITHUB_OUTPUT
-
 }
 else {
+    # Set both a regular variable and an output variable
     Write-Host "##vso[task.setvariable variable=artifactVersion]$version"
+    Write-Host "##vso[task.setvariable variable=version;isoutput=true]$version"
 }
