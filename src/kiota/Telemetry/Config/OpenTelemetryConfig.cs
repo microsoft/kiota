7976e7ef351863529161c64ff9746a1e6db642d8
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2025-02-27 15:22:10 +0300

    Add telemetry PoC (#6128)
    * Add .NET generic host
    
    Co-authored-by: Caleb Magiya (from Dev Box) <calebmagiya@microsoft.com>
    Co-authored-by: Vincent Biret <vibiret@microsoft.com>

---
+++ src/kiota/Telemetry/Config/OpenTelemetryConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace kiota.Telemetry.Config;
+
+public class OpenTelemetryConfig
+{
+    public bool Enabled
+    {
+        get;
+        set;
+    }
+    public string? EndpointAddress
+    {
+        get;
+        set;
+    }
+}
---
+++ src/kiota/Telemetry/Instrumentation.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace kiota.Telemetry;
+
+internal class Instrumentation(IMeterFactory meterFactory) : IDisposable
+{
+    private readonly Meter _meter = meterFactory.Create(TelemetryLabels.ScopeName);
+
+    /// <summary>
+    /// An activity source is used to create activities (spans) during manual instrumentation.
+    /// </summary>
+    public ActivitySource ActivitySource { get; } = new(TelemetryLabels.ScopeName);
+
+    /// <summary>
+    /// Creates a histogram instrument used to time command duration
+    /// </summary>
+    /// <returns>A histogram instrument</returns>
+    public Histogram<double> CreateCommandDurationHistogram()
+    {
+        return _meter.CreateHistogram<double>(name: TelemetryLabels.InstrumentCommandDurationName, unit: "s",
+            description: "Duration of the command");
+    }
+
+    /// <summary>
+    /// Creates a counter instrument for client generations
+    /// </summary>
+    /// <returns>A counter instrument</returns>
+    public Counter<long> CreateClientGenerationCounter()
+    {
+        return _meter.CreateCounter<long>(name: TelemetryLabels.InstrumentClientGenerationCount,
+            description: "Count of client generations that have been run");
+    }
+
+    /// <summary>
+    /// Creates a counter instrument for plugin generations
+    /// </summary>
+    /// <returns>A counter instrument</returns>
+    public Counter<long> CreatePluginGenerationCounter()
+    {
+        return _meter.CreateCounter<long>(name: TelemetryLabels.InstrumentPluginGenerationCount,
+            description: "Count of plugin generations that have been run");
+    }
+
+    /// <summary>
+    /// Creates a counter instrument for command execution
+    /// </summary>
+    /// <returns>A counter instrument</returns>
+    public Counter<long> CreateCommandExecutionCounter()
+    {
+        return _meter.CreateCounter<long>(name: TelemetryLabels.InstrumentCommandExecutionsCount,
+            description: "Count of commands that have been run");
+    }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        this.ActivitySource.Dispose();
+    }
+}
