b66112facfcc834cc3331291edf066a96c60906f
luisjfdez <luisj@microsoft.com>
2025-03-14 16:13:33 +0100

    setup integration tests and ability to run unit and/or integration tests

--- vscode/npm-package/jest.config.cjs
+++ vscode/npm-package/jest.config.cjs
@@ -1,21 +1,17 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: [
-    '**/?(*.)+(spec).ts?(x)'
-  ],
-  transform: {
-    '^.+\\.ts?$': ['ts-jest', {
-      tsconfig: 'tsconfig.json',
-      diagnostics: false
-    }],
-  },
-  transformIgnorePatterns: [
-    '/node_modules/',
-    '/dist/',
-  ],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-    '^dist/(.*)$': '<rootDir>/src/$1'
-  },
+const common = require('./jest.common.config.cjs')
+
+/** @returns {Promise<import('jest').Config>} */
+module.exports = async () => {
+  // When debugging, we want to have a longer timeout
+  let testTimeout = 5000; 
+  if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+      testTimeout = 999999;
+  }
+
+  return {
+    ...common,
+    globalSetup: "<rootDir>/integration_tests/setup.ts",
+    globalTeardown: "<rootDir>/integration_tests/teardown.ts",
+    testTimeout: testTimeout,
+  };
 };
