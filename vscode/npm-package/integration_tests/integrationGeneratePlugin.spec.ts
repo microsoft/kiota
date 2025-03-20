ea4cd1da0e5f1228e3c28694571a2a4ff2f8ce9e
luisjfdez <luisj@microsoft.com>
2025-03-20 07:03:24 +0100

    integration tests to get tree and generate plugin

---
+++ vscode/npm-package/integration_tests/integrationGeneratePlugin.spec.ts
@@ -0,0 +1,22 @@
+import { generatePlugin } from '../lib/generatePlugin';
+import { ConsumerOperation, KiotaPluginType } from '../types';
+
+describe("GeneratePlugin", () => {
+  test('should create plugin manifest', async () => {
+    const descriptionUrl = '../../tests/Kiota.Builder.IntegrationTests/DiscriminatorSample.yaml';
+    const outputPath = './.tests_output';
+
+    const pluginType = KiotaPluginType.ApiPlugin;
+    const actual = await generatePlugin({
+      descriptionPath: descriptionUrl,
+      outputPath: outputPath,
+      clearCache: false,
+      pluginTypes: [pluginType],
+      pluginName: 'test3',
+      operation: ConsumerOperation.Generate,
+      workingDirectory: ''
+    });
+    expect(actual).toBeDefined();
+  });
+
+});
