6a79c572739c1d26df911ab9987df50b35dc65dd
Evans Aboge (from Dev Box) <evansaboge@microsoft.com>
2025-03-28 17:34:57 +0300

    Add test

--- tests/Kiota.Builder.Tests/Writers/HTTP/CodeClassDeclarationWriterTests.cs
+++ tests/Kiota.Builder.Tests/Writers/HTTP/CodeClassDeclarationWriterTests.cs
@@ -171,4 +171,105 @@ public sealed class CodeClassDeclarationWriterTests : IDisposable
         Assert.Contains("\"title\": \"string\"", result);
         Assert.Contains("\"userId\": 0", result);
     }
+
+     [Fact]
+    public async Task WritesRequestExecutorsWithoutCrashing()
+    {
+        var codeClass = new CodeClass
+        {
+            Name = "TestClass",
+            Kind = CodeClassKind.RequestBuilder
+        };
+        var urlTemplateProperty = new CodeProperty
+        {
+            Name = "urlTemplate",
+            Kind = CodePropertyKind.UrlTemplate,
+            DefaultValue = "\"{+baseurl}/posts\"",
+            Type = new CodeType
+            {
+                Name = "string",
+                IsExternal = true
+            },
+            Documentation = new CodeDocumentation
+            {
+                DescriptionTemplate = "The URL template for the request."
+            }
+        };
+        codeClass.AddProperty(urlTemplateProperty);
+
+        // Add base url property
+        var baseUrlProperty = new CodeProperty
+        {
+            Name = "BaseUrl",
+            Kind = CodePropertyKind.Custom,
+            Access = AccessModifier.Private,
+            DefaultValue = "https://example.com",
+            Type = new CodeType { Name = "string", IsExternal = true }
+        };
+        codeClass.AddProperty(baseUrlProperty);
+
+        var method = new CodeMethod
+        {
+            Name = "get",
+            Kind = CodeMethodKind.RequestExecutor,
+            Documentation = new CodeDocumentation { DescriptionTemplate = "GET method" },
+            ReturnType = new CodeType { Name = "void" }
+        };
+
+        var postMethod = new CodeMethod
+        {
+            Name = "post",
+            Kind = CodeMethodKind.RequestExecutor,
+            Documentation = new CodeDocumentation { DescriptionTemplate = "Post method" },
+            ReturnType = new CodeType { Name = "void" },
+            RequestBodyContentType = "application/json"
+        };
+
+
+        var typeDefinition = new CodeClass
+        {
+            Name = "PostParameter",
+            Kind = CodeClassKind.QueryParameters
+        };
+
+        var properties = new List<CodeProperty>
+        {
+            new() {
+                Name = "body",
+                Kind = CodePropertyKind.QueryParameter,
+                Type = new CodeType { Name = "string", IsExternal = true }
+            }
+        };
+
+        typeDefinition.AddProperty([.. properties]);
+
+        // Define the parameter with the specified properties
+        var postParameter = new CodeParameter
+        {
+            Name = "postParameter",
+            Kind = CodeParameterKind.QueryParameter,
+            Type = new CodeType
+            {
+                Name = "PostParameter",
+                TypeDefinition = typeDefinition
+            }
+        };
+
+        // Add the parameter to the post and get methods
+        method.AddParameter(postParameter);
+        postMethod.AddParameter(postParameter);
+        codeClass.AddMethod(postMethod);
+        codeClass.AddMethod(method);
+
+
+        root.AddClass(codeClass);
+
+        await ILanguageRefiner.RefineAsync(new GenerationConfiguration { Language = GenerationLanguage.HTTP }, root);
+
+        writer.Write(codeClass.StartBlock);
+        var result = tw.ToString();
+
+        // Check HTTP operations 
+        Assert.Contains("GET {{hostAddress}}/posts HTTP/1.1", result);
+    }
 }
