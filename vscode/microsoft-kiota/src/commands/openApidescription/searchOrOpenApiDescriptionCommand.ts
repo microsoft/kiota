66a238c86bf8c958cb888f2d999312bc56061f25
thewahome <thewahome.cw@gmail.com>
2025-04-01 16:03:09 +0300

    make call to searchDescription async

--- vscode/microsoft-kiota/src/commands/openApidescription/searchOrOpenApiDescriptionCommand.ts
+++ vscode/microsoft-kiota/src/commands/openApidescription/searchOrOpenApiDescriptionCommand.ts
@@ -55,9 +55,15 @@ export class SearchOrOpenApiDescriptionCommand extends Command {
       location: vscode.ProgressLocation.Notification,
       cancellable: false,
       title: vscode.l10n.t("Searching...")
-    }, (progress, _) => {
+    }, async (progress, _) => {
       const settings = getExtensionSettings(extensionId);
-      return searchDescription({ searchTerm: x, clearCache: settings.clearCache });
+      try {
+        return await searchDescription({ searchTerm: x, clearCache: settings.clearCache });
+      } catch (err) {
+        const error = err as Error;
+        vscode.window.showErrorMessage(error.message);
+        return undefined;
+      }
     }));
 
     if (config.descriptionPath) {
