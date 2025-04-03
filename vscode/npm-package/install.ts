2507d7fb3489691e516c512f8dfdd055caa98fb9
thewahome <thewahome.cw@gmail.com>
2025-04-03 09:37:32 +0300

    chore: simplify npm install command and update environment variable for kiota binary zip path

--- vscode/npm-package/install.ts
+++ vscode/npm-package/install.ts
@@ -59,7 +59,7 @@ export async function ensureKiotaIsPresentInPath(installPath: string) {
           fs.mkdirSync(installPath, { recursive: true });
           const zipFilePath = `${installPath}.zip`;
           // If env variable that points to kiota binary zip exists, use it to copy the file instead of downloading it
-          const kiotaBinaryZip = process.env.SIDELOADING_KIOTA_BINARY_ZIP_PATH;
+          const kiotaBinaryZip = process.env.KIOTA_SIDELOADING_BINARY_ZIP_PATH;
           if (kiotaBinaryZip && fs.existsSync(kiotaBinaryZip)) {
             fs.copyFileSync(kiotaBinaryZip, zipFilePath);
           } else {
