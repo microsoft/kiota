50f05e9a5fac45191441dd084b30787e0d84dd12
Christian Mehlmauer <105281+firefart@users.noreply.github.com>
2025-04-07 15:58:55 +0000

    add comment

--- src/Kiota.Builder/Writers/Go/CodeFileDeclarationWriter.cs
+++ src/Kiota.Builder/Writers/Go/CodeFileDeclarationWriter.cs
@@ -32,7 +32,7 @@ public class CodeFileDeclarationWriter : BaseElementWriter<CodeFileDeclaration, 
                                     .Where(static x => x.Declaration != null && x.Declaration.IsExternal)
                                     .Select(static x => new Tuple<string, string>(x.Name.StartsWith('*') ? x.Name[1..] : x.Declaration!.Name.GetNamespaceImportSymbol(), x.Declaration!.Name))
                                     .Distinct())
-                                .OrderBy(static x => x.Item2)
+                                .OrderBy(static x => x.Item2) // Item1: import alias, Item2: import path
                                 .ThenBy(static x => x.Item2.Count(static y => y == '/'))
                                 .ToList();
             if (importSegments.Count != 0)
