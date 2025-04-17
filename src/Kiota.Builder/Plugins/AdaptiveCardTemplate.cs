831125b834ec9ddc82399184dcc944e20ee31533
thewahome <thewahome.cw@gmail.com>
2025-04-17 11:04:07 +0300

    use the adaptive card template as an embedded resource

--- src/Kiota.Builder/Plugins/AdaptiveCardTemplate.cs
+++ src/Kiota.Builder/Plugins/AdaptiveCardTemplate.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+﻿using System.IO;
 using Microsoft.Extensions.Logging;
 
 namespace Kiota.Builder.Plugins
@@ -11,98 +7,25 @@ namespace Kiota.Builder.Plugins
     internal class AdaptiveCardTemplate
     {
         private readonly ILogger<KiotaBuilder> Logger;
-        private string AdaptiveCard => @"
-            {
-                ""$schema"": ""http://adaptivecards.io/schemas/adaptive-card.json"",
-                ""type"": ""AdaptiveCard"",
-                ""version"": ""1.0"",
-                ""body"": [
-                    {
-                        ""type"": ""Container"",
-                        ""items"": [
-                            {
-                                ""type"": ""TextBlock"",
-                                ""text"": ""This is your adaptive card template"",
-                                ""weight"": ""bolder"",
-                                ""size"": ""medium""
-                            },
-                            {
-                                ""type"": ""ColumnSet"",
-                                ""columns"": [
-                                    {
-                                        ""type"": ""Column"",
-                                        ""width"": ""auto"",
-                                        ""items"": [
-                                            {
-                                                ""type"": ""Image"",
-                                                ""url"": ""https://github.com/microsoft/kiota/blob/main/vscode/microsoft-kiota/images/logo.png?raw=true"",
-                                                ""altText"": ""Kiota logo"",
-                                                ""size"": ""medium"",
-                                                ""style"": ""person""
-                                            }
-                                        ]
-                                    },
-                                    {
-                                        ""type"": ""Column"",
-                                        ""width"": ""auto"",
-                                        ""items"": [
-                                            {
-                                                ""type"": ""TextBlock"",
-                                                ""text"": ""Adaptive Card"",
-                                                ""weight"": ""bolder"",
-                                                ""wrap"": true
-                                            }
-                                        ]
-                                    }
-                                ]
-                            }
-                        ]
-                    },
-                    {
-                        ""type"": ""Container"",
-                        ""items"": [
-                            {
-                                ""type"": ""TextBlock"",
-                                ""text"": ""Now that we have defined the adaptive card template, one can go to the plugin manifest file and edit it to create a card that displays the relevant information for their users."",
-                                ""wrap"": true
-                            }
-                        ]
-                    },
-                    {
-                        ""type"": ""ColumnSet"",
-                        ""columns"": [
-                            {
-                                ""type"": ""Column"",
-                                ""width"": ""auto"",
-                                ""items"": [
-                                    {
-                                        ""type"": ""TextBlock"",
-                                        ""horizontalAlignment"": ""center"",
-                                        ""text"": ""Learn about [Adaptive Cards](https://adaptivecards.io/)"",
-                                        ""wrap"": true
-                                    }
-                                ]
-                            },
-                            {
-                                ""type"": ""Column"",
-                                ""width"": ""auto"",
-                                ""items"": [
-                                    {
-                                        ""type"": ""TextBlock"",
-                                        ""horizontalAlignment"": ""center"",
-                                        ""text"": ""Learn about [API Plugin for Microsoft 365 Copilot](https://learn.microsoft.com/en-us/microsoft-365-copilot/extensibility/overview-api-plugins)"",
-                                        ""wrap"": true
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            }";
+        private readonly string AdaptiveCard;
 
         public AdaptiveCardTemplate(ILogger<KiotaBuilder> logger)
         {
             Logger = logger;
+            AdaptiveCard = LoadEmbeddedResource("Kiota.Builder.Resources.AdaptiveCardTemplate.json");
+        }
+
+        private string LoadEmbeddedResource(string resourceName)
+        {
+            var assembly = typeof(AdaptiveCardTemplate).Assembly;
+            using var stream = assembly.GetManifestResourceStream(resourceName);
+            if (stream == null)
+            {
+                Logger.LogCritical("Failed to load embedded resource: {ResourceName}", resourceName);
+                throw new FileNotFoundException($"Resource {resourceName} not found.");
+            }
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
         }
 
         public void Write(string filePath)
