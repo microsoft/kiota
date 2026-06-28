0a802236d4af44b1835880881b3ab7d60eb7b0fd
Christian Mehlmauer <105281+firefart@users.noreply.github.com>
2026-06-28 22:17:04 +0200

    fix comma

--- src/Kiota.Builder/Writers/Go/CodeMethodWriter.cs
+++ src/Kiota.Builder/Writers/Go/CodeMethodWriter.cs
@@ -473,7 +473,7 @@ public class CodeMethodWriter : BaseElementWriter<CodeMethod, GoConventionServic
         var funcPrefix = writePrototypeOnly ? string.Empty : "func ";
         var returnTypeString = (code, finalReturnType, errorDeclaration) switch
         {
-            _ when code.IsAsync && string.IsNullOrEmpty(finalReturnType) => "error"
+            _ when code.IsAsync && string.IsNullOrEmpty(finalReturnType) => "error",
             _ when code.IsAsync && !string.IsNullOrEmpty(finalReturnType) => $"({finalReturnType}, error)",
             _ when !string.IsNullOrEmpty(finalReturnType) && !string.IsNullOrEmpty(errorDeclaration) => $"({finalReturnType}, {errorDeclaration})",
             _ when string.IsNullOrEmpty(finalReturnType) && !string.IsNullOrEmpty(errorDeclaration) => errorDeclaration,
