608b44cc8a89b7491a1dac9a93172d346a7299df
Vincent Biret <vibiret@microsoft.com>
2026-07-24 15:11:52 -0400

    ci: adds validation and message for changelog entry
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

---
+++ scripts/check-changelog-entry.ps1
@@ -0,0 +1,46 @@
+param (
+    [string]
+    [Parameter(Mandatory = $true)]
+    $baseChangelogPath,
+    [string]
+    [Parameter(Mandatory = $true)]
+    $headChangelogPath
+)
+
+function Get-UnreleasedEntries {
+    param (
+        [string]
+        [Parameter(Mandatory = $true)]
+        $changelogPath
+    )
+
+    $lines = Get-Content -Path $changelogPath
+    $startIndex = -1
+    for ($index = 0; $index -lt $lines.Count; $index++) {
+        if ($lines[$index].Trim() -match "^## \[Unreleased\]\s*$") {
+            $startIndex = $index
+            break
+        }
+    }
+
+    if ($startIndex -lt 0) {
+        return @()
+    }
+
+    $entries = @()
+    for ($index = $startIndex + 1; $index -lt $lines.Count; $index++) {
+        $line = $lines[$index].Trim()
+        if ($line -match "^## \[[^\]]+\]") {
+            break
+        }
+        if ($line -match "^[-*]\s+\S") {
+            $entries += ($line -replace "\s+", " ")
+        }
+    }
+    return $entries
+}
+
+$baseEntries = @(Get-UnreleasedEntries -changelogPath $baseChangelogPath)
+$headEntries = @(Get-UnreleasedEntries -changelogPath $headChangelogPath)
+
+return $headEntries.Count -gt $baseEntries.Count
