97d2b2ace093158778e83ac8ff87c7d41695f334
Wolfgang Knauf <WolfgangHG@users.noreply.github.com>
2026-08-27 14:20:41 +0000

    C#: don't create xml doc links to arrays (#8100)
    * C#: don't create xml doc links to arrays
    
    * C#: don't create xml doc links to arrays (fix formatting)

---
+++ tests/Kiota.Builder.Tests/Writers/CSharp/CSharpConventionServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kiota.Builder.CodeDOM;
+using Kiota.Builder.Writers.CSharp;
+using Xunit;
+
+namespace Kiota.Builder.Tests.Writers.CSharp
+{
+    public class CSharpConventionServiceTests
+    {
+        private readonly CSharpConventionService instance = new();
+
+        [Fact]
+        public void WritesSeeDoc()
+        {
+            // Tests that "<see cref=" documentation tags are written properly
+
+            CodeElement targetElement = new CodeClass();
+
+            // Reference to any class:
+            CodeType codeType = new CodeType()
+            {
+                Name = "SomeClass"
+            };
+
+            var see = instance.GetTypeStringForDocumentation(codeType, targetElement);
+
+            Assert.Equal("<see cref=\"SomeClass\"/>", see);
+
+            // Reference to byte[]:
+            codeType = new CodeType()
+            {
+                Name = "base64"
+            };
+
+            see = instance.GetTypeStringForDocumentation(codeType, targetElement);
+
+            // Brackets are stripped, the word "array" is added.
+            Assert.Equal("<see cref=\"byte\"/> array", see);
+        }
+    }
+}
