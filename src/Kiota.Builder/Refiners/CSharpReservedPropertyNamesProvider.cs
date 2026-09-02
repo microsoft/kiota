5ceb34d6ea966862b268732242bf59a720605ce2
WolfgangHG <wolfgang.knauf@gmx.de>
2026-09-02 22:04:58 +0200

    C#: rename property 'Equals'

---
+++ src/Kiota.Builder/Refiners/CSharpReservedPropertyNamesProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kiota.Builder.Refiners;
+
+/// <summary>
+/// Names that are not allowed for properties.
+/// </summary>
+public class CSharpReservedPropertyNamesProvider : IReservedNamesProvider
+{
+    private readonly Lazy<HashSet<string>> _reservedNames = new(static () => new(StringComparer.OrdinalIgnoreCase)
+    {
+        "Equals",  //warning "hides inherited member 'object.Equals(object?)'", https://github.com/microsoft/kiota/issues/8133
+        "GetHashCode",
+        "ToString",
+    });
+    public HashSet<string> ReservedNames => _reservedNames.Value;
+}
